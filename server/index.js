5c1a39b7ab60fa1d7769252010a4937c2f0051fa
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2019-03-30 09:32:01 -0400

    mongoose added to express i.e. server/index.js

--- server/index.js
+++ server/index.js
@@ -9,17 +9,18 @@ var dev = process.env.NODE_ENV !== 'production'
 
 var NextApp = next({ dev })
 var handle = NextApp.getRequestHandler()
-var mongoose = require('mongoose');
 
+
+var mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost:27017')
 
 NextApp.prepare().then(() => {
   const app = express()
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(cors())
+  // app.use(cors())
 
-  // app.use('/', require('./routes'))
+  app.use('/', require('./routes'))
 
   app.get('*', (req, res) => {
     return handle(req, res)
