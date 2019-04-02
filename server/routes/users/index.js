73a15d37993f100092f55442d1752cff0a218016
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-04-02 11:09:06 -0400

    worked on get and post route

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -1,15 +1,25 @@
-var router = require('express').Router()
-var UserModel = require('../../models/UserModel')
+var router = require('express').Router();
+var UserModel = require('../../models/UserModel');
 
 router
   .route('/')
-  .get()
-  .post()
+  .get(function(req, res) {
+    UserModel.find({}, (err, users) => {
+      if (err) res.status(500).send(err);
+      res.json(users);
+    });
+  })
+  .post((req, res) => {
+    var newUser = new UserModel({
+      username: req.email,
+      password: req.password
+    });
+  });
 
-router
-  .route('/:id')
-  .get()
-  .put()
-  .delete()
+// router
+//   .route('/:id')
+//   .get()
+//   .put()
+//   .delete();
 
-module.exports = router
+module.exports = router;
