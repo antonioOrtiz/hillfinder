04683f7991a69d1fc03423b6f57e9c3a665946b4
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2019-04-07 10:25:54 -0400

    syntax changes from prettier

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-var */
-var mongoose = require('mongoose');
-var emailValidator = require('email-validator');
-var bcrypt = require('bcrypt');
+var mongoose = require('mongoose')
+var emailValidator = require('email-validator')
+var bcrypt = require('bcrypt')
 
-const SALT_ROUNDS = 12;
+const SALT_ROUNDS = 12
 
 var UserSchema = new mongoose.Schema(
   {
@@ -28,23 +28,23 @@ var UserSchema = new mongoose.Schema(
   {
     timestamps: true
   }
-);
+)
 
-UserSchema.pre('save', async function preSave(next) {
-  var user = this;
-  var hash;
-  if (!user.isModified('password')) return next();
+UserSchema.pre('save', async function preSave (next) {
+  var user = this
+  var hash
+  if (!user.isModified('password')) return next()
   try {
-    hash = await bcrypt.hash(user.password, SALT_ROUNDS);
-    user.password = hash;
-    return next();
+    hash = await bcrypt.hash(user.password, SALT_ROUNDS)
+    user.password = hash
+    return next()
   } catch (err) {
-    return next(err);
+    return next(err)
   }
-});
+})
 
-UserSchema.methods.comparePassword = async function comparePassword(candidate) {
-  return bcrypt.compare(candidate, this.password);
+UserSchema.methods.comparePassword = async function comparePassword (candidate) {
+  return bcrypt.compare(candidate, this.password)
 };
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', UserSchema)
