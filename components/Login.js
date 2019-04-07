c37f4f9bc66e6123aec7e6ba601920c5df2ef5bc
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2019-04-07 19:47:54 -0400

    Adddeed front-side validation

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
-import Router from 'next/router'
-import { login } from 'next-authentication'
+
 
 import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
-// import PropTypes from 'prop-types';
+import './Login.css'
 
 class Login extends Component {
   constructor(props) {
@@ -12,17 +11,18 @@ class Login extends Component {
     this.state = {
       isLoggedIn: false,
       username: '',
-      password: ''
+      password: '',
+      usernameError: false,
+      passwordError: false,
+      formError: true,
+      formSuccess: false
+      // createUserError: false
     }
 
     this.handleChange = this.handleChange.bind(this)
     this.handleIsLoggedInClick = this.handleIsLoggedInClick.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
-
-  // componentDidMount() {
-  //   this.setState({})
-  // }
 
   handleChange(event) {
     var { name, value } = event.target
@@ -38,9 +38,33 @@ class Login extends Component {
 
   handleSubmit(event) {
     event.preventDefault()
-    var { username, password } = this.state
 
-    window.fetch('http://localhost:8016/auth/users', {
+    var { username, password } = this.state
+    var mailFormat = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+    var error = false
+
+    if (!username.match(mailFormat)) {
+      this.setState({ usernameError: true })
+      error = true
+    } else {
+      this.setState({ usernameError: false })
+    }
+
+    if (password.length <= 8) {
+      this.setState({ passwordError: true })
+      error = true
+    } else {
+      this.setState({ passwordError: false })
+    }
+     console.log(`error ${error}`)
+    if (error == false) {
+      this.setState({formError: false, formSuccess: true})
+    }
+
+
+
+   window.fetch('http://localhost:8016/auth/users', {
       method: 'POST',
       headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
       body: JSON.stringify({ email: username, password: password })
@@ -50,58 +74,28 @@ class Login extends Component {
     }).then(function (data) {
       console.log('User created:', data)
     })
-
-    // try {
-    //   const response = await window.fetch('http://localhost:8016/', {
-    //     method: 'POST',
-    //     headers: { 'Content-Type': 'application/json' },
-    //     body: JSON.stringify({ username, password })
-    //   })
-
-    //   if (response.ok) {
-    //     const { token } = await response.json()
-
-    //     const loginOptions = {
-    //       token,
-    //       cookieOptions: { expires: 1 },
-    //       callback: () => Router.push('/auth/users')
-    //     }
-    //     console.log(`token ${token}`)
-    //     login(loginOptions)
-    //   } else {
-    //     // eslint-disable-next-line no-console
-    //     console.log('Login failed.')
-    //   }
-    // } catch (error) {
-    //   console.log('Implementation or Network error.')
-    // }
   }
 
   render() {
-    var { username, password, isLoggedIn } = this.state
+    var { username, password, usernameError, passwordError, formError, formSuccess, createUserError, isLoggedIn } = this.state
     return (
       <div className='login-form'>
-        {/*
+      {/*
       Heads up! The styles below are necessary for the correct render of this example.
       You can do same with CSS, the main idea is that all the elements up to the `Grid`
       below must have a height of 100%.
     */}
-        <style>
-          {`
-      body > div,
-      body > div > div,
-      body > div > div > div.login-form {
-        height: 100%;
-      }
-    `}
-        </style>
+        <style>{`body > div, body > div > div, body > div > div > div.login-form { height: 100%;}`}</style>
+
         <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
           <Grid.Column style={{ maxWidth: 450 }}>
             <Header as='h2' color='teal' textAlign='center'>
-              {/* <Image src='/logo.png' /> Log-in to your account */}
               {isLoggedIn ? `Register for an account` : ` Log-in to your account`}
             </Header>
-            <Form size='large' onSubmit={this.handleSubmit}>
+            <Form size='large' onSubmit={this.handleSubmit} error={formError || usernameError || passwordError } success={  !formError && formSuccess } >
+            {usernameError ? <Message error header='See message below' content='Email is not incorrect format'/> : null }
+            {passwordError ? <Message error header='See message below' content='Password should be greater than eight charaters'/> : null }
+            {(!formError && formSuccess) ? <Message success header='Your user registration was successful' content='You may now log-in with the username you have chosen'/> : null }
               <Segment stacked>
                 <Form.Input
                   fluid
@@ -111,8 +105,8 @@ class Login extends Component {
                   name='username'
                   value={username}
                   onChange={this.handleChange}
+                  error={usernameError}
                 />
-
                 <Form.Input
                   fluid
                   icon='lock'
@@ -121,8 +115,9 @@ class Login extends Component {
                   name='password'
                   value={password}
                   onChange={this.handleChange}
+                  error={passwordError}
                 />
-                <Button color='teal' fluid size='large'>
+                <Button color='teal' fluid size='large' disabled={!this.state.username || !this.state.password}>
                   {isLoggedIn ? `Register` : `Log-in`}
                 </Button>
               </Segment>
@@ -131,10 +126,10 @@ class Login extends Component {
               ? <Message>
                 New to us?
                 <a onClick={this.handleIsLoggedInClick} href='#'> Register!</a>
-              </Message>
+               </Message>
               : <Message>
                 <a onClick={this.handleIsLoggedInClick} href='#'>Back to Login</a>
-              </Message>
+                </Message>
             }
           </Grid.Column>
         </Grid>
@@ -142,7 +137,4 @@ class Login extends Component {
     )
   }
 }
-
-Login.propTypes = {}
-
 export default Login
