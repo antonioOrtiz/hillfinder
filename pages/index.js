1c4e8bbd73373397b28eb863882beb2aad33903e
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-07-26 10:55:16 -0400

    seperating register and login into two individual components

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
-// import Link from 'next/link'
+import Link from 'next/link'
 import Head from '../components/head'
-import Login from '../components/Login'
+import Register from '../components/Register'
+import '../components/Register.css'
 import 'semantic-ui-css/semantic.min.css'
 
 const Home = () => (
   <div>
     <Head title='Home' />
-    <Login />
+    <Link href='/about'>
+      <a className='btn btn-light'>About us</a>
+    </Link>
+    <Register />
   </div>
 )
 
