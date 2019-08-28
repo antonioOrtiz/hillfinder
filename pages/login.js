0fc5d47f1659e0527712d2cbdc2e29b8b04c1c45
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-08-28 10:46:25 -0400

    begun work on restricting pages to logged-in users

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,24 @@
-import React from 'react'
-import Link from 'next/link'
+import Router from 'next/router'
+import withAuth from 'next-authentication'
 import 'semantic-ui-css/semantic.min.css'
 
 const Login = () => (
-  <div>
-    <Link href='/register'>
-      <a className='btn btn-light'>Register</a>
-    </Link>
+ <div>
+  {/* <Link href='/register'>
+   <a className='btn btn-light'>Register</a>
+  </Link> */}
 
-    <h2>LOGIN</h2>
-    <p>This is the login page</p>
-  </div>
+  <h2>LOGIN</h2>
+  <p>This is the login page</p>
+ </div>
 )
 
-export default Login
+const authOptions = {
+ // client callback for invalid sessions
+ callback: () => Router.push('/register'),
+ // the server takes care of the redirect, only pass a string
+ // with the route
+ serverRedirect: '/register'
+}
+export default withAuth(authOptions)(Login)
+
