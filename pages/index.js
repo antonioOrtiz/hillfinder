c07aa0a3cba8ce49eda934f98c446c6634010243
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-08-28 10:48:52 -0400

    updated import name due to change in "Register component name"; was Registe, now RegisterForm

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Head from '../components/head'
-import Register from '../components/Register'
+import Register from '../components/RegisterForm'
 import '../components/Register.css'
 import 'semantic-ui-css/semantic.min.css'
 
