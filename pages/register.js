590f9d7531c71ef022d19fd4fba00c30bbfe3480
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-09-03 21:29:10 -0400

    changed import path to match new file structure

--- pages/register.js
+++ pages/register.js
@@ -1,17 +1,12 @@
 import React from 'react'
 import Link from 'next/link'
 import 'semantic-ui-css/semantic.min.css'
-import RegisterForm from '../components/RegisterForm'
-import '../components/Register.css'
+import RegisterForm from '../components/Register/RegisterForm'
+import '../components/Register/Register.css'
 
 const Register = () => (
   <div>
-    {/* <Link href='/login'>
-      <a className='btn btn-light'>Login</a>
-    </Link> */}
-
     <RegisterForm />
-
   </div>
 )
 
