26011a407c87606f7311e482d6a81f82ae8d10ca
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-09-11 14:16:38 -0400

    removed RegisterForm imports

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Head from '../components/head'
-import Register from '../components/Register/RegisterForm'
-import '../components/Register/Register.css'
+import HomeLayout from '../components/Home/Home'
 import 'semantic-ui-css/semantic.min.css'
 import '../styles/styles.scss'
 
 const Home = () => (
   <div>
     <Head title='Home' />
-    <Register />
+    <HomeLayout/>
   </div>
 )
 
