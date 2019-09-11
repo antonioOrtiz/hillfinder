9a1dc476f77dbe3160b84707cd26c262ecf9e724
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-09-11 14:15:11 -0400

    removed auth and fixed indentation

--- pages/login.js
+++ pages/login.js
@@ -1,20 +1,13 @@
 import Router from 'next/router'
-import withAuth from 'next-authentication'
 import 'semantic-ui-css/semantic.min.css'
 import LoginForm from '../components/Login/LoginForm'
 
 const Login = () => (
  <div>
- <LoginForm/>
-</div>
+  <LoginForm/>
+ </div>
 )
 
-const authOptions = {
- // client callback for invalid sessions
- callback: () => Router.push('/register'),
- // the server takes care of the redirect, only pass a string
- // with the route
- serverRedirect: '/register'
-}
-export default withAuth(authOptions)(Login)
+export default Login
+
 
