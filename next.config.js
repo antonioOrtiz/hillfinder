846baf1f9d223d66691de1f39abed15d00a99dda
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-10-08 17:16:09 -0400

    tweaked CSS config portion in file

--- next.config.js
+++ next.config.js
@@ -1,13 +1,17 @@
 const withSass = require('@zeit/next-sass');
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS(withSass({
+ target: 'serverless',
+
     webpack(config, options) {
         config.module.rules.push({
             test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
             use: {
                 loader: 'url-loader',
                 options: {
-                    limit: 100000
+                limit: 100000,
+                target: 'serverless',
+
                 }
             }
         });
