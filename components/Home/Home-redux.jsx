7da91c74a87acd5ada6ce88575e3634494e507ec
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-10-09 16:15:35 -0400

    refactoring for integration of Nav component

--- components/Home/Home-redux.jsx
+++ components/Home/Home-redux.jsx
@@ -1,19 +1,19 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import {
- Button,
- Container,
- Divider,
- Grid,
- Header,
- Icon,
- Image,
- List,
- Menu,
- Responsive,
- Segment,
- Sidebar,
- Visibility,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Responsive,
+  Segment,
+  Sidebar,
+  Visibility,
 } from 'semantic-ui-react'
 
 // Heads up!
@@ -26,37 +26,37 @@ import {
  * such things.
  */
 const HomepageHeading = ({ mobile }) => (
- <Container text>
-  <Header
-   as='h1'
-   content='Imagine-a-Company'
-   inverted
-   style={{
-    fontSize: mobile ? '2em' : '4em',
-    fontWeight: 'normal',
-    marginBottom: 0,
-    marginTop: mobile ? '1.5em' : '3em',
-   }}
-  />
-  <Header
-   as='h2'
-   content='Do whatever you want when you want to.'
-   inverted
-   style={{
-    fontSize: mobile ? '1.5em' : '1.7em',
-    fontWeight: 'normal',
-    marginTop: mobile ? '0.5em' : '1.5em',
-   }}
-  />
-  <Button primary size='huge'>
-   Get Started
+  <Container text>
+    <Header
+      as='h1'
+      content='Imagine-a-Company'
+      inverted
+      style={{
+        fontSize: mobile ? '2em' : '4em',
+        fontWeight: 'normal',
+        marginBottom: 0,
+        marginTop: mobile ? '1.5em' : '3em',
+      }}
+    />
+    <Header
+      as='h2'
+      content='Do whatever you want when you want to.'
+      inverted
+      style={{
+        fontSize: mobile ? '1.5em' : '1.7em',
+        fontWeight: 'normal',
+        marginTop: mobile ? '0.5em' : '1.5em',
+      }}
+    />
+    <Button primary size='huge'>
+      Get Started
       <Icon name='right arrow' />
-  </Button>
- </Container>
+    </Button>
+  </Container>
 )
 
 HomepageHeading.propTypes = {
- mobile: PropTypes.bool,
+  mobile: PropTypes.bool,
 }
 
 /* Heads up!
@@ -64,147 +64,146 @@ HomepageHeading.propTypes = {
  * It can be more complicated, but you can create really flexible markup.
  */
 class DesktopContainer extends Component {
- state = {}
+  state = {}
 
- hideFixedMenu = () => this.setState({ fixed: false })
- showFixedMenu = () => this.setState({ fixed: true })
+  hideFixedMenu = () => this.setState({ fixed: false })
+  showFixedMenu = () => this.setState({ fixed: true })
 
- render() {
-  const { children, getWidth } = this.props
-  const { fixed } = this.state
+  render() {
+    const { children, getWidth } = this.props
+    const { fixed } = this.state
 
-  return (
-   <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
-    <Visibility
-     once={false}
-     onBottomPassed={this.showFixedMenu}
-     onBottomPassedReverse={this.hideFixedMenu}
-    >
-     <Segment
-      inverted
-      textAlign='center'
-      style={{ minHeight: 700, padding: '1em 0em' }}
-      vertical
-     >
-      <Menu
-       fixed={fixed ? 'top' : null}
-       inverted={!fixed}
-       pointing={!fixed}
-       secondary={!fixed}
-       size='large'
-      >
-       <Container>
-        <Menu.Item as='a' active>
-         Home
-        </Menu.Item>
-        <Menu.Item as='a'>Work</Menu.Item>
-        <Menu.Item as='a'>Company</Menu.Item>
-        <Menu.Item as='a'>Careers</Menu.Item>
-        <Menu.Item position='right'>
-         <Button as='a' inverted={!fixed}>
-          Log in
-        </Button>
-         <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
-          Sign Up
+    return (
+      <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
+        <Visibility
+          once={false}
+          onBottomPassed={this.showFixedMenu}
+          onBottomPassedReverse={this.hideFixedMenu}
+        >
+          <Segment
+            inverted
+            textAlign='center'
+            style={{ minHeight: 700, padding: '1em 0em' }}
+            vertical
+          >
+            <Menu
+              fixed={fixed ? 'top' : null}
+              inverted={!fixed}
+              pointing={!fixed}
+              secondary={!fixed}
+              size='large'
+            >
+              <Container>
+                <Menu.Item as='a' active>
+                  Home
+                </Menu.Item>
+                <Menu.Item as='a'>Work</Menu.Item>
+                <Menu.Item as='a'>Company</Menu.Item>
+                <Menu.Item as='a'>Careers</Menu.Item>
+                <Menu.Item position='right'>
+                  <Button as='a' inverted={!fixed}>
+                    Log in
                   </Button>
-        </Menu.Item>
-       </Container>
-      </Menu>
-      <HomepageHeading />
-     </Segment>
-    </Visibility>
+                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
+                    Sign Up
+                  </Button>
+                </Menu.Item>
+              </Container>
+            </Menu>
+            <HomepageHeading />
+          </Segment>
+        </Visibility>
 
-    {children}
-   </Responsive>
-  )
- }
+        {children}
+      </Responsive>
+    )
+  }
 }
 
 DesktopContainer.propTypes = {
- children: PropTypes.node,
+  children: PropTypes.node,
 }
 
 class MobileContainer extends Component {
- state = {
-  sidebarOpened:false
- }
- handleSidebarHide = () => this.setState({ sidebarOpened: false })
+  state = {}
 
- handleToggle = () => this.setState({ sidebarOpened: true })
+  handleSidebarHide = () => this.setState({ sidebarOpened: false })
 
- render() {
-  const { children, getWidth } = this.props
-  const { sidebarOpened } = this.state
+  handleToggle = () => this.setState({ sidebarOpened: true })
 
-  return (
-   <Responsive
-    as={Sidebar.Pushable}
-    getWidth={getWidth}
-    maxWidth={Responsive.onlyMobile.maxWidth}
-   >
-    <Sidebar
-     as={Menu}
-     animation='push'
-     inverted
-     onHide={this.handleSidebarHide}
-     vertical
-     visible={sidebarOpened}
-    >
-     <Menu.Item as='a' active>
-      Home
-     </Menu.Item>
-     <Menu.Item as='a'>Work</Menu.Item>
-     <Menu.Item as='a'>Company</Menu.Item>
-     <Menu.Item as='a'>Careers</Menu.Item>
-     <Menu.Item as='a'>Log in</Menu.Item>
-     <Menu.Item as='a'>Sign Up</Menu.Item>
-    </Sidebar>
+  render() {
+    const { children, getWidth } = this.props
+    const { sidebarOpened } = this.state
 
-    <Sidebar.Pusher dimmed={sidebarOpened}>
-     <Segment
-      inverted
-      textAlign='center'
-      style={{ minHeight: 350, padding: '1em 0em' }}
-      vertical
-     >
-      <Container>
-       <Menu inverted pointing secondary size='large'>
-        <Menu.Item onClick={this.handleToggle}>
-         <Icon name='sidebar' />
-        </Menu.Item>
-        <Menu.Item position='right'>
-         <Button as='a' inverted>
-          Log in
+    return (
+      <Responsive
+        as={Sidebar.Pushable}
+        getWidth={getWidth}
+        maxWidth={Responsive.onlyMobile.maxWidth}
+      >
+        <Sidebar
+          as={Menu}
+          animation='push'
+          inverted
+          onHide={this.handleSidebarHide}
+          vertical
+          visible={sidebarOpened}
+        >
+          <Menu.Item as='a' active>
+            Home
+          </Menu.Item>
+          <Menu.Item as='a'>Work</Menu.Item>
+          <Menu.Item as='a'>Company</Menu.Item>
+          <Menu.Item as='a'>Careers</Menu.Item>
+          <Menu.Item as='a'>Log in</Menu.Item>
+          <Menu.Item as='a'>Sign Up</Menu.Item>
+        </Sidebar>
+
+        <Sidebar.Pusher dimmed={sidebarOpened}>
+          <Segment
+            inverted
+            textAlign='center'
+            style={{ minHeight: 350, padding: '1em 0em' }}
+            vertical
+          >
+            <Container>
+              <Menu inverted pointing secondary size='large'>
+                <Menu.Item onClick={this.handleToggle}>
+                  <Icon name='sidebar' />
+                </Menu.Item>
+                <Menu.Item position='right'>
+                  <Button as='a' inverted>
+                    Log in
                   </Button>
-         <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-          Sign Up
+                  <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
+                    Sign Up
                   </Button>
-        </Menu.Item>
-       </Menu>
-      </Container>
-      <HomepageHeading mobile />
-     </Segment>
+                </Menu.Item>
+              </Menu>
+            </Container>
+            <HomepageHeading mobile />
+          </Segment>
 
-     {children}
-    </Sidebar.Pusher>
-   </Responsive>
-  )
- }
+          {children}
+        </Sidebar.Pusher>
+      </Responsive>
+    )
+  }
 }
 
 MobileContainer.propTypes = {
- children: PropTypes.node,
+  children: PropTypes.node,
 }
 
-const ResponsiveContainer = ({ children }) => (
- <div>
-  <DesktopContainer>{children}</DesktopContainer>
-  <MobileContainer>{children}</MobileContainer>
- </div>
+const ResponsiveContainer = ({ children,  getWidth, isLoggedIn }) => (
+  <React.Fragment>
+    <DesktopContainer isLoggedIn={isLoggedIn}>{children}</DesktopContainer>
+    <MobileContainer getWidth={getWidth} isLoggedIn={isLoggedIn}>{children}</MobileContainer>
+  </React.Fragment>
 )
 
 ResponsiveContainer.propTypes = {
- children: PropTypes.node,
+  children: PropTypes.node,
 }
 
 const getWidthFactory = isMobileFromSSR => () => {
@@ -216,11 +215,8 @@ const getWidthFactory = isMobileFromSSR => () => {
   return isSSR ? ssrValue : window.innerWidth;
 };
 
-
-
-const HomepageLayout = ({ getWidth, isMobileFromSSR }) => (
-  <ResponsiveContainer getWidth={getWidthFactory(isMobileFromSSR)} >
-    <Segment style={{ padding: '8em 0em' }} vertical>
+const HomepageLayout = ({ getWidth, isMobileFromSSR, isLoggedIn }) => (
+  <ResponsiveContainer isLoggedIn={isLoggedIn} getWidth={getWidthFactory(isMobileFromSSR)} >    <Segment style={{ padding: '8em 0em' }} vertical>
       <Grid container stackable verticalAlign='middle'>
         <Grid.Row>
           <Grid.Column width={8}>
