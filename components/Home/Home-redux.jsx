d68d55bbfc1b5e6985373a1e25a5def11e1f0da9
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-10-09 12:46:19 -0400

    tring to fix warning expected server HTML to contain a matching <a> in <div>

--- components/Home/Home-redux.jsx
+++ components/Home/Home-redux.jsx
@@ -19,11 +19,7 @@ import {
 // Heads up!
 // We using React Static to prerender our docs with server side rendering, this is a quite simple solution.
 // For more advanced usage please check Responsive docs under the "Usage" section.
-const getWidth = () => {
- const isSSR = typeof window === 'undefined'
 
- return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
-}
 
 /* eslint-disable react/no-multi-comp */
 /* Heads up! HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled components for
@@ -74,7 +70,7 @@ class DesktopContainer extends Component {
  showFixedMenu = () => this.setState({ fixed: true })
 
  render() {
-  const { children } = this.props
+  const { children, getWidth } = this.props
   const { fixed } = this.state
 
   return (
@@ -129,14 +125,15 @@ DesktopContainer.propTypes = {
 }
 
 class MobileContainer extends Component {
- state = {}
-
+ state = {
+  sidebarOpened:false
+ }
  handleSidebarHide = () => this.setState({ sidebarOpened: false })
 
  handleToggle = () => this.setState({ sidebarOpened: true })
 
  render() {
-  const { children } = this.props
+  const { children, getWidth } = this.props
   const { sidebarOpened } = this.state
 
   return (
@@ -210,129 +207,144 @@ ResponsiveContainer.propTypes = {
  children: PropTypes.node,
 }
 
-const HomepageLayout = () => (
- <ResponsiveContainer>
-  <Segment style={{ padding: '8em 0em' }} vertical>
-   <Grid container stackable verticalAlign='middle'>
-    <Grid.Row>
-     <Grid.Column width={8}>
-      <Header as='h3' style={{ fontSize: '2em' }}>
-       We Help Companies and Companions
-      </Header>
-      <p style={{ fontSize: '1.33em' }}>
-       We can give your company superpowers to do things that they never thought possible.
-       Let us delight your customers and empower your needs... through pure data analytics.
-      </p>
-      <Header as='h3' style={{ fontSize: '2em' }}>
-       We Make Bananas That Can Dance
-            </Header>
-      <p style={{ fontSize: '1.33em' }}>
-       Yes that's right, you thought it was the stuff of dreams, but even bananas can be
-       bioengineered.
-     </p>
-     </Grid.Column>
-     <Grid.Column floated='right' width={6}>
+const getWidthFactory = isMobileFromSSR => () => {
+  const isSSR = typeof window === "undefined";
+  const ssrValue = isMobileFromSSR
+    ? Responsive.onlyMobile.maxWidth
+    : Responsive.onlyTablet.minWidth;
 
-     </Grid.Column>
-    </Grid.Row>
-    <Grid.Row>
-     <Grid.Column textAlign='center'>
-      <Button size='huge'>Check Them Out</Button>
-     </Grid.Column>
-    </Grid.Row>
-   </Grid>
-  </Segment>
-{/*
-  <Segment style={{ padding: '0em' }} vertical>
-   <Grid celled='internally' columns='equal' stackable>
-    <Grid.Row textAlign='center'>
-     <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-      <Header as='h3' style={{ fontSize: '2em' }}>
-       "What a Company"
-            </Header>
-      <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
-     </Grid.Column>
-     <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-      <Header as='h3' style={{ fontSize: '2em' }}>
-       "I shouldn't have gone with their competitor."
-            </Header>
+  return isSSR ? ssrValue : window.innerWidth;
+};
 
-     </Grid.Column>
-    </Grid.Row>
-   </Grid>
-  </Segment> */}
 
-  <Segment style={{ padding: '8em 0em' }} vertical>
-   <Container text>
-    <Header as='h3' style={{ fontSize: '2em' }}>
-     Breaking The Grid, Grabs Your Attention
+
+const HomepageLayout = ({ getWidth, isMobileFromSSR }) => (
+  <ResponsiveContainer getWidth={getWidthFactory(isMobileFromSSR)} >
+    <Segment style={{ padding: '8em 0em' }} vertical>
+      <Grid container stackable verticalAlign='middle'>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Header as='h3' style={{ fontSize: '2em' }}>
+              We Help Companies and Companions
+            </Header>
+            <p style={{ fontSize: '1.33em' }}>
+              We can give your company superpowers to do things that they never thought possible.
+              Let us delight your customers and empower your needs... through pure data analytics.
+            </p>
+            <Header as='h3' style={{ fontSize: '2em' }}>
+              We Make Bananas That Can Dance
+            </Header>
+            <p style={{ fontSize: '1.33em' }}>
+              Yes that's right, you thought it was the stuff of dreams, but even bananas can be
+              bioengineered.
+            </p>
+          </Grid.Column>
+          <Grid.Column floated='right' width={6}>
+            <Image bordered rounded size='large' src='/images/wireframe/white-image.png' />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column textAlign='center'>
+            <Button size='huge'>Check Them Out</Button>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+
+    <Segment style={{ padding: '0em' }} vertical>
+      <Grid celled='internally' columns='equal' stackable>
+        <Grid.Row textAlign='center'>
+          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+            <Header as='h3' style={{ fontSize: '2em' }}>
+              "What a Company"
+            </Header>
+            <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
+          </Grid.Column>
+          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
+            <Header as='h3' style={{ fontSize: '2em' }}>
+              "I shouldn't have gone with their competitor."
+            </Header>
+            <p style={{ fontSize: '1.33em' }}>
+              <Image avatar src='/images/avatar/large/nan.jpg' />
+              <b>Nan</b> Chief Fun Officer Acme Toys
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Segment>
+
+    <Segment style={{ padding: '8em 0em' }} vertical>
+      <Container text>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+          Breaking The Grid, Grabs Your Attention
         </Header>
-    <p style={{ fontSize: '1.33em' }}>
-     Instead of focusing on content creation and hard work, we have learned how to master the
-     art of doing nothing by providing massive amounts of whitespace and generic content that
-     can seem massive, monolithic and worth your attention.
+        <p style={{ fontSize: '1.33em' }}>
+          Instead of focusing on content creation and hard work, we have learned how to master the
+          art of doing nothing by providing massive amounts of whitespace and generic content that
+          can seem massive, monolithic and worth your attention.
         </p>
-    <Button as='a' size='large'>
-     Read More
-   </Button>
-    <Divider
-     as='h4'
-     className='header'
-     horizontal
-     style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-    >
-     <a href='#'>Case Studies</a>
-    </Divider>
+        <Button as='a' size='large'>
+          Read More
+        </Button>
 
-    <Header as='h3' style={{ fontSize: '2em' }}>
-     Did We Tell You About Our Bananas?
-   </Header>
-    <p style={{ fontSize: '1.33em' }}>
-     Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
-     it's really true. It took years of gene splicing and combinatory DNA research, but our
-     bananas can really dance.
-    </p>
-    <Button as='a' size='large'>
-     I'm Still Quite Interested
-    </Button>
-   </Container>
-  </Segment>
+        <Divider
+          as='h4'
+          className='header'
+          horizontal
+          style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+        >
+          <a href='#'>Case Studies</a>
+        </Divider>
 
-  <Segment inverted vertical style={{ padding: '5em 0em' }}>
-   <Container>
-    <Grid divided inverted stackable>
-     <Grid.Row>
-      <Grid.Column width={3}>
-       <Header inverted as='h4' content='About' />
-       <List link inverted>
-        <List.Item as='a'>Sitemap</List.Item>
-        <List.Item as='a'>Contact Us</List.Item>
-        <List.Item as='a'>Religious Ceremonies</List.Item>
-        <List.Item as='a'>Gazebo Plans</List.Item>
-       </List>
-      </Grid.Column>
-      <Grid.Column width={3}>
-       <Header inverted as='h4' content='Services' />
-       <List link inverted>
-        <List.Item as='a'>Banana Pre-Order</List.Item>
-        <List.Item as='a'>DNA FAQ</List.Item>
-        <List.Item as='a'>How To Access</List.Item>
-        <List.Item as='a'>Favorite X-Men</List.Item>
-       </List>
-      </Grid.Column>
-      <Grid.Column width={7}>
-       <Header as='h4' inverted>
-        Footer Header
-       </Header>
-       <p>
-        Extra space for a call to action inside the footer that could help re-engage users.
-      </p>
-      </Grid.Column>
-     </Grid.Row>
-    </Grid>
-   </Container>
-  </Segment>
- </ResponsiveContainer>
+        <Header as='h3' style={{ fontSize: '2em' }}>
+          Did We Tell You About Our Bananas?
+        </Header>
+        <p style={{ fontSize: '1.33em' }}>
+          Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
+          it's really true. It took years of gene splicing and combinatory DNA research, but our
+          bananas can really dance.
+        </p>
+        <Button as='a' size='large'>
+          I'm Still Quite Interested
+        </Button>
+      </Container>
+    </Segment>
+
+    <Segment inverted vertical style={{ padding: '5em 0em' }}>
+      <Container>
+        <Grid divided inverted stackable>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='About' />
+              <List link inverted>
+                <List.Item as='a'>Sitemap</List.Item>
+                <List.Item as='a'>Contact Us</List.Item>
+                <List.Item as='a'>Religious Ceremonies</List.Item>
+                <List.Item as='a'>Gazebo Plans</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={3}>
+              <Header inverted as='h4' content='Services' />
+              <List link inverted>
+                <List.Item as='a'>Banana Pre-Order</List.Item>
+                <List.Item as='a'>DNA FAQ</List.Item>
+                <List.Item as='a'>How To Access</List.Item>
+                <List.Item as='a'>Favorite X-Men</List.Item>
+              </List>
+            </Grid.Column>
+            <Grid.Column width={7}>
+              <Header as='h4' inverted>
+                Footer Header
+              </Header>
+              <p>
+                Extra space for a call to action inside the footer that could help re-engage users.
+              </p>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </Segment>
+  </ResponsiveContainer>
 )
 
 export default HomepageLayout
