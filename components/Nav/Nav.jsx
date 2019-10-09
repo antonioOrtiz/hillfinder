f895c40825b4df20a7ed5d377e760b923dc5d703
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-10-09 17:28:52 -0400

    saving nav in this branch

--- components/Nav/Nav.jsx
+++ components/Nav/Nav.jsx
@@ -15,18 +15,71 @@ import {
  Segment,
  Sidebar,
  Visibility,
-} from 'semantic-ui-react'
+} from 'semantic-ui-react';
 
+import { HomepageHeading } from '../Home/Home-redux.jsx'
 
-class Nav extends Component {
-
- render() {
-  const { fixed, isLoggedIn } = this.props;
-
-  console.log("fixed ", fixed)
+export default function Nav({ children, mobile, fixed, isLoggedIn, isMobile, handleSidebarHide, handleToggle, sidebarOpened  }) {
+  console.log("fixed ", fixed);
+  console.log("isLoggedIn ", isLoggedIn)
 
   return (
-       <Container>
+     <React.Fragment>
+     {isMobile ?
+      <React.Fragment>
+       <Sidebar
+        as={Menu}
+        animation='push'
+        inverted
+        onHide={()=>handleSidebarHide}
+        vertical
+        visible={sidebarOpened}
+       >
+       <Menu.Item active>
+        <Link href="/">
+         <a>Home</a>
+        </Link>
+       </Menu.Item>
+       {isLoggedIn ? <Menu.Item as='a'> <Link href="/profile">
+           <a>Profile</a>
+          </Link></Menu.Item> : null}
+       {isLoggedIn ? <Menu.Item as='a'> <Link href="/dashboard">
+           <a>Dashboard</a>
+          </Link></Menu.Item> : null}
+       <Menu.Item>
+        <Link href="/login">
+         <a>Log in!</a>
+        </Link>
+        </Menu.Item>
+         <Menu.Item >
+          <Link href="/register">
+           <a>Register</a>
+          </Link>
+         </Menu.Item>
+       </Sidebar>
+
+      <Sidebar.Pusher dimmed={sidebarOpened}>
+       <Segment
+        inverted
+        textAlign='center'
+        style={{ minHeight: 350, padding: '1em 0em' }}
+        vertical
+       >
+        <Container>
+         <Menu inverted pointing secondary size='large'>
+          <Menu.Item onClick={()=>handleToggle}>
+           <Icon name='sidebar' />
+          </Menu.Item>
+
+         </Menu>
+        </Container>
+        <HomepageHeading mobile />
+       </Segment>
+
+       {children}
+      </Sidebar.Pusher>
+      </React.Fragment>
+      : <Container>
         <Menu.Item active>
          <Link href="/">
           <a>Home</a>
@@ -58,8 +111,7 @@ class Nav extends Component {
           </Link>
          </Button>
         </Menu.Item>
-       </Container>
+       </Container> }
+     </React.Fragment>
   )
- }
 }
-export default Nav
