81d140089bba4a69895a6fab5030990ad1020063
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-10-31 14:54:57 -0400

    added fragment to wrap component instead of div

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,11 @@
 import Router from 'next/router'
 import 'semantic-ui-css/semantic.min.css'
 import LoginForm from '../components/Login/LoginForm.jsx'
-// import Nav from '../components/Nav/Nav.jsx'
 
 const Login = () => (
- <div>
+ <>
   <LoginForm/>
- </div>
+ </>
 )
 
 export default Login
