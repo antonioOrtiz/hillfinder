144d59979519114871d826a201235c783a50db5c
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-10-31 14:50:44 -0400

    was able to finally understand what getInitialProps was doing and thus rearranged the values being passed from redux

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,44 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import Head from '../components/head'
-import HomeLayout from '../components/Home/Home.jsx'
+import HomePage from '../components/Home/Home.jsx'
 import 'semantic-ui-css/semantic.min.css'
 import '../styles/styles.scss'
-import { connect } from 'react-redux'
-
-// import {bindActionCreators} from 'redux'
-// import withRedux from 'next-redux-wrapper'
-import { logInUser } from '../store'
-
+import { logInUser, logOutUser } from '../store'
+import { bindActionCreators } from 'redux'
 
 class Home extends React.Component {
-    static async getInitialProps({ isLoggedIn }) {
-        return { isLoggedIn }
-    }
+ static getInitialProps({ isLoggedIn, logInUser, logOutUser}) {
 
-
+  return { isLoggedIn, logInUser, logOutUser}
+  }
 
     render() {
-        const { isLoggedIn } = this.props
-        return (
-        <div>
-        {console.log("isLoggedIn ", isLoggedIn)}
+     const { isLoggedIn, logInUser, logOutUser } = this.props
+     console.log("isLoggedIn", isLoggedIn );
+
+     console.log(" logInUser", logInUser);
+
+     console.log("logOutUser", logOutUser);
+
+         return (
+          <>
            <Head title = 'Home' />
-            <HomeLayout isLoggedIn = { isLoggedIn }/>
-        </div>
-        )
+           <HomePage isLoggedIn={isLoggedIn} logInUser={logInUser} logOutUser={logOutUser}/>
+          </>
+         )
     }
 }
 
+function mapStateToProps(state) {
+ const { isLoggedIn, logInUser, logOutUser } = state
+ return { isLoggedIn, logInUser, logOutUser  }
+}
+const mapDispatchToProps = dispatch =>
+ bindActionCreators({ logInUser, logOutUser }, dispatch)
 
+export default connect(
+ mapStateToProps,
+ mapDispatchToProps
+)(Home)
 
-
-export default connect(state => state)(Home);
