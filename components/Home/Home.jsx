54d9de109358f752ed7afe51bc96bd8f2ded4d99
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-11-10 13:12:05 -0500

    reorganized for inetgration with react router

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -1,81 +1,26 @@
-import PropTypes from 'prop-types'
 import {
   Button,
   Container,
   Divider,
-  Icon,
   Grid,
   Header,
   List,
   Segment,
-
 } from 'semantic-ui-react'
 
-// import Layout from '../Layout/LayoutComponent.jsx'
 import { getWidthFactory } from '../../utils/utils';
-
+import MyHeader from '../Header/Header.jsx'
 import { connect } from 'react-redux'
 
-const HomepageHeading = ({ mobile }) => (
- <Container text>
-  <Header
-   as='h1'
-   content='This is the home page! An app on the decline...uh about one!'
+const HomepageLayout = () => (
+ <>
+  <Segment
    inverted
-   style={{
-    fontSize: mobile ? '2em' : '4em',
-    fontWeight: 'normal',
-    marginBottom: 0,
-    marginTop: mobile ? '1.5em' : '3em',
-   }}
-  />
-  <Header
-   as='h2'
-   content='Do whatever you want when you want to.'
-   inverted
-   style={{
-    fontSize: mobile ? '1.5em' : '1.7em',
-    fontWeight: 'normal',
-    marginTop: mobile ? '0.5em' : '1.5em',
-   }}
-  />
-
-  <Button primary size='huge'>
-   Get Started
-      <Icon name='right arrow' />
-  </Button>
- </Container>
-)
-
-HomepageHeading.propTypes = {
- mobile: PropTypes.bool,
-}
-
-const HomepageLayout = ({ isMobileFromSSR, isLoggedIn, logInUser, logOutUser}) => (
-<>
-  <Segment style={{ padding: '8em 0em' }} vertical>
-   <Grid centered container stackable verticalAlign='middle'>
-    <Grid.Row>
-     <Grid.Column width={12}>
-      <Header as='h3' style={{ fontSize: '2em' }}>
-       We Help Companies and Companions
-      </Header>
-      <p style={{ fontSize: '1.33em' }}>
-       We can give your company superpowers to do things that they never thought possible.
-       Let us delight your customers and empower your needs... through pure data analytics.
-      </p>
-
-
-     </Grid.Column>
-     <Grid.Column floated='right' width={6}>
-     </Grid.Column>
-    </Grid.Row>
-    <Grid.Row>
-     <Grid.Column>
-      <Button size='huge'>Check Them Out</Button>
-     </Grid.Column>
-    </Grid.Row>
-   </Grid>
+   textAlign='center'
+   style={{ minHeight: 'auto', padding: '4.5em 0em 5em' }}
+   vertical
+  >
+  <MyHeader H1HeaderContent="foo" H2HeaderContent="bar" />
   </Segment>
 
   <Segment style={{ padding: '0em' }} vertical>
@@ -98,12 +43,11 @@ const HomepageLayout = ({ isMobileFromSSR, isLoggedIn, logInUser, logOutUser}) =
     </Grid.Row>
    </Grid>
   </Segment>
-
   <Segment style={{ padding: '8em 0em' }} vertical>
    <Container text>
     <Header as='h3' style={{ fontSize: '2em' }}>
      Breaking The Grid, Grabs Your Attention
-    </Header>
+        </Header>
     <p style={{ fontSize: '1.33em' }}>
      Instead of focusing on content creation and hard work, we have learned how to master the
      art of doing nothing by providing massive amounts of whitespace and generic content that
@@ -111,31 +55,11 @@ const HomepageLayout = ({ isMobileFromSSR, isLoggedIn, logInUser, logOutUser}) =
         </p>
     <Button as='a' size='large'>
      Read More
-    </Button>
-
-    <Divider
-     as='h4'
-     className='header'
-     horizontal
-     style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-    >
-     <a href='#'>Case Studies</a>
-    </Divider>
-
-    <Header as='h3' style={{ fontSize: '2em' }}>
-     Did We Tell You About Our Bananas?
-    </Header>
-    <p style={{ fontSize: '1.33em' }}>
-     Yes I know you probably disregarded the earlier boasts as non-sequitur filler content, but
-     it's really true. It took years of gene splicing and combinatory DNA research, but our
-     bananas can really dance.
-    </p>
-    <Button as='a' size='large'>
-     I'm Still Quite Interested
         </Button>
+
+
    </Container>
   </Segment>
-
   <Segment inverted vertical style={{ padding: '5em 0em' }}>
    <Container>
     <Grid divided inverted stackable>
@@ -171,9 +95,11 @@ const HomepageLayout = ({ isMobileFromSSR, isLoggedIn, logInUser, logOutUser}) =
    </Container>
   </Segment>
 
-</>
 
+ </>
 )
+
+
 
 function mapStateToProps(state) {
  const { isLoggedIn, logInUser, logOutUser } = state
