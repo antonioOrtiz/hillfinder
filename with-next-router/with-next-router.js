aba2f35f71f9a611b243ffc8b26b8699f5c4bdbb
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-11-18 13:47:36 -0500

    added relevant code to make app compatible with react-router-dom

--- with-next-router/with-next-router.js
+++ with-next-router/with-next-router.js
@@ -2,22 +2,63 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 const isServer = typeof window === 'undefined';
 
+// export default App => {
+//  return (
+//   class AppWithReactRouter extends React.Component {
+//    static async getInitialProps(appContext) {
+//     const {
+//      ctx: {
+//       req: {
+//        originalUrl,
+//        locals = {},
+//       },
+//      },
+//     } = appContext;
+//     return {
+//      originalUrl,
+//      context: locals.context || {},
+//     };
+//    }
+
+//    render() {
+//     if (isServer) {
+//      const { StaticRouter } = require('react-router');
+//      return (
+//       <StaticRouter
+//        location={this.props.originalUrl}
+//        context={this.props.context}
+//       >
+//        <App {...this.props} />
+//       </StaticRouter>
+//      );
+//     }
+//     return (
+//      <BrowserRouter>
+//       <App {...this.props} />
+//      </BrowserRouter>
+//     );
+//    }
+//   }
+//  )
+// };
+
+
 export default App => {
  return class AppWithReactRouter extends React.Component {
   static async getInitialProps(appContext) {
-   const {
-    ctx: {
-     req: {
-      originalUrl,
-      locals = {},
+    const {
+     ctx: {
+      req: {
+       originalUrl,
+       locals = {},
+      },
      },
-    },
-   } = appContext;
-   return {
-    originalUrl,
-    context: locals.context || {},
-   };
-  }
+    } = appContext;
+    return {
+     originalUrl,
+     context: locals.context || {},
+    };
+ }
 
   render() {
    if (isServer) {
@@ -26,7 +67,7 @@ export default App => {
      <StaticRouter
       location={this.props.originalUrl}
       context={this.props.context}
-     >
+      >
       <App {...this.props} />
      </StaticRouter>
     );
