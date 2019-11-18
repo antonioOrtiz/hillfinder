5ad4880a65550658715a7a8631cfb29db9a35cb5
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-11-18 13:33:20 -0500

    removed unused and unnecessary code

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,23 +1,11 @@
 import 'semantic-ui-css/semantic.min.css'
 import ProfilePage from '../components/Profile/ProfilePage.jsx'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import {  logOutUser } from '../store'
 import { withRouter } from "react-router-dom";
 
-const Profile = ({ isLoggedIn, logOutUser, props}) => (
-    <>
-      <ProfilePage isLoggedIn={isLoggedIn} logOutUser={logOutUser} {...props} />
-    </>
- )
+const Profile = (props) => (<>
+ <ProfilePage {...props} />
+</>)
 
-function mapStateToProps(state) {
- const { isLoggedIn, logOutUser } = state
- return { isLoggedIn, logOutUser }
-}
+export default withRouter(Profile)
 
-const mapDispatchToProps = dispatch =>
- bindActionCreators({logOutUser }, dispatch)
-
-export default connect(mapStateToProps, mapDispatchToProps)(withRouter(Profile));
 
