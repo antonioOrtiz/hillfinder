ddccb18fcabb6bc28f0c447ae05e654c344c8ed4
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-11-18 12:43:38 -0500

    removed unused and unnecessary code

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,38 +1,24 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import {
+ withRouter
+} from 'react-router-dom'
 import Head from '../components/head'
 import HomePage from '../components/Home/Home.jsx'
 import 'semantic-ui-css/semantic.min.css'
 import '../styles/styles.scss'
-import { logInUser, logOutUser } from '../store'
-import { bindActionCreators } from 'redux'
 
 class Home extends React.Component {
- static getInitialProps({ isLoggedIn, logInUser, logOutUser}) {
-
-  return { isLoggedIn, logInUser, logOutUser}
- }
-
     render() {
-     const { isLoggedIn, logInUser, logOutUser } = this.props
+     console.log("pages/home this.props ", this.props);
+
          return (
           <>
            <Head title = 'Home' />
-           <HomePage isLoggedIn={isLoggedIn} logInUser={logInUser} logOutUser={logOutUser}/>
+           <HomePage/>
           </>
          )
     }
 }
 
-function mapStateToProps(state) {
- const { isLoggedIn, logInUser, logOutUser } = state
- return { isLoggedIn, logInUser, logOutUser  }
-}
-const mapDispatchToProps = dispatch =>
- bindActionCreators({ logInUser, logOutUser }, dispatch)
-
-export default connect(
- mapStateToProps,
- mapDispatchToProps
-)(Home)
+export default withRouter(Home)
 
