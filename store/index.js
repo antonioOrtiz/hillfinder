3f018f473d087aad89b28b284fa637e0be6cca65
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-12-03 17:37:18 -0500

    formatting adjustments

--- store/index.js
+++ store/index.js
@@ -23,21 +23,21 @@ export const actionTypes = {
 export const reducer = (state = startState, action) => {
     switch (action.type) {
         case actionTypes.IS_LOGGED_IN:
-          return Object.assign({}, state, {
-            isLoggedIn: true,
-          });
+            return Object.assign({}, state, {
+                isLoggedIn: true,
+            });
         case actionTypes.IS_LOGGED_OUT:
-          return Object.assign({}, state, {
-            isLoggedIn: false,
-          });
+            return Object.assign({}, state, {
+                isLoggedIn: false,
+            });
         case actionTypes.MODAL_ACTIVE:
-          return Object.assign({}, state, {
-            modalActive: true
-          });
+            return Object.assign({}, state, {
+                modalActive: true
+            });
         case actionTypes.MODAL_INACTIVE:
-          return Object.assign({}, state, {
-            modalActive: false
-      });
+            return Object.assign({}, state, {
+                modalActive: false
+            });
         default:
             return state
     }
@@ -51,48 +51,43 @@ export const logOutUser = () => {
     return { type: actionTypes.IS_LOGGED_OUT }
 }
 export const modalStateOn = () => {
- return { type: actionTypes.MODAL_ACTIVE, modalActive: true}
+    return { type: actionTypes.MODAL_ACTIVE, modalActive: true }
 }
 
 export const modalStateOff = () => {
- return { type: actionTypes.MODAL_INACTIVE, modalActive: false }
+    return { type: actionTypes.MODAL_INACTIVE, modalActive: false }
 }
 
-
-
-
 export default () => {
- let store;
- const isClient = typeof window !== 'undefined';
- if (isClient) {
-  const { persistReducer } = require('redux-persist');
-  const storage = require('redux-persist/lib/storage').default;
-  const persistConfig = {
-   key: 'primary',
-   storage,
-   whitelist: ['isLoggedIn', 'modalActive'], // place to select which state you want to persist
+    let store;
+    const isClient = typeof window !== 'undefined';
+    if (isClient) {
+        const { persistReducer } = require('redux-persist');
+        const storage = require('redux-persist/lib/storage').default;
+        const persistConfig = {
+            key: 'primary',
+            storage,
+            whitelist: ['isLoggedIn', 'modalActive'], // place to select which state you want to persist
 
-  }
-  store = createStore(
-   persistReducer(persistConfig, reducer),
-   startState,
-     composeWithDevTools(applyMiddleware(
-            thunkMiddleware,
-            createLogger({ collapsed: false })
-        ))
-  );
-  store.__PERSISTOR = persistStore(store);
- } else {
-  store = createStore(
-   reducer,
-   startState,
-     composeWithDevTools(applyMiddleware(
-            thunkMiddleware,
-            createLogger({ collapsed: false })
-        ))
-  );
- }
- return store;
+        }
+        store = createStore(
+            persistReducer(persistConfig, reducer),
+            startState,
+            composeWithDevTools(applyMiddleware(
+                thunkMiddleware,
+                createLogger({ collapsed: false })
+            ))
+        );
+        store.__PERSISTOR = persistStore(store);
+    } else {
+        store = createStore(
+            reducer,
+            startState,
+            composeWithDevTools(applyMiddleware(
+                thunkMiddleware,
+                createLogger({ collapsed: false })
+            ))
+        );
+    }
+    return store;
 };
-
-
