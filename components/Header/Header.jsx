6752192d92106f65b129985d4145507693c16c18
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-12-07 12:12:37 -0500

    tweaked custom header component

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,13 +3,15 @@ import { Header } from 'semantic-ui-react'
 
 const MyHeader = ({ mobile, content, textAlign}) => (
   <Header
+   as='h1'
    content={content}
    textAlign={textAlign}
+   inverted
    style={{
-    fontSize: mobile ? '.5em' : '1.7em',
+    fontSize: mobile ? '.5em' : '2.5em',
     fontWeight: 'normal',
     padding: mobile ? '0' : '0',
-    margin: mobile ? '1em 0 0 0' : '1em 0 0 0'
+    margin: mobile ? '1em 0 0 0' : '0',
    }}
   />
 )
