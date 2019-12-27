4b21beb3a86382117622bffa71983d68fd74b044
antonioOrtiz <17antonio.ortiz@gmail.com>
2019-12-27 11:52:15 -0500

    broke up reducers to categories i.e. users, ui etc

--- store/index.js
+++ store/index.js
@@ -1,59 +1,27 @@
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux'
 
-import { composeWithDevTools } from 'redux-devtools-extension';
+/* imported initialStates */
+// import { usersStartState } from './reducers/users/index'
+// import { uiStartState } from './reducers/ui/index'
+
+/* imported reducers */
+import ui from './reducers/ui/index'
+import users from './reducers/users/index'
+
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { persistStore } from 'redux-persist';
 
 import { createLogger } from 'redux-logger'
-import thunkMiddleware from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk'
 
-/* initial state */
-const startState = { isLoggedIn: false, modalActive: false }
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
-/* action types */
-export const actionTypes = {
-    IS_LOGGED_IN: 'IS_LOGGED_IN',
-    IS_LOGGED_OUT: 'IS_LOGGED_OUT',
-    MODAL_ACTIVE: 'MODAL_ACTIVE',
-    MODAL_INACTIVE: 'MODAL_INACTIVE',
-}
+var rootReducer = combineReducers({
+    users: users,
+    ui: ui
+})
 
-/* reducer(s) */
-export const reducer = (state = startState, action) => {
-    switch (action.type) {
-        case actionTypes.IS_LOGGED_IN:
-            return Object.assign({}, state, {
-                isLoggedIn: true,
-            });
-        case actionTypes.IS_LOGGED_OUT:
-            return Object.assign({}, state, {
-                isLoggedIn: false,
-            });
-        case actionTypes.MODAL_ACTIVE:
-            return Object.assign({}, state, {
-                modalActive: true
-            });
-        case actionTypes.MODAL_INACTIVE:
-            return Object.assign({}, state, {
-                modalActive: false
-            });
-        default:
-            return state
-    }
-};
-
-/* actions */
-export const logInUser = () => {
-    return { type: actionTypes.IS_LOGGED_IN }
-}
-export const logOutUser = () => {
-    return { type: actionTypes.IS_LOGGED_OUT }
-}
-export const modalStateOn = () => {
-    return { type: actionTypes.MODAL_ACTIVE }
-}
-export const modalStateOff = () => {
-    return { type: actionTypes.MODAL_INACTIVE }
-}
+// var startState = Object.assign({}, uiStartState, usersStartState)
 
 export default () => {
     let store;
@@ -62,14 +30,15 @@ export default () => {
         const { persistReducer } = require('redux-persist');
         const storage = require('redux-persist/lib/storage').default;
         const persistConfig = {
-            key: 'primary',
+            key: 'root',
             storage,
-            whitelist: ['isLoggedIn', 'modalActive'], // place to select which state you want to persist
+            stateReconciler: autoMergeLevel2,
+
+            whitelist: ['users', 'ui'], // place to select which state you want to persist
 
         }
         store = createStore(
-            persistReducer(persistConfig, reducer),
-            startState,
+            persistReducer(persistConfig, rootReducer),
             composeWithDevTools(applyMiddleware(
                 thunkMiddleware,
                 createLogger({ collapsed: false })
@@ -78,8 +47,7 @@ export default () => {
         store.__PERSISTOR = persistStore(store);
     } else {
         store = createStore(
-            reducer,
-            startState,
+            rootReducer,
             composeWithDevTools(applyMiddleware(
                 thunkMiddleware,
                 createLogger({ collapsed: false })
