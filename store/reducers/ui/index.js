499913000e9c0364155151a946961f2f4c9d8785
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-02-22 09:21:41 -0500

    MODAL actions added

--- store/reducers/ui/index.js
+++ store/reducers/ui/index.js
@@ -1,32 +1,48 @@
 /* initial state */
-export var uiStartState = { modalActive: false }
+export var uiStartState = { modalActive: false, avatarModalActive: false, }
 
 /* action types */
 export const actionTypes = {
-    MODAL_ACTIVE: 'MODAL_ACTIVE',
-    MODAL_INACTIVE: 'MODAL_INACTIVE'
+  MODAL_ACTIVE: 'MODAL_ACTIVE',
+  MODAL_INACTIVE: 'MODAL_INACTIVE',
+  AVATAR_MODAL_ACTIVE: 'AVATAR_MODAL_ACTIVE',
+  AVATAR_MODAL_INACTIVE: 'AVATAR_MODAL_INACTIVE',
 }
 
 /* reducer(s) */
 export default function ui(state = uiStartState, action) {
-    switch (action.type) {
-        case actionTypes.MODAL_ACTIVE:
+  switch (action.type) {
 
-            return Object.assign({}, state, { modalActive: true });
+    case actionTypes.MODAL_ACTIVE:
+      return Object.assign({}, state, { modalActive: true });
 
-        case actionTypes.MODAL_INACTIVE:
+    case actionTypes.MODAL_INACTIVE:
+      return Object.assign({}, state, { modalActive: false });
 
-            return Object.assign({}, state, { modalActive: false });
+    case actionTypes.AVATAR_MODAL_ACTIVE:
+      return Object.assign({}, state, { avatarModalActive: true });
 
-        default:
-            return state
-    }
+    case actionTypes.AVATAR_MODAL_INACTIVE:
+      return Object.assign({}, state, { avatarModalActive: false });
+
+    default:
+      return state
+  }
 };
 
 /* actions */
 export const modalStateOn = () => {
-    return { type: actionTypes.MODAL_ACTIVE }
+  return { type: actionTypes.MODAL_ACTIVE }
 }
+
 export const modalStateOff = () => {
-    return { type: actionTypes.MODAL_INACTIVE }
+  return { type: actionTypes.MODAL_INACTIVE }
+}
+
+export const avatarModalStateOn = () => {
+  return { type: actionTypes.AVATAR_MODAL_ACTIVE }
+}
+
+export const avatarModalStateOff = () => {
+  return { type: actionTypes.AVATAR_MODAL_INACTIVE }
 }
