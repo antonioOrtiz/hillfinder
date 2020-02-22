75b13963ba8db61708a126036fdebc19cc3dca72
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-02-22 09:18:23 -0500

    created Token Model

---
+++ server/models/TokenSchema.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-var */
+var mongoose = require('mongoose');
+
+var TokenSchema = new mongoose.Schema({
+  _userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User' },
+  token: { type: String, required: true },
+  createdAt: { type: Date, required: true, default: Date.now, expires: 43200 }
+});
+
+module.exports = mongoose.model('Token', TokenSchema);
