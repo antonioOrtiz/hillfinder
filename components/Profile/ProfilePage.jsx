6b2af975d64c5d0eb38870970ea96399fdf4d6e1
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-03-25 13:38:12 -0400

    Added avatar data and other data for user

--- components/Profile/ProfilePage.jsx
+++ components/Profile/ProfilePage.jsx
@@ -1,252 +1,254 @@
-import { Button, Card, Feed, Icon, Image, Segment, Grid, Divider, Container, Header } from 'semantic-ui-react'
+import {
+  Button,
+  Card,
+  Feed,
+  Icon,
+  Image,
+  Segment,
+  Grid,
+  Divider,
+  Container,
+  Header
+} from 'semantic-ui-react';
 
-import MyHeader from '../Header/Header.jsx'
+import MyHeader from '../Header/Header.jsx';
 import ImageUploader from '../ImageUploader/ImageUploader.jsx';
 import React, { useState } from 'react';
 
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { loadAvatar } from '../../store/reducers/users/index'
-
+import { connect } from 'react-redux';
 
 const style = {
   h1: {
-    marginTop: '3em',
+    marginTop: '3em'
   },
   h2: {
-    margin: '4em 0em 2em',
+    margin: '4em 0em 2em'
   },
   h3: {
     marginTop: '1em',
-    padding: '0',
+    padding: '0'
   },
   last: {
-    marginBottom: '300px',
-  },
-}
+    marginBottom: '300px'
+  }
+};
 
 const ProfilePage = ({ history, isMobileFromSSR, ...props }) => {
-  var {userAvatar} = props
- return (
-  <>
- {console.log('Profile Page!! ', props)}
-     <Grid container columns={1} relaxed stackable>
-      <Grid.Column>
-        <MyHeader as='h2' content='Foo' textAlign='left' />
-      </Grid.Column>
-    </Grid>
+  var { userAvatar } = props;
+  return (
+    <>
+      {console.log('Profile Page!! ', props)}
+      <Grid container columns={1} relaxed stackable>
+        <Grid.Column>
+          <MyHeader as="h2" content="Foo" textAlign="left" />
+        </Grid.Column>
+      </Grid>
 
-    <Grid container columns={2} divided relaxed stackable>
-      <Grid.Column>
-        <Segment>
-         <Card fluid>
-          <Image src='static/profile-avatars/charly_desktop.jpg' wrapped ui={false} />
-          <ImageUploader userAvatar={userAvatar} history={history} />
-          <Card.Content>
-            <Card.Header>Charly</Card.Header>
-            <Card.Meta>
-            <span className='date'>Joined in 2015</span>
-            </Card.Meta>
-            <Card.Description>
-            Charly
-            </Card.Description>
-            </Card.Content>
-          <Card.Content extra>
-          <a>
-           <Icon name='user' />
-           22 Friends
-          </a>
-        </Card.Content>
-       </Card>
-      </Segment>
-      </Grid.Column>
-      <Grid.Column>
-        <Segment>
-         <Card fluid>
-          <Card.Content>
-            <Card.Header>Recent Activity</Card.Header>
-          </Card.Content>
-          <Card.Content>
+      <Grid container columns={2} divided relaxed stackable>
+        <Grid.Column>
+          <Segment>
+            <Card fluid>
+              <Image src="static/profile-avatars/charly_desktop.jpg" wrapped ui={false} />
+              <ImageUploader userAvatar={userAvatar} history={history} />
+              <Card.Content>
+                <Card.Header>Charly</Card.Header>
+                <Card.Meta>
+                  <span className="date">Joined in 2015</span>
+                </Card.Meta>
+                <Card.Description>Charly</Card.Description>
+              </Card.Content>
+              <Card.Content extra>
+                <a>
+                  <Icon name="user" />
+                  22 Friends
+                </a>
+              </Card.Content>
+            </Card>
+          </Segment>
+        </Grid.Column>
+        <Grid.Column>
+          <Segment>
+            <Card fluid>
+              <Card.Content>
+                <Card.Header>Recent Activity</Card.Header>
+              </Card.Content>
+              <Card.Content>
+                <Feed>
+                  <Feed.Event>
+                    <Feed.Label image="/images/avatar/small/jenny.jpg" />
+                    <Feed.Content>
+                      <Feed.Date content="1 day ago" />
+                      <Feed.Summary>
+                        You added <a>Jenny Hess</a> to your <a>coworker</a> group.
+                      </Feed.Summary>
+                    </Feed.Content>
+                  </Feed.Event>
+
+                  <Feed.Event>
+                    <Feed.Label image="/images/avatar/small/molly.png" />
+                    <Feed.Content>
+                      <Feed.Date content="3 days ago" />
+                      <Feed.Summary>
+                        You added <a>Molly Malone</a> as a friend.
+                      </Feed.Summary>
+                    </Feed.Content>
+                  </Feed.Event>
+
+                  <Feed.Event>
+                    <Feed.Label image="/images/avatar/small/elliot.jpg" />
+                    <Feed.Content>
+                      <Feed.Date content="4 days ago" />
+                      <Feed.Summary>
+                        You added <a>Elliot Baker</a> to your <a>musicians</a> group.
+                      </Feed.Summary>
+                    </Feed.Content>
+                  </Feed.Event>
+                </Feed>
+              </Card.Content>
+            </Card>
+          </Segment>
+
+          <Segment>
             <Feed>
               <Feed.Event>
-                <Feed.Label image='/images/avatar/small/jenny.jpg' />
+                <Feed.Label>
+                  <img src="/images/avatar/small/elliot.jpg" />
+                </Feed.Label>
                 <Feed.Content>
-                  <Feed.Date content='1 day ago' />
                   <Feed.Summary>
-                    You added <a>Jenny Hess</a> to your <a>coworker</a> group.
+                    <Feed.User>Elliot Fu</Feed.User> added you as a friend
+                    <Feed.Date>1 Hour Ago</Feed.Date>
                   </Feed.Summary>
+                  <Feed.Meta>
+                    <Feed.Like>
+                      <Icon name="like" />4 Likes
+                    </Feed.Like>
+                  </Feed.Meta>
                 </Feed.Content>
               </Feed.Event>
 
               <Feed.Event>
-                <Feed.Label image='/images/avatar/small/molly.png' />
+                <Feed.Label image="/images/avatar/small/helen.jpg" />
                 <Feed.Content>
-                  <Feed.Date content='3 days ago' />
                   <Feed.Summary>
-                    You added <a>Molly Malone</a> as a friend.
+                    <a>Helen Troy</a> added <a>2 new illustrations</a>
+                    <Feed.Date>4 days ago</Feed.Date>
                   </Feed.Summary>
+                  <Feed.Extra images>
+                    <a>
+                      <img src="/images/wireframe/image.png" />
+                    </a>
+                    <a>
+                      <img src="/images/wireframe/image.png" />
+                    </a>
+                  </Feed.Extra>
+                  <Feed.Meta>
+                    <Feed.Like>
+                      <Icon name="like" />1 Like
+                    </Feed.Like>
+                  </Feed.Meta>
                 </Feed.Content>
               </Feed.Event>
 
               <Feed.Event>
-                <Feed.Label image='/images/avatar/small/elliot.jpg' />
+                <Feed.Label image="/images/avatar/small/jenny.jpg" />
                 <Feed.Content>
-                  <Feed.Date content='4 days ago' />
+                  <Feed.Summary
+                    date="2 Days Ago"
+                    user="Jenny Hess"
+                    content="add you as a friend"
+                  />
+                  <Feed.Meta>
+                    <Feed.Like>
+                      <Icon name="like" />8 Likes
+                    </Feed.Like>
+                  </Feed.Meta>
+                </Feed.Content>
+              </Feed.Event>
+
+              <Feed.Event>
+                <Feed.Label image="/images/avatar/small/joe.jpg" />
+                <Feed.Content>
                   <Feed.Summary>
-                    You added <a>Elliot Baker</a> to your <a>musicians</a> group.
+                    <a>Joe Henderson</a> posted on his page
+                    <Feed.Date>3 days ago</Feed.Date>
                   </Feed.Summary>
+                  <Feed.Extra text>
+                    Ours is a life of constant reruns. We're always circling back to where
+                    we'd we started, then starting all over again. Even if we don't run
+                    extra laps that day, we surely will come back for more of the same
+                    another day soon.
+                  </Feed.Extra>
+                  <Feed.Meta>
+                    <Feed.Like>
+                      <Icon name="like" />5 Likes
+                    </Feed.Like>
+                  </Feed.Meta>
+                </Feed.Content>
+              </Feed.Event>
+
+              <Feed.Event>
+                <Feed.Label image="/images/avatar/small/justen.jpg" />
+                <Feed.Content>
+                  <Feed.Summary>
+                    <a>Justen Kitsune</a> added <a>2 new photos</a> of you
+                    <Feed.Date>4 days ago</Feed.Date>
+                  </Feed.Summary>
+                  <Feed.Extra images>
+                    <a>
+                      <img src="/images/wireframe/image.png" />
+                    </a>
+                    <a>
+                      <img src="/images/wireframe/image.png" />
+                    </a>
+                  </Feed.Extra>
+                  <Feed.Meta>
+                    <Feed.Like>
+                      <Icon name="like" />
+                      41 Likes
+                    </Feed.Like>
+                  </Feed.Meta>
                 </Feed.Content>
               </Feed.Event>
             </Feed>
-          </Card.Content>
-       </Card>
+          </Segment>
+        </Grid.Column>
+      </Grid>
+
+      <Divider
+        as="h4"
+        className="header"
+        horizontal
+        style={{ margin: '3em 0em', textTransform: 'uppercase' }}
+      >
+        <a href="#">Case Studies</a>
+      </Divider>
+
+      <Segment style={{ padding: '8em 0em' }} vertical>
+        <Container text>
+          <Header as="h3" style={{ fontSize: '2em' }}>
+            Breaking The Grid, Grabs Your Attention
+          </Header>
+          <p style={{ fontSize: '1.33em' }}>
+            Instead of focusing on content creation and hard work, we have learned how to
+            master the art of doing nothing by providing massive amounts of whitespace and
+            generic content that can seem massive, monolithic and worth your attention.
+          </p>
+          <Button as="a" size="large">
+            Read More
+          </Button>
+        </Container>
       </Segment>
-
-      <Segment>
-         <Feed>
-           <Feed.Event>
-             <Feed.Label>
-               <img src='/images/avatar/small/elliot.jpg' />
-             </Feed.Label>
-             <Feed.Content>
-               <Feed.Summary>
-                 <Feed.User>Elliot Fu</Feed.User> added you as a friend
-                 <Feed.Date>1 Hour Ago</Feed.Date>
-               </Feed.Summary>
-               <Feed.Meta>
-                 <Feed.Like>
-                   <Icon name='like' />4 Likes
-                 </Feed.Like>
-               </Feed.Meta>
-             </Feed.Content>
-           </Feed.Event>
-
-           <Feed.Event>
-             <Feed.Label image='/images/avatar/small/helen.jpg' />
-             <Feed.Content>
-               <Feed.Summary>
-                 <a>Helen Troy</a> added <a>2 new illustrations</a>
-                 <Feed.Date>4 days ago</Feed.Date>
-               </Feed.Summary>
-               <Feed.Extra images>
-                 <a>
-                   <img src='/images/wireframe/image.png' />
-                 </a>
-                 <a>
-                   <img src='/images/wireframe/image.png' />
-                 </a>
-               </Feed.Extra>
-               <Feed.Meta>
-                 <Feed.Like>
-                   <Icon name='like' />1 Like
-                 </Feed.Like>
-               </Feed.Meta>
-             </Feed.Content>
-           </Feed.Event>
-
-           <Feed.Event>
-             <Feed.Label image='/images/avatar/small/jenny.jpg' />
-             <Feed.Content>
-               <Feed.Summary
-                 date='2 Days Ago'
-                 user='Jenny Hess'
-                 content='add you as a friend'
-               />
-               <Feed.Meta>
-                 <Feed.Like>
-                   <Icon name='like' />8 Likes
-                 </Feed.Like>
-               </Feed.Meta>
-             </Feed.Content>
-           </Feed.Event>
-
-           <Feed.Event>
-             <Feed.Label image='/images/avatar/small/joe.jpg' />
-             <Feed.Content>
-               <Feed.Summary>
-                 <a>Joe Henderson</a> posted on his page
-                 <Feed.Date>3 days ago</Feed.Date>
-               </Feed.Summary>
-               <Feed.Extra text>
-                 Ours is a life of constant reruns. We're always circling back to where
-                 we'd we started, then starting all over again. Even if we don't run
-                 extra laps that day, we surely will come back for more of the same
-                 another day soon.
-               </Feed.Extra>
-               <Feed.Meta>
-                 <Feed.Like>
-                   <Icon name='like' />5 Likes
-                 </Feed.Like>
-               </Feed.Meta>
-             </Feed.Content>
-           </Feed.Event>
-
-           <Feed.Event>
-             <Feed.Label image='/images/avatar/small/justen.jpg' />
-             <Feed.Content>
-               <Feed.Summary>
-                 <a>Justen Kitsune</a> added <a>2 new photos</a> of you
-                 <Feed.Date>4 days ago</Feed.Date>
-               </Feed.Summary>
-               <Feed.Extra images>
-                 <a>
-                   <img src='/images/wireframe/image.png' />
-                 </a>
-                 <a>
-                   <img src='/images/wireframe/image.png' />
-                 </a>
-               </Feed.Extra>
-               <Feed.Meta>
-                 <Feed.Like>
-                   <Icon name='like' />
-                   41 Likes
-                 </Feed.Like>
-               </Feed.Meta>
-             </Feed.Content>
-           </Feed.Event>
-         </Feed>
-       </Segment>
-     </Grid.Column>
-    </Grid>
-
-     <Divider
-      as='h4'
-      className='header'
-      horizontal
-      style={{ margin: '3em 0em', textTransform: 'uppercase' }}
-     >
-      <a href='#'>Case Studies</a>
-     </Divider>
-
-   <Segment style={{ padding: '8em 0em' }} vertical>
-    <Container text>
-     <Header as='h3' style={{ fontSize: '2em' }}>
-      Breaking The Grid, Grabs Your Attention
-     </Header>
-     <p style={{ fontSize: '1.33em' }}>
-      Instead of focusing on content creation and hard work, we have learned how to master the
-      art of doing nothing by providing massive amounts of whitespace and generic content that
-      can seem massive, monolithic and worth your attention.
-      </p>
-     <Button as='a' size='large'>
-      Read More
-     </Button>
-    </Container>
-   </Segment>
-   <Segment inverted vertical style={{ padding: '5em 0em' }}></Segment>
- </>
- )
-
- }
+      <Segment inverted vertical style={{ padding: '5em 0em' }} />
+    </>
+  );
+};
 
 function mapStateToProps(state) {
- const {  users } = state
- const { userAvatar } = users
+  const { users } = state;
+  const { userAvatar } = users;
 
- return { userAvatar }
+  return { userAvatar };
 }
 
-// const mapDispatchToProps = dispatch =>
-//  bindActionCreators({ modalStateOn, modalStateOff, loadAvatar }, dispatch)
-
-export default connect(mapStateToProps)(ProfilePage)
+export default connect(mapStateToProps)(ProfilePage);
