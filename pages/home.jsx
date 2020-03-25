873cf46345d34d57fe53a14367440dde1acfb869
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-03-25 13:25:35 -0400

    regactoring of code

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import {
- withRouter
-} from 'react-router-dom'
-import Head from '../components/head'
-import HomePage from '../components/Home/Home.jsx'
-import 'semantic-ui-css/semantic.min.css'
-import '../styles/styles.scss'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import Head from '../components/head';
+import HomePage from '../components/HomePage/HomePage.jsx';
+import 'semantic-ui-css/semantic.min.css';
+import '../styles/styles.scss';
 
 class Home extends React.Component {
-    render() {
-         return (
-          <>
-           <Head title = 'Home' />
-           <HomePage/>
-          </>
-         )
-    }
+  render() {
+    return (
+      <>
+        <Head title="Home" />
+        <HomePage />
+      </>
+    );
+  }
 }
 
-export default withRouter(Home)
-
+export default withRouter(Home);
