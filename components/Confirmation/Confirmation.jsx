87060009d883d2b26e5466b0212ca402f75d0b4e
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-03-25 13:12:18 -0400

    Transformed component from a class component to functional component to try Hooks! And it works!

--- components/Confirmation/Confirmation.jsx
+++ components/Confirmation/Confirmation.jsx
@@ -1,96 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Loader, Dimmer, Transition, Message } from 'semantic-ui-react';
-import axios from 'axios';
 
+// import { confirmUser } from '../../utils/index';
 import { hasBeenVerified } from '../../store/reducers/users/index';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-class Confirmation extends Component {
-  _isMounted = false;
+function Confirmation({ match, isAccountVerified, hasBeenVerified }) {
+  var [duration, setDuration] = useState(500);
+  var [responseMessage, setResponseMessage] = useState({});
+  var [error, setError] = useState(false);
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      duration: 500,
-      confirmedParam: false,
-      responseMessage: {},
-      error: false
-    };
-  }
-
-  componentDidMount() {
-    this._isMounted = true;
-
-    var { token } = this.props.match.params;
-    var { hasBeenVerified } = this.props;
+  useEffect(() => {
     axios
-      .get(`http://localhost:8016/users/confirmation/${token}`)
+      .get(`http://localhost:8016/users/confirmation/${match.params.token}`)
       .then(response => {
-        console.log('response', response);
         if (response.status === 200) {
           hasBeenVerified();
-          this.setState({
-            responseMessage: response.data.msg
-          });
-          return;
+          setResponseMessage(response.data.msg);
         }
-
-        // this.setState(() => ({ confirmedParam }));
       })
-      .catch(
-        function(error) {
-          if (error.response.status === 404) {
-            this.setState({
-              responseMessage: error.response.data.msg,
-              error: true
-            });
-            return;
-          }
-          if (error.response.status === 400) {
-            this.setState({
-              responseMessage: error.response.data.msg,
-              error: true
-            });
-            return;
-          }
-        }.bind(this)
-      );
-  }
+      .catch(function(error) {
+        if (error.response.status === 404) {
+          setResponseMessage(error.response.data.msg);
+          setError(true);
+        }
+        if (error.response.status === 400) {
+          setResponseMessage(error.response.data.msg);
+          setError(true);
+        }
+      });
+  }, []);
 
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-
-  render() {
-    var { responseMessage, duration, error } = this.state;
-    console.log('responseMessage ', responseMessage);
-
-    console.log('this.props ', this.props);
-    var { isAccountVerified } = this.props;
-    console.log('isAccountVerified ', isAccountVerified);
-    return (
-      <div className="login-form">
-        <Transition
-          visible={isAccountVerified}
-          unmountOnHide={true}
-          animation="scale"
-          duration={duration}
-        >
-          {!isAccountVerified ? (
-            <Dimmer active inverted>
-              <Loader />
-            </Dimmer>
-          ) : (
-            <Message success={!error} error={error} header={responseMessage[0]} />
-          )}
-        </Transition>
-      </div>
-    );
-  }
+  console.log('isAccountVerified ', isAccountVerified);
+  console.log('match', match);
+  console.log('responseMessage', responseMessage);
+  return (
+    <div className="login-form">
+      <Transition
+        visible={isAccountVerified}
+        unmountOnHide={true}
+        animation="scale"
+        duration={duration}
+      >
+        {!isAccountVerified ? (
+          <Dimmer active inverted>
+            <Loader />
+          </Dimmer>
+        ) : (
+          <Message success={!error} error={error} header={responseMessage[0]} />
+        )}
+      </Transition>
+    </div>
+  );
 }
 
 function mapStateToProps(state) {
