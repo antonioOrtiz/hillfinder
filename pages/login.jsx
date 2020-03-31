6337f17529e431fb2a2ba098738c8fee0f358283
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-03-31 15:48:32 -0400

    swapped in generic component for Login!!!

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,10 +1,11 @@
-import 'semantic-ui-css/semantic.min.css'
-import LoginForm from '../components/Login/LoginForm.jsx'
-import { withRouter } from "react-router-dom";
+import 'semantic-ui-css/semantic.min.css';
+import LoginForm from '../components/FormComponent/FormComponent.jsx';
+import { withRouter } from 'react-router-dom';
 
-const Login = (props) => (<>
- <LoginForm {...props}/>
-</>)
+const Login = props => (
+  <>
+    <LoginForm formType={'Login'} {...props} />
+  </>
+);
 
-export default withRouter(Login)
-
+export default withRouter(Login);
