880a910782695d12eb7faf8e792327fca56b2a32
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-04-05 19:26:48 -0400

    created new updated component from generic component

---
+++ pages/updatePassword.jsx
@@ -0,0 +1,11 @@
+import 'semantic-ui-css/semantic.min.css';
+import UpdatePasswordForm from '../components/FormComponent/FormComponent.jsx';
+import { withRouter } from 'react-router-dom';
+
+const UpdatePassword = props => (
+  <>
+    <UpdatePasswordForm formType="UpdatePassword" {...props} />
+  </>
+);
+
+export default withRouter(UpdatePassword);
