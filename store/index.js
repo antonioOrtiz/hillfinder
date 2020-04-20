0b843718ef4d7c52901a48ff9b676b7d1caf7b41
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-04-20 14:48:49 -0400

    formatting changed

--- store/index.js
+++ store/index.js
@@ -1,23 +1,21 @@
-import { applyMiddleware, combineReducers, createStore } from 'redux'
-
-
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 
 /* imported reducers */
-import ui from './reducers/ui/index'
-import users from './reducers/users/index'
+import ui from './reducers/ui/index';
+import users from './reducers/users/index';
 
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
 
-import { createLogger } from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 var rootReducer = combineReducers({
   users: users,
   ui: ui
-})
+});
 
 // var startState = Object.assign({}, uiStartState, usersStartState)
 
@@ -32,24 +30,21 @@ export default () => {
       storage,
       stateReconciler: autoMergeLevel2,
 
-      whitelist: ['users', 'ui'], // place to select which state you want to persist
-
-    }
+      whitelist: ['users', 'ui'] // place to select which state you want to persist
+    };
     store = createStore(
       persistReducer(persistConfig, rootReducer),
-      composeWithDevTools(applyMiddleware(
-        thunkMiddleware,
-        createLogger({ collapsed: false })
-      ))
+      composeWithDevTools(
+        applyMiddleware(thunkMiddleware, createLogger({ collapsed: false }))
+      )
     );
     store.__PERSISTOR = persistStore(store);
   } else {
     store = createStore(
       rootReducer,
-      composeWithDevTools(applyMiddleware(
-        thunkMiddleware,
-        createLogger({ collapsed: false })
-      ))
+      composeWithDevTools(
+        applyMiddleware(thunkMiddleware, createLogger({ collapsed: false }))
+      )
     );
   }
   return store;
