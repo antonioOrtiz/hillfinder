9f77720959db53dc68078bf9d9d88fe6f3aa184d
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-04-29 14:34:28 -0400

    removed logs

--- server/models/Image.js
+++ server/models/Image.js
@@ -3,19 +3,18 @@ var Schema = mongoose.Schema;
 
 /* Image Schema for storing images in the mongodb database */
 
-
 var ImageSchema = new Schema({
- imageName : {
-  type: String,
-  default: "none",
-  required: true
- },
- imageData : {
-  type: String,
-  required: true
- }
-})
+  imageName: {
+    type: String,
+    default: 'none',
+    required: true
+  },
+  imageData: {
+    type: String,
+    required: true
+  }
+});
 
-var Image = mongoose.model('Image', ImageSchema)
+var Image = mongoose.model('Image', ImageSchema);
 
 module.exports = Image;
