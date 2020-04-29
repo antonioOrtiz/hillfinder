5adf469f4fa626c5b475095beb7897a713677754
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-04-29 14:27:49 -0400

    cleaned up "password_reset/:token" route and other logic

--- server/users/index.js
+++ server/users/index.js
@@ -4,7 +4,6 @@ var User = require('../models/UserModel');
 var Token = require('../models/TokenSchema');
 var crypto = require('crypto');
 var nodemailer = require('nodemailer');
-var auth = require('../lib/auth');
 require('dotenv').config();
 
 function nodeMailerFunc(user, subjectField, textField, emailType, res) {
@@ -16,8 +15,7 @@ function nodeMailerFunc(user, subjectField, textField, emailType, res) {
   // Save the token
   token.save(function(err) {
     if (err) {
-      res.status(500).send({ msg: err.message });
-      return;
+      return res.status(500).send({ msg: err.message });
     }
 
     // Send the email
@@ -45,13 +43,7 @@ function nodeMailerFunc(user, subjectField, textField, emailType, res) {
       if (err) {
         return res.status(500).send({ msg: err.message });
       }
-      res.status(201).send(`A ${emailType} has been sent to ${user.username}`);
-      // return res.status(201).send({
-      //   msg: [
-      //     'Your user registration was successful.',
-      //     'Please check your email to complete your registration!'
-      //   ]
-      // });
+      return res.status(201).send(`A ${emailType} has been sent to ${user.username}`);
     });
   });
 }
@@ -91,7 +83,6 @@ router.route('/login').post((req, res, next) => {
 
 router.route('/registration').post((req, res, next) => {
   User.findOne({ username: req.body.username }, function(err, user) {
-    console.log('user ', user);
     if (user) {
       return res.status(409).send({
         msg: [
@@ -100,10 +91,12 @@ router.route('/registration').post((req, res, next) => {
         ]
       });
     }
+
     user = new User({
       username: req.body.username,
       password: req.body.password
     });
+
     user.save(function(err) {
       if (err) {
         return res.status(401).send({
@@ -134,10 +127,10 @@ router.route('/registration').post((req, res, next) => {
 });
 
 router.route('/confirmation/:token').get((req, res, next) => {
-  var usersToken = req.params.token;
+  var { token } = req.params;
 
   try {
-    Token.findOne({ token: usersToken }, function(err, token) {
+    Token.findOne({ token: token }, function(err, token) {
       if (token === null) {
         console.log('We were unable to find a valid token 404 ', 404);
         return res.status(404).send({
@@ -148,28 +141,21 @@ router.route('/confirmation/:token').get((req, res, next) => {
 
       if (token) {
         User.findOne({ _id: token._userId }, function(err, user) {
-          console.log('user 151 ', user);
           if (!user) {
-            console.log("'We were unable to find a user for this token.' ", 404);
             return res.status(404).send({
               msg: ['We were unable to find a user for this token.']
             });
           } else if (user.isVerified) {
-            console.log('User has already been verified ', 400);
             return res.status(400).send({
               msg: ['This user has already been verified.']
             });
           } else if (!user.isVerified) {
             // Verify and save the user
             user.isVerified = true;
-            console.log('user 165', user);
-
             user.update({ isVerified: true }, function(err) {
               if (err) {
-                console.log('User save error 500  167', 500);
                 return res.status(500).send({ msg: [err.message] });
               }
-              console.log('The account has been verified. Please log in. 171', 200);
             });
             return res.status(200).send({
               msg: ['The account has been verified. Please log in!']
@@ -188,35 +174,31 @@ router.route('/forgot_password').post((req, res) => {
   try {
     User.findOne({ username: req.body.username }, function(err, user) {
       if (!user) {
-        res.status(404).send({
+        return res.status(404).send({
           msg: [
             'We were unable to find this user.',
             'Please re-enter another email address, or click the link below to register.'
           ]
         });
-        return;
+      } else if (user) {
+        user.generatePasswordReset();
+        user.save(() => {
+          nodeMailerFunc(
+            user,
+            `Your password has been reset`,
+            `Click the following link to reset your password:\nhttp://${
+              req.headers.host
+            }/update_password`,
+            'email to update your password'
+          );
+        });
+        return res.status(200).send({
+          msg: [
+            'Your password has been reset!',
+            'Please check your email for the link which will allow you to reset your password!'
+          ]
+        });
       }
-
-      user.generatePasswordReset();
-      user.save(err => {
-        if (err) {
-          return res.status(500).send({ msg: err.message });
-        }
-        nodeMailerFunc(
-          user,
-          `Your password has been reset`,
-          `Click the following link to reset your password:\nhttp://${
-            req.headers.host
-          }/update_password`,
-          'email to update your password'
-        );
-      });
-      return res.status(200).send({
-        msg: [
-          'Your password has been reset!',
-          'Please check your email for the link which will allow you to reset your password!'
-        ]
-      });
     });
   } catch (err) {
     return next(err);
@@ -224,38 +206,42 @@ router.route('/forgot_password').post((req, res) => {
 });
 
 router.route('/reset_password/:token').post((req, res, next) => {
+  var { token } = req.params;
+
   try {
-    User.findOne({
-      resetPasswordToken: req.body.token,
-      resetPasswordExpires: { $gt: Date.now() }
-    }).then(user => {
-      if (!user)
+    Token.findOne({ token: token }, function(err, token) {
+      if (token === null) {
         return res.status(401).send({
-          msg: ['Password reset token is invalid or has expired.']
+          msg: ['We were unable to find a valid token. Your token my have expired.']
         });
-
-      //Set the new password
-      user.password = req.body.password;
-      user.resetPasswordToken = undefined;
-      user.resetPasswordExpires = undefined;
-      user.isVerified = true;
-
-      // Save
-      user.save(err => {
-        if (err) {
-          return res.status(500).json({ message: err.message });
-        }
-
-        nodeMailerFunc(
-          user,
-          `Your password has been changed!`,
-          `You may now login with your new password ${req.body.password} `,
-          'change of password'
-        );
-        return res.status(201).send({
-          msg: ['Your password has been changed!']
+      }
+      if (token) {
+        User.findOne({ _id: token._userId }, function(err, user) {
+          console.log('user ', user);
+          if (!user) {
+            return res.status(404).send({
+              msg: ['We were unable to find a user for this token.']
+            });
+          } else if (user) {
+            user.password = req.body.password;
+            user.isVerified = true;
+            user.save(err => {
+              if (err) {
+                return res.status(500).json({ message: err.message });
+              }
+              nodeMailerFunc(
+                user,
+                `Your password has been changed!`,
+                `You may now login with your new password—${req.body.password}!`,
+                'change of password'
+              );
+              return res.status(201).send({
+                msg: ['Your password has been changed!']
+              });
+            });
+          }
         });
-      });
+      }
     });
   } catch (err) {
     return next(err);
