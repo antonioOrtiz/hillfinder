c2aac31ad78b84931e4fc84daae528b179a484ad
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-04-29 13:36:13 -0400

    got rid of redundant code

--- components/Modal/MyModal.jsx
+++ components/Modal/MyModal.jsx
@@ -1,77 +1,81 @@
-import React, { Component } from 'react'
-import { Button, Modal } from 'semantic-ui-react'
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
+import { Button, Modal } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-import { logOutUser  } from '../../store/reducers/users/index'
-import { modalStateOn, modalStateOff  } from '../../store/reducers/ui/index'
-
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { logOutUser } from '../../store/reducers/users/index';
+import { modalStateOn, modalStateOff } from '../../store/reducers/ui/index';
 
 class MyModal extends Component {
+  render() {
+    const {
+      message,
+      modalActive,
+      isAlertModal,
+      modalStateOff,
+      logOutUser,
+      affirmativeUsed
+    } = this.props;
 
- close = () => {
-  const { modalStateOff } = this.props
-  modalStateOff();
- }
-
- logOutUser = () => {
-  const { logOutUser } = this.props
-  logOutUser()
- }
-
- render() {
-  const { modalActive, isAlertModal } = this.props
-
-   return (
-    <>
-      <Modal dimmer={'blurring'} centered={true} size={'mini'} open={modalActive} onClose={this.close}>
-        <Modal.Header>
-         <p>{this.props.message}</p>
-        </Modal.Header>
-        <Modal.Actions>
-         {this.props.isAlertModal ?
-         <Button
-          color='black'
-          onClick={this.close}
-          content={this.props.affirmativeUsed}
-         />
-         :
-        <>
-         <Button
-           color='black'
-           onClick={this.close}
-          >
-           No
-          </Button>
-          <Button
-           positive
-           icon='checkmark'
-           labelPosition='right'
-           content={this.props.affirmativeUsed}
-           onClick={() => { this.close(); this.logOutUser() }}
-          />
-        </>
-         }
-        </Modal.Actions>
-      </Modal>
-    </>
-   )
- }
+    return (
+      <>
+        <Modal
+          dimmer={'blurring'}
+          centered={true}
+          size={'mini'}
+          open={modalActive}
+          onClose={() => modalStateOff()}
+        >
+          <Modal.Header>
+            <p>{message}</p>
+          </Modal.Header>
+          <Modal.Actions>
+            {isAlertModal ? (
+              <Button
+                color="black"
+                onClick={() => modalStateOff()}
+                content={affirmativeUsed}
+              />
+            ) : (
+              <>
+                <Button color="black" onClick={() => modalStateOff()}>
+                  No
+                </Button>
+                <Button
+                  positive
+                  icon="checkmark"
+                  labelPosition="right"
+                  content={affirmativeUsed}
+                  onClick={() => {
+                    modalStateOff();
+                    logOutUser();
+                  }}
+                />
+              </>
+            )}
+          </Modal.Actions>
+        </Modal>
+      </>
+    );
+  }
 }
 
 MyModal.propTypes = {
- message: PropTypes.string,
- affirmativeUsed: PropTypes.string
-}
+  message: PropTypes.string,
+  affirmativeUsed: PropTypes.string
+};
 
 function mapStateToProps(state) {
- const { ui } = state
- const { modalActive } = ui
+  const { ui } = state;
+  const { modalActive } = ui;
 
- return { modalActive }
+  return { modalActive };
 }
 const mapDispatchToProps = dispatch =>
- bindActionCreators({ logOutUser, modalStateOn, modalStateOff }, dispatch)
+  bindActionCreators({ logOutUser, modalStateOn, modalStateOff }, dispatch);
 
-export default connect(mapStateToProps, mapDispatchToProps)(MyModal)
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MyModal);
