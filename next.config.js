4a7a60b4b8a6b5bc626df4c50c00533f4a50d659
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-05-19 20:27:15 -0400

    added MAPBOC ENV VAR

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-const withSass = require('@zeit/next-sass');
 const withCSS = require('@zeit/next-css');
+const withSass = require('@zeit/next-sass');
+
 module.exports = withCSS(
   withSass({
     distDir: 'build',
     target: 'serverless',
+    env: {
+      MAPBOX_ACCESS_TOKEN:
+        'pk.eyJ1IjoiYW50b25pb3BvcnRpeiIsImEiOiJja2E3NWx4Zm8wN3k4MnBvOWc0YnNoMm96In0.Ckc-lH-tUqB7aQckJhM2IQ'
+    },
+
     webpack(config, options) {
       config.module.rules.push({
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
