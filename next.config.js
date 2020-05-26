7041d10db5ec77dbe778fb11ac8a42040dab8d85
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-05-26 14:14:03 -0400

    added sourcemap to next.config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,30 @@
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
+const withSourceMaps = require('@zeit/next-source-maps');
 
-module.exports = withCSS(
-  withSass({
-    target: 'serverless',
-    env: {
-      MAPBOX_ACCESS_TOKEN:
-        'pk.eyJ1IjoiYW50b25pb3BvcnRpeiIsImEiOiJja2E3NWx4Zm8wN3k4MnBvOWc0YnNoMm96In0.Ckc-lH-tUqB7aQckJhM2IQ'
-    },
+module.exports = withSourceMaps(
+  withCSS(
+    withSass({
+      target: 'serverless',
+      env: {
+        MAPBOX_ACCESS_TOKEN:
+          'pk.eyJ1IjoiYW50b25pb3BvcnRpeiIsImEiOiJja2E3NWx4Zm8wN3k4MnBvOWc0YnNoMm96In0.Ckc-lH-tUqB7aQckJhM2IQ'
+      },
 
-    webpack(config, options) {
-      config.module.rules.push({
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: {
-          loader: 'url-loader',
-          options: {
-            limit: 100000,
-            target: 'serverless'
+      webpack(config, options) {
+        config.module.rules.push({
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 100000,
+              target: 'serverless'
+            }
           }
-        }
-      });
+        });
 
-      return config;
-    }
-  })
+        return config;
+      }
+    })
+  )
 );
