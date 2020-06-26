46dbb6c5273dd939aef642623d4b03939565cf17
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-06-26 12:01:22 -0400

    changed function signature

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,10 +1,13 @@
 import ProfilePage from '../components/Profile/ProfilePage.jsx';
 import { withRouter } from 'react-router-dom';
 
-const Profile = props => (
-  <>
-    <ProfilePage {...props} />
-  </>
-);
+const Profile = props => {
+  // console.log('props line 5', props);
+  return (
+    <>
+      <ProfilePage {...props} />
+    </>
+  );
+};
 
 export default withRouter(Profile);
