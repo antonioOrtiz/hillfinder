db45f9e4c162a9d4b7a1880a8ce142f223a8ea8b
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-07-21 13:11:48 -0400

    trying to remove CSS modules as next.js indicates it is no longer needed

--- next.config.js
+++ next.config.js
@@ -1,34 +1,72 @@
-const withCSS = require('@zeit/next-css');
-const withSass = require('@zeit/next-sass');
-const withImages = require('next-images');
-const optimizedImages = require('next-optimized-images');
+// const withCSS = require('@zeit/next-css');
+// const withSass = require('@zeit/next-sass');
+// const withImages = require('next-images');
+// const optimizedImages = require('next-optimized-images');
 
-module.exports = withImages(
-  optimizedImages(
-    withCSS(
-      withSass({
-        // useFileSystemPublicRoutes: false,
-        target: 'serverless',
-        env: {
-          MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
-          useFileSystemPublicRoutes: false
-        },
+// module.exports = withImages(
+//   optimizedImages(
+//     withCSS(
+//       withSass({
+//         // useFileSystemPublicRoutes: false,
+//         target: 'serverless',
+//         env: {
+//           MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
+//           useFileSystemPublicRoutes: false
+//         },
 
-        webpack(config, options) {
-          config.module.rules.push({
-            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            use: {
-              loader: 'url-loader',
-              options: {
-                limit: 100000,
-                target: 'serverless'
-              }
-            }
-          });
+//         webpack(config, options) {
+//           config.module.rules.push({
+//             test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+//             use: {
+//               loader: 'url-loader',
+//               options: {
+//                 limit: 100000,
+//                 target: 'serverless'
+//               }
+//             }
+//           });
 
-          return config;
-        }
-      })
-    )
-  )
-);
+//           return config;
+//         }
+//       })
+//     )
+//   )
+// );
+
+module.exports = {
+  target: 'serverless',
+  env: {
+    MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
+    useFileSystemPublicRoutes: false
+  }
+};
+
+// module.exports = withImages(
+//   optimizedImages(
+//     withCSS(
+//       withSass({
+//         // useFileSystemPublicRoutes: false,
+//         target: 'serverless',
+//         env: {
+//           MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
+//           useFileSystemPublicRoutes: false
+//         },
+
+//         webpack(config, options) {
+//           config.module.rules.push({
+//             test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+//             use: {
+//               loader: 'url-loader',
+//               options: {
+//                 limit: 100000,
+//                 target: 'serverless'
+//               }
+//             }
+//           });
+
+//           return config;
+//         }
+//       })
+//     )
+//   )
+// );
