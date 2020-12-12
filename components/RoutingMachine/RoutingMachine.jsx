9ed9073d9346fd0d729d1f55710bc2a4037c9e35
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-12-12 12:38:54 -0500

    prettier updated file

--- components/RoutingMachine/RoutingMachine.jsx
+++ components/RoutingMachine/RoutingMachine.jsx
@@ -1,11 +1,10 @@
-import { MapLayer } from "react-leaflet";
-import L from "leaflet";
-import "leaflet-routing-machine";
-import { withLeaflet } from "react-leaflet";
-
+import { MapLayer } from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet-routing-machine';
+import { withLeaflet } from 'react-leaflet';
 
 class Routing extends MapLayer {
- constructor(props) {
+  constructor(props) {
     super(props);
   }
 
@@ -15,47 +14,42 @@ class Routing extends MapLayer {
     var dStart = L.latLng(coords.fromLat, coords.fromLon);
     var dGoal = L.latLng(coords.toLat, coords.toLon);
 
+    if (map && !this.routing) {
+      this.routing = L.Routing.control({
+        collapsible: true,
+        position: 'bottomleft',
+        lineOptions: {
+          styles: [{ color: 'chartreuse', opacity: 1, weight: 5 }]
+        },
+        waypoints: [dStart, dGoal],
+        createMarker: function(i, waypoints, n) {
+          var marker_icon;
 
-     if (map && !this.routing) {
-
-    this.routing = L.Routing.control({
-      collapsible: true,
-      position: 'bottomleft',
-       lineOptions: {
-      styles: [{color: 'chartreuse', opacity: 1, weight: 5}]
-     },
-      waypoints: [dStart, dGoal],
-      createMarker: function(i, waypoints, n) {
-        var marker_icon;
-
-        if (i === 0) {
-         marker_icon = icon.startIcon;
+          if (i === 0) {
+            marker_icon = icon.startIcon;
+          } else if (i == n - 1) {
+            marker_icon = icon.endIcon;
+          }
+          var marker = L.marker(i === 0 ? dStart : dGoal, {
+            draggable: true,
+            icon: marker_icon
+          });
+          return marker;
         }
-        else if (i == n - 1) {
-         marker_icon = icon.endIcon
-        }
-        var marker = L.marker(i === 0 ? dStart : dGoal,{
-         draggable: true,
-         icon: marker_icon
-        });
-        return marker;
-     }
-    });
-
+      });
     }
 
     return this.routing.getPlan();
   }
 
-  componentDidMount(){
+  componentDidMount() {
     const { map } = this.props;
 
- console.log("map ", map);
     map.addControl(this.routing);
   }
 
-  updateLeafletElement(fromProps, toProps){
-    if (toProps.removeRoutingMachine !== false){
+  updateLeafletElement(fromProps, toProps) {
+    if (toProps.removeRoutingMachine !== false) {
       this.routing.setWaypoints([]);
     }
   }
