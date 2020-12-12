1ed12e1bc32d448a67dda2b406664d8d68eb6d1b
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2020-12-12 12:33:05 -0500

    switched to next-pwa

--- next.config.js
+++ next.config.js
@@ -2,32 +2,38 @@ const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
 const withImages = require('next-images');
 const optimizedImages = require('next-optimized-images');
-// const withOffline = require('next-offline');
+const withPWA = require('next-pwa');
 
-module.exports = withImages(
-  optimizedImages(
-    withCSS(
-      withSass({
-        env: {
-          MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
-          useFileSystemPublicRoutes: false
-        },
+module.exports = withPWA(
+  withImages(
+    optimizedImages(
+      withCSS(
+        withSass({
+          pwa: {
+            dest: 'public'
+          },
 
-        webpack(config, options) {
-          config.module.rules.push({
-            test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-            use: {
-              loader: 'url-loader',
-              options: {
-                limit: 100000,
-                target: 'serverless'
+          env: {
+            MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN,
+            useFileSystemPublicRoutes: false
+          },
+
+          webpack(config, options) {
+            config.module.rules.push({
+              test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+              use: {
+                loader: 'url-loader',
+                options: {
+                  limit: 100000,
+                  target: 'serverless'
+                }
               }
-            }
-          });
+            });
 
-          return config;
-        }
-      })
+            return config;
+          }
+        })
+      )
     )
   )
 );
