3460fc72770e658780b15cc82d5fc4588f0cd714
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-01-31 14:56:24 -0500

    removed "click" handler on Map to live inn Map file

--- components/Map/MyMap.jsx
+++ components/Map/MyMap.jsx
@@ -1,46 +1,47 @@
-import React, { useState, useContext, useEffect, useRef, useCallback } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Button, Dimmer, Loader } from 'semantic-ui-react';
 
 import L from 'leaflet';
 import * as ELG from 'esri-leaflet-geocoder';
-import { Map } from 'react-leaflet';
+import { Map, TileLayer } from 'react-leaflet';
 
 import Control from 'react-leaflet-control';
-import LocateControl from '../LocateControl/LocateControl.jsx';
-import MapboxLayer from '../MapboxLayer/MapboxLayer.jsx';
+// import MapboxLayer from '../MapboxLayer/MapboxLayer.jsx';
 import Routing from '../RoutingMachine/RoutingMachine.jsx';
 
-import { stringify } from 'flatted';
+import { parse, stringify } from 'flatted';
 
 import { userState, userDispatch } from '../Context/UserContext.jsx';
 import UIContext from '../Context/UIContext.jsx';
 
-export default function MyMap({}) {
-  var [zoom, setZoom] = useState(18);
+function currentMapViewPropsAreEqual(prevZoom, nextZoom) {
+  return prevZoom.currentMapView === nextZoom.currentMapView;
+}
+
+function MyMap({ currentMapView }) {
   var [animate, setAnimate] = useState(false);
   var [userLocation, setUserLocation] = useState(null);
+  // const [viewport, setViewport] = useState(DEFAULT_VIEWPORT);
+  var handleWaypointsOnMapRef = useRef(handleWaypointsOnMap);
 
   var mapRef = useRef();
+  var mapRefForRoutingMachine = useRef();
   var { state } = userState();
   var { dispatch } = userDispatch();
   var {
     currentMap,
-    isMapLoading,
     isRoutingVisible,
     removeRoutingMachine,
     isLengthOfMarkersLessThanTwo,
     markers
   } = state;
 
-  var { isMobile, isDesktop } = useContext(UIContext);
-
-  // useEffect(() => {
-  //   console.log('markers ', markers);
-  //   return () => {
-  //   };
-  // }, [stringify(markers)]);
+  useEffect(() => {
+    handleWaypointsOnMapRef.current = handleWaypointsOnMap;
+  }); // update after each render
 
   useEffect(() => {
+    console.log('mapRefForRoutingMachine ', mapRefForRoutingMachine);
     dispatch({
       type: 'isMapLoading',
       payload: { isMapLoading: false }
@@ -55,15 +56,28 @@ export default function MyMap({}) {
   }, []);
 
   useEffect(() => {
+    var { current = {} } = mapRef;
+    var { leafletElement: map } = current;
+
+    console.log('foo');
+
+    console.log('currentMap ', currentMapView);
+    map.locate({ setView: true });
+    map.on('locationfound', handleOnLocationFound);
+  }, []);
+
+  useEffect(() => {
     var searchControl = new ELG.Geosearch({
       useMapBounds: false
     });
     var { current = {} } = mapRef;
     var { leafletElement: map } = current;
 
+    console.log('mapRef ', mapRef);
+
     searchControl.addTo(map);
 
-    var cb = e => handleOnClickSetMarkersRef.current(e); // then use most recent cb value
+    var cb = e => handleWaypointsOnMapRef.current(e); // then use most recent cb value
 
     searchControl.on('results', cb);
 
@@ -81,59 +95,187 @@ export default function MyMap({}) {
     };
   }, []);
 
-  useEffect(() => {
-    console.log('markers ', markers);
-
+  function handleOnClickClearOneMarkerAtTime() {
     dispatch({
       type: 'setIsRoutingVisible',
       payload: {
-        isRoutingVisible: true
+        isRoutingVisible: false
       }
     });
-    if (markers.length === 2) {
-      dispatch({
-        type: 'isLengthOfMarkersLessThanTwoFalse',
-        payload: { isLengthOfMarkersLessThanTwo: false }
-      });
-    }
-  }, [JSON.stringify(markers)]);
-
-  function handleOnClickClearOneMarkerAtTime() {
+    mapRefForRoutingMachine.current.handleRemoveWayPoint();
     dispatch({
-      type: 'deleteUserMarkers',
-      payload: 1
+      type: 'deleteUserMarkers'
     });
   }
 
   function handleOnClickClearAllMarkers() {
+    mapRefForRoutingMachine.current.handleClearWayPoints();
     dispatch({
       type: 'resetUserMarkers'
     });
-  }
-
-  function handleOnLocationFound(e) {
-    setUserLocation(e.latlng);
   }
 
   function handleOnClickMarkerClick(e) {
     e.originalEvent.view.L.DomEvent.stopPropagation(e);
   }
 
+  function handleWaypointsOnMap(e) {
+    var { current = {} } = mapRef;
+    var { leafletElement: map } = current;
+    dispatch({
+      type: 'setIsRoutingVisible',
+      payload: {
+        isRoutingVisible: true
+      }
+    });
+    dispatch({
+      type: 'setRemoveRoutingMachine',
+      payload: {
+        removeRoutingMachine: false
+      }
+    });
+
+    function createButton(label, container) {
+      var btn = L.DomUtil.create('button', '', container);
+      btn.setAttribute('type', 'button');
+      btn.innerHTML = label;
+      return btn;
+    }
+    var container = L.DomUtil.create('div'),
+      startBtn = createButton('Start from this location', container),
+      destBtn = createButton('Go to this location', container);
+
+    L.popup()
+      .setContent(container)
+      .setLatLng(e.latlng)
+      .openOn(map);
+
+    L.DomEvent.on(startBtn, 'click', function() {
+      if (markers.length === 0) {
+        e.latlng.alt = 'current location';
+
+        console.log('adding current location', e.latlng);
+
+        dispatch({
+          type: 'addMarker',
+          payload: {
+            marker: e.latlng
+          }
+        });
+      }
+
+      if (markers[0] != undefined) {
+        e.latlng.alt = 'current location';
+        console.log('updating current location', e.latlng);
+
+        dispatch({
+          type: 'updateMarkers',
+          payload: {
+            marker: e.latlng
+          }
+        });
+      }
+
+      mapRefForRoutingMachine.current.handleSpliceWaypoints(0, 1, e.latlng);
+
+      map.closePopup();
+    });
+
+    L.DomEvent.on(
+      destBtn,
+      'click',
+      function() {
+        console.log('e', e);
+        if (markers.length === 1) {
+          e.latlng.alt = 'current destination';
+          console.log('adding destination ', e.latlng);
+          dispatch({
+            type: 'addMarker',
+            payload: {
+              marker: e.latlng
+            }
+          });
+        }
+        if (markers.length === 2 && markers[1] !== undefined) {
+          e.latlng.alt = 'current destination';
+          console.log('updating destination', e.latlng);
+
+          dispatch({
+            type: 'updateMarkers',
+            payload: {
+              marker: e.latlng
+            }
+          });
+        }
+
+        mapRefForRoutingMachine.current.handleSpliceWaypoints(1, 1, e.latlng);
+
+        map.closePopup();
+      }.bind(this)
+    );
+  }
+
+  function handleOnViewportChanged(e) {
+    console.log('viewport change', e);
+
+    console.log('currentMapView ', currentMapView);
+    var { current = {} } = mapRef;
+    var { leafletElement: map } = current;
+
+    map.on('zoomend', function() {
+      var zoom = map.getZoom();
+      console.log('zoom ', zoom);
+
+      console.log("'dispatch setMapZoom'; ");
+      dispatch({
+        type: 'setMapZoom',
+        payload: {
+          currentMapView: zoom
+        }
+      });
+    });
+  }
+
+  function handleOnLocationFound(e) {
+    console.log('e ', e);
+    var { current = {} } = mapRef;
+    var { leafletElement: map } = current;
+    map.setZoom(currentMapView);
+
+    var latlng = e.latlng;
+    var radius = e.accuracy;
+    var circle = L.circle(latlng, radius);
+    circle.addTo(map);
+  }
+
+  useEffect(() => {
+    console.log(
+      'process.env.MAPBOX_USERNAME, process.env.MAPBOX_STYLE_ID, process.env.MAPBOX_ACCESS_TOKEN, process.env.MAILGUN_PUBLIC_KEY, process.env.EMAIL_ADDRESS; ',
+      process.env.MAPBOX_USERNAME,
+      process.env.MAPBOX_STYLE_ID,
+      process.env.MAPBOX_ACCESS_TOKEN,
+      process.env.MAILGUN_PUBLIC_KEY,
+      process.env.EMAIL_ADDRESS
+    );
+    return () => {};
+  }, []);
   return (
     <Map
       preferCanvas={true}
       id="myMap"
       animate={animate}
-      onLocationFound={handleOnLocationFound}
-      zoom={zoom}
+      zoom={currentMapView}
       ref={mapRef}
+      onViewportChanged={handleOnViewportChanged}
+      onClick={e => handleWaypointsOnMap(e)}
     >
-      <MapboxLayer
-        accessToken={process.env.MAPBOX_ACCESS_TOKEN}
-        style="mapbox://styles/mapbox/streets-v9"
+      <TileLayer
+        url={`https://api.mapbox.com/styles/v1/${process.env.MAPBOX_USERNAME}/${
+          process.env.MAPBOX_STYLE_ID
+        }/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`}
+        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
       />
 
-      <LocateControl isMapLoading={isMapLoading} startDirectly />
       <Control position="bottomleft">
         <Button onClick={handleOnClickClearOneMarkerAtTime} color="orange" size="small">
           delete one marker!
@@ -144,16 +286,22 @@ export default function MyMap({}) {
           clear all!
         </Button>
       </Control>
-      {isRoutingVisible && (
+
+      {mapRef && (
         <Routing
+          isRoutingVisible={isRoutingVisible}
+          ref={mapRefForRoutingMachine}
           markers={markers}
+          stringify={stringify}
           isLengthOfMarkersLessThanTwo={isLengthOfMarkersLessThanTwo}
           removeRoutingMachine={removeRoutingMachine}
           userLocation={userLocation}
-          isMobile={isMobile}
-          isDesktop={isDesktop}
         />
       )}
     </Map>
   );
 }
+
+var MemoizedMyMap = React.memo(MyMap, currentMapViewPropsAreEqual);
+
+export default MemoizedMyMap;
--- components/RoutingMachine/RoutingMachine.jsx
+++ components/RoutingMachine/RoutingMachine.jsx
@@ -4,6 +4,9 @@ import 'leaflet-routing-machine';
 import { withLeaflet } from 'react-leaflet';
 import UserContextDispatch from '../Context/UserContext.jsx';
 import { Dimmer, Loader } from 'semantic-ui-react';
+import { isEqual } from 'lodash';
+
+import AwesomeDebouncePromise from 'awesome-debounce-promise';
 
 class Routing extends MapLayer {
   static contextType = UserContextDispatch;
@@ -11,31 +14,43 @@ class Routing extends MapLayer {
   constructor(props) {
     super(props);
     this.state = {
-      from: 0,
-      to: 0,
       showSpinner: false,
       localDispatch: null,
-      routing: null,
-      markerStart: null
+      markerIsBeingDragged: false,
+      currentMarker: {}
     };
-    this.handleFromIncrement = this.handleFromIncrement.bind(this);
-    this.handleResetFrom = this.handleResetFrom.bind(this);
 
     this.handleLoader = this.handleLoader.bind(this);
-
+    this.handleRemoveWayPoint = this.handleRemoveWayPoint.bind(this);
+    this.handleClearWayPoints = this.handleClearWayPoints.bind(this);
     this.handleSpliceWaypoints = this.handleSpliceWaypoints.bind(this);
-    this.handleMySetState = this.handleMySetState.bind(this);
+    this.handleSetMarker = this.handleSetMarker.bind(this);
   }
-  handleFromIncrement() {
-    this.setState(function(prevState) {
-      return { from: prevState.from + 1 };
-    });
+
+  handleSetMarker(marker) {
+    var { markers } = this.props;
+
+    if (markers[0] !== undefined && markers[0].alt === 'current location') {
+      this.setState(prevState => ({
+        currentMarker: { ...prevState.currentMarker, ...marker }
+      }));
+    }
+    if (markers[1] !== undefined && markers[1].alt === 'current destination') {
+      this.setState(prevState => ({
+        currentMarker: { ...prevState.currentMarker, ...marker }
+      }));
+    }
+
+    console.log('this.state ', this.state);
+  }
+
+  handleSpliceWaypoints(start, end, obj) {
+    this.control.spliceWaypoints(start, end, obj);
   }
 
   handleLoader() {
     var { showSpinner } = this.state;
 
-    // console.log('showSpinner ', showSpinner);
     if (this.state.showSpinner === false) {
       this.setState(function(prevState) {
         return { showSpinner: !prevState.showSpinner };
@@ -51,21 +66,38 @@ class Routing extends MapLayer {
     });
   }
 
-  handleMySetState(context, event) {
+  handleRemoveWayPoint() {
+    var waypoints = this.control.getWaypoints();
+
+    for (let i = waypoints.length - 1; i >= 0; i--) {
+      console.log('waypoints[i].latLng !== null ', waypoints[i].latLng !== null);
+      if (waypoints[i].latLng !== null) {
+        waypoints[i].latLng = null;
+        break;
+      }
+    }
+    console.log('waypoints ', waypoints);
+    this.control.setWaypoints(waypoints);
+  }
+
+  handleClearWayPoints() {
+    this.control.setWaypoints([L.latLng(null, null), L.latLng(null, null)]);
+  }
+
+  componentDidMount() {
+    const { map } = this.props.leaflet;
+
+    var { markers } = this.props;
+
+    this.control.setWaypoints([L.latLng(markers[0]), L.latLng(markers[1])]);
+
+    // console.log("'mounting' ", 'mounting');
+    // console.log('this.props ', this.props);
     this.setState(prevState => {
-      markers: prevState.markers.start = {
-        this: context,
-        event: event
-      };
+      localDispatch: prevState.localDispatch = this.context.dispatch;
     });
-  }
 
-  handleResetFrom() {
-    this.setState({ from: (this.state.from = 0) });
-  }
-
-  handleSpliceWaypoints(s, f, e) {
-    this.routing.spliceWaypoints(s, f, e.latlng);
+    map.addControl(this.control);
   }
 
   createLeafletElement(props) {
@@ -94,11 +126,8 @@ class Routing extends MapLayer {
     });
 
     if (map && !this.control) {
-      if (this.state != undefined) {
-        if (this.state.routing !== null) {
-          var { routing } = this.state;
-        }
-      }
+      var RoutingMachineRef = this;
+
       this.control = L.Routing.control({
         collapsible: true,
         show: false,
@@ -114,30 +143,35 @@ class Routing extends MapLayer {
               draggable: true,
               keyboard: true,
               alt: 'current location'
+            }).on('move', function(e) {
+              e.target._latlng.alt = 'current location';
+              console.log('e.target._latlng', e.target._latlng);
+
+              console.log('there be dragons start!!', e);
+              RoutingMachineRef.setState(prevState => ({
+                markerIsBeingDragged: !prevState.markerIsBeingDragged
+              }));
+              RoutingMachineRef.handleSetMarker(e.target._latlng);
             });
           }
           if (i === nWps - 1) {
             return L.marker(wp.latLng, {
               icon: endIcon,
               draggable: true,
-              alt: 'destination'
+              alt: 'current destination'
+            }).on('move', function(e) {
+              e.target._latlng.alt = 'current destination';
+
+              console.log(' e.target._latlng', e.target._latlng);
+              RoutingMachineRef.setState(prevState => ({
+                markerIsBeingDragged: !prevState.markerIsBeingDragged
+              }));
+              console.log('there be dragons dest!!', e);
+              RoutingMachineRef.handleSetMarker(e.target._latlng);
             });
           }
         }
-      })
-        .on('routingstart', function(e) {
-          console.log('e ', e);
-        })
-        .on('routesfound', function(e) {
-          console.log('routers found ', e);
-        })
-
-        .on('routeselected', function(e) {
-          console.log('e ', e);
-        })
-        .on('waypointschanged', function(e) {
-          console.log('waypoint changed', e);
-        });
+      });
 
       L.Routing.errorControl(this.control).addTo(map);
     }
@@ -145,180 +179,40 @@ class Routing extends MapLayer {
     return this.control.getPlan();
   }
 
-  componentDidMount() {
-    const { map } = this.props.leaflet;
-    var { markers } = this.props;
-
-    console.log('markers ', markers);
-    this.setState(prevState => {
-      localDispatch: prevState.localDispatch = this.context.dispatch;
-    });
-
-    this.setState(prevState => {
-      routing: prevState.routing = this.control;
-    });
-
-    console.log('this.getWaypoints() ', this.control.getWaypoints());
-
-    var waypoints = this.control.getWaypoints();
-
-    waypoints.forEach((waypoint, i, arr) => {
-      if (markers != undefined) {
-        if (
-          arr[0].latLng == null &&
-          markers[0] !== undefined &&
-          (arr[1].latLng == null && markers[1] !== undefined)
-        ) {
-          this.control.setWaypoints([
-            L.latLng(markers[0] || null),
-            L.latLng(markers[1] || null)
-          ]);
-        }
-      }
-    });
-
-    map.addControl(this.control);
-  }
-
   updateLeafletElement(fromProps, toProps) {
-    const { map } = this.props.leaflet;
+    var { currentMarker, localDispatch, markerIsBeingDragged } = this.state;
 
-    var { markers } = this.props;
-
-    function createButton(label, container) {
-      var btn = L.DomUtil.create('button', '', container);
-      btn.setAttribute('type', 'button');
-      btn.innerHTML = label;
-      return btn;
-    }
-
-    var { localDispatch } = this.state;
-    var container = L.DomUtil.create('div'),
-      startBtn = createButton('Start from this location', container),
-      destBtn = createButton('Go to this location', container);
-    map.on(
-      'click',
-      function(e) {
+    // console.log('fromProps, toProps ', fromProps, toProps);
+    if (markerIsBeingDragged && currentMarker.hasOwnProperty('alt')) {
+      if (isEqual(toProps.markers[0], currentMarker) === false) {
         localDispatch({
-          type: 'setIsRoutingVisible',
+          type: 'updateMarkers',
           payload: {
-            isRoutingVisible: true
+            marker: currentMarker
           }
         });
-
-        if (markers.length === 2) {
-          console.log("Can't add anymore markers");
-        } else {
-          L.popup()
-            .setContent(container)
-            .setLatLng(e.latlng)
-            .openOn(map);
-
-          L.DomEvent.on(
-            startBtn,
-            'click',
-            function() {
-              console.log('e ', e);
-
-              if (markers[0] != undefined) {
-                if (markers[0].alt == 'current location') {
-                  localDispatch({
-                    type: 'updateMarkers',
-                    payload: {
-                      marker: e.latlng
-                    }
-                  });
-                }
-              } else {
-                e.latlng.alt = 'current location';
-
-                localDispatch({
-                  type: 'addMarker',
-                  payload: {
-                    marker: e.latlng
-                  }
-                });
-              }
-              this.control.spliceWaypoints(0, 1, e.latlng);
-              map.closePopup();
-            }.bind(this)
-          );
-
-          L.DomEvent.on(
-            destBtn,
-            'click',
-            function() {
-              if (markers[1] != undefined) {
-                if (markers[1].alt == 'current destination') {
-                  localDispatch({
-                    type: 'updateMarkers',
-                    payload: {
-                      marker: e.latlng
-                    }
-                  });
-                }
-              } else {
-                e.latlng.alt = 'current destination';
-
-                localDispatch({
-                  type: 'addMarker',
-                  payload: {
-                    marker: e.latlng
-                  }
-                });
-              }
-
-              this.control.spliceWaypoints(
-                this.control.getWaypoints().length - 1,
-                1,
-                e.latlng
-              );
-              // e.latlng.alt = 'current destination';
-              // console.log('e.latlng ', e.latlng);
-              // localDispatch({
-              //   type: 'addMarker',
-              //   payload: {
-              //     marker: e.latlng
-              //   }
-              // });
-              map.closePopup();
-            }.bind(this)
-          );
-        }
-      }.bind(this)
-    );
-
-    this.control.on('waypointschanged', function(e) {
-      console.log('waypoint changed', e);
-      e.waypoints.forEach((waypoint, i, arr) => {
-        if (waypoint.latLng !== null) {
-          if (i === 0) {
-            waypoint.latLng.alt = 'current location';
-          } else {
-            waypoint.latLng.alt = 'current destination';
+      }
+      if (isEqual(toProps.markers[1], currentMarker) === false) {
+        localDispatch({
+          type: 'updateMarkers',
+          payload: {
+            marker: currentMarker
           }
+        });
+      }
 
-          console.log('waypoint.latLng ', waypoint.latLng);
-          localDispatch({
-            type: 'updateMarkers',
-            payload: {
-              marker: waypoint.latLng
-            }
-          });
-        }
-      });
-    });
+      this.setState(prevState => ({
+        markerIsBeingDragged: !prevState.markerIsBeingDragged
+      }));
+    }
 
     if (toProps.removeRoutingMachine !== false) {
       this.control.setWaypoints([]);
     }
   }
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('nextProps, nextState ', nextProps, nextState);
-    // return this.state.value != nextState.value;
-  }
 
   componentWillUnmount() {
+    console.log("'unmount' ", 'unmount');
     this.destroyRouting();
   }
 
