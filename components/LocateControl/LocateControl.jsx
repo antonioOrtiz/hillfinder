30fcc28335d2b382b8906dca190fc27487cfeb55
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-02-20 18:03:01 -0500

    Fixing Locate issues between isMobile and isDesktop

--- components/LocateControl/LocateControl.jsx
+++ components/LocateControl/LocateControl.jsx
@@ -7,35 +7,19 @@ function LocateControl({ startDirectly, leaflet }) {
 
   var options = {
     flyTo: true,
-
+    keepCurrentZoomLevel: true,
     position: 'topleft',
-    enableHighAccuracy: true,
-    setView: 'always',
+    setView: 'once',
     strings: {
       title: 'Show me where I am, yo!'
     },
+    locateOptions: { enableHighAccuracy: true, setView: false },
     onActivate: () => {} // callback before engine starts retrieving locations
   };
   useEffect(() => {
     const lc = new Locate(options);
 
     console.log('lc ', lc);
-    // lc.on('locationfound', function(e) {
-    //   var marker = L.marker([e.latitude, e.longitude]).bindPopup('Your are here :)');
-    //   var circle = L.circle([e.latitude, e.longitude], e.accuracy / 2, {
-    //     weight: 1,
-    //     color: 'blue',
-    //     fillColor: '#cacaca',
-    //     fillOpacity: 0.2
-    //   });
-    //   map.addLayer(marker);
-    //   map.addLayer(circle);+
-    // }).on('locationerror', function(e) {
-    //   console.log(e);
-    //   alert('Location access denied.');
-    // });
-
-    // console.log('map.getCenter(); ', map.getCenter());
 
     lc.addTo(map);
 
