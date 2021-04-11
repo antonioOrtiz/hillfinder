8b2594c608589bc8756a9e5676861f44968ea201
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-04-11 01:11:09 -0400

    fixing window reference error

--- pages/dashboard.jsx
+++ pages/dashboard.jsx
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from 'react';
+
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { logOutUser } from '../store/reducers/users/index';
