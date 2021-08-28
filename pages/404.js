9cb30ac1438a47bdab4071e6648c212e464e7351
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-08-28 16:01:56 -0400

    removed space

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ export default function FourOhFour({ location }) {
     <h1>
       404 - Page Not Found;
     </h1>
-    <h2 style={{ fontWeight: 'strong' }}>Sorry but the page{''}
+    <h2 style={{ fontWeight: 'strong' }}>Sorry but the page
       {location.pathname.substring(1)} could not be found</h2>{' '}
     <Link to="/">
       Home
