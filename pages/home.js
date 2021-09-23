edee2e1274685708f6c40f5b32a2786c93d9fd24
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-09-23 15:46:20 -0400

    added placeholder content

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,20 @@
 export default function () {
   return (
     <>
-      Home
+      <div className="hero min-h-screen bg-base-200">
+        <div className="flex-col hero-content lg:flex-row-reverse">
+          <img src="https://picsum.photos/id/1005/600/600" className="max-w-sm rounded-lg shadow-2xl" />
+          <div>
+            <h1 className="mb-5 text-5xl font-bold">
+              Hello there
+            </h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in.Quaerat fugiat ut assumenda excepturi exercitationem quasi.In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-primary ">Get Started</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
