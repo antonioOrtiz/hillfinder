381cb4cd4124de21c1969796571b9fdcdfe102eb
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-09-23 15:44:43 -0400

    added app related imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
-import 'tailwindcss/tailwind.css'
-import '../styles/styles.scss'
+import { CloudinaryContext } from 'cloudinary-react';
 
+import 'tailwindcss/tailwind.css'
+import 'leaflet/dist/leaflet.css';
+import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
+import '../styles/styles.scss'
+// import { userState, userDispatch } from '../components/Context/UserContext';
+
+import { UserProvider, userState } from '../components/Context/UserContext';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider value={userState}>
+      <CloudinaryContext cloudName="hillfinders">
+
+        <Component {...pageProps} />
+
+      </CloudinaryContext>
+    </UserProvider>
+  )
 }
 
 export default MyApp
