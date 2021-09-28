194a5b902b9114d00d6db01757150758b9aabaa2
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-09-28 17:38:19 -0400

    added logic for different dev ENV

--- middleware/mongodb.js
+++ middleware/mongodb.js
@@ -1,32 +1,33 @@
 import mongoose from 'mongoose';
 
 // Get your connection string from .env.local
+let MONGODB_CONN_STR;
 
-const MONGODB_CONN_STR = process.env.MONGODB_URI;
+
+if (process.env.NODE_ENV === 'production') MONGODB_CONN_STR = process.env.MONGODB_URI;
+else MONGODB_CONN_STR = process.env.DEVELOPMENT_DB_DSN
+
 
 const databaseMiddleware = async (req, res, next) => {
 
   try {
-
     if (!global.mongoose) {
 
       global.mongoose = await mongoose.connect(MONGODB_CONN_STR, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       });
-
     }
-
   }
   catch (ex) {
-    console.error(ex);
+    console.error('fucking error', ex);
   }
 
   // You could extend the NextRequest interface
   // with the mongoose instance as well if you wish.
   // req.mongoose = global.mongoose;
 
-  return next();
+
 
 };
 
