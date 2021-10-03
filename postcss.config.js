63e63f5992c5bc25f0410440f50e3f2a5c1058f0
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-03 13:51:41 -0400

    removed unused config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,7 @@
 // If you want to use other PostCSS plugins, see the following:
 // https://tailwindcss.com/docs/using-with-preprocessors
 module.exports = {
-  theme: {
-    extend: {
 
-    }
-  },
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
