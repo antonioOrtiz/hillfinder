3944f82555b179ac99e526f9f15cc8372a758e61
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-09 12:45:22 -0400

    added punctuation to message

--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,5 @@
-import { useEffect } from 'react';
 import { validate, validateAll } from 'indicative/validator';
-import axios from 'axios';
+import { sanitize } from 'indicative/sanitizer'
 
 import crypto from 'crypto';
 
@@ -78,11 +77,12 @@ export function validateInputs(
         password: 'required|min:7|max:11'
       };
       const messages = {
-        email: 'Make sure this is a valid email',
+        email: 'Make sure this is a valid email.',
         required: 'This is a required field.',
         min: 'The password is too short. Minimum 7 characters.',
         max: 'The password is too long. Maximum 11 characters.'
       };
+
 
       validateAll(data, schema, messages)
         .then(success => {
@@ -123,8 +123,8 @@ export function validateInputs(
         username: 'email'
       };
       const messages = {
-        required: 'Make sure to enter the field value',
-        email: 'Enter valid email address'
+        required: 'Make sure to enter the field value.',
+        email: 'Enter valid email address.'
       };
       validate(data, schema, messages)
         .then(success => {
@@ -152,7 +152,7 @@ export function validateInputs(
         password_confirmation: 'required|min:7|max:11|string|same:password'
       };
       const messages = {
-        required: 'Make sure to enter the field value',
+        required: 'Make sure to enter the field value.',
         min: 'The password is too short. Minimum 7 characters.',
         max: 'The password is too long. Maximum 11 characters.',
         same: 'Passwords must match.'
@@ -162,7 +162,6 @@ export function validateInputs(
         .then(success => {
           if (success.password) {
             setPasswordError(false);
-            setDisableButton(false);
           }
 
           if (success.password === success.password_confirmation) {
@@ -171,17 +170,23 @@ export function validateInputs(
           }
         })
         .catch(errors => {
-          if (errors[0].field === 'password') {
-            setPasswordError(true);
-            setPasswordFeedback(errors[0].message);
-            setDisableButton(true);
-          }
+          errors.map((error) => {
 
-          if (errors[0].field === 'password_confirmation') {
-            setPasswordConfirmationError(true);
-            setPasswordConfirmationFeedback(errors[0].message);
-            setDisableButton(true);
-          }
+            console.log("error 179", error);
+            if (error.field === 'password') {
+              setPasswordError(true);
+              setPasswordFeedback(error.message);
+              setDisableButton(true);
+              setFormError(true)
+            }
+
+            if (error.field === 'password_confirmation') {
+              setPasswordConfirmationError(true);
+              setPasswordConfirmationFeedback(error.message);
+              setDisableButton(true);
+              setFormError(true)
+            }
+          })
         });
     }
 
@@ -194,82 +199,12 @@ export function validateInputs(
 
     try {
       Forms[formType]();
-    } catch (error) { }
+    } catch (error) { console.log('Error', error) }
   }
 
   return getFormValidation(formType);
 }
 
-export function logOutUserSession() {
-  axios
-    .get('/api/logout')
-    .then(response => {
-      if (response.status === 200) {
-      }
-    })
-    .catch(error => { });
-}
-
-export function getUserAvatar() {
-  return axios
-    .get('/api/user_avatar')
-    .then(response => {
-      if (response.status === 200) {
-        if (!response.data.hasOwnProperty('avatar_info')) {
-          return '/uploads/profile-avatars/placeholder.jpg';
-        }
-        return response.data.avatar_info.secure_url;
-      }
-      if (response.status === 500) {
-      }
-    })
-    .catch((error) => { });
-}
-
-export function Message({ state, header = '', content = '' }) {
-
-  useEffect(() => {
-    document.addEventListener("DOMContentLoaded", () => {
-      setTimeout(() => {
-        const replacers = document.querySelectorAll('[data-replace]');
-        for (var i = 0; i < replacers.length; i++) {
-          console.log('hit here2');
-          const replaceClasses = JSON.parse(replacers[i].dataset.replace.replace(/'/g, '"'));
-          Object.keys(replaceClasses).forEach((key) => {
-            replacers[i].classList.remove(key);
-            replacers[i].classList.add(replaceClasses[key]);
-          });
-        }
-      }, 0);
-    });
-  }, [])
-
-  return (
-    <>
-      {{
-        Waring: <div className="animate-fade-in-down my-4 py-2 p-3  text-orangeDark bg-orangeLight border border-orangeDark-300 rounded relative" role="alert">
-          <strong className="font-bold">{header}</strong>
-          <span className="block sm">{content}</span>
-
-        </div>,
-        Success: <div
-          className="my-4 py-2 p-3  text-green-700 bg-green-100 border border-green-300 rounded relative animate-fade-in-down"
-          role="alert"
-        >
-          <p className="font-bold">{header}</p>
-
-          <strong className="font-bold">{content}</strong>
-        </div>,
-        Error: <div className="animate-fade-in-down relative my-4 py-2 pl-3 pr-10 leading-normal text-red-700 bg-red-100 border border-red-700 rounded-lg" role="alert">
-          <p className="font-bold">{header}</p>
-
-          <p>{content}</p>
-        </div>
-
-      }[state]}
-    </>
-  )
-}
 
 
 
