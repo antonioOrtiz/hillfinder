9500c8fc8111ce65df3ee43af254f09f8e1200bc
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-13 17:06:38 -0400

    changed property name from username to email

--- pages/api/login.js
+++ pages/api/login.js
@@ -11,11 +11,13 @@ import passport from '../../lib/passport'
 
 import connectDB from '../../middleware/mongodb';
 
+import errorHandler from './error-handler'
+
 require('dotenv').config();
 
 const validateBody = initMiddleware(
   validateMiddleware([
-    body('username').isEmail(),
+    body('email').isEmail(),
     check('password').isLength({ min: 7, max: 11 }),
   ], validationResult)
 )
@@ -27,56 +29,69 @@ connectDB()
 
 handler
   .use(auth)
-  .post(async (req, res, next) => {
-    await validateBody(req, res)
+  .post(async (req, res) => {
+    try {
+      await validateBody(req, res)
 
-    const errors = validationResult(req);
+      const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-      return res.status(422).json({
-        msg: [
-          'Please follow the validations above',
-          're-enter a proper email and/or password.'
-        ]
-      })
-    }
-
-    User.findOne({ username: req.body.username }).then(user => {
-
-      if (!user) {
-        return res.status(404).json({
+      if (!errors.isEmpty()) {
+        return res.status(422).json({
           msg: [
-            'We were unable to find this user.',
-            'This email may be incorrect. Please confirm with the "Forgot password" link above or the "Register" link below!.'
+            'Please follow the validations above',
+            're-enter a proper email and/or password.'
           ]
-        });
+        })
       }
-      return res.status(401).json({
-        msg: [
-          'This email and/or password combo may be incorrect',
-          'Please confirm with the "Forgot password" link above or the "Create new account" link below!'
-        ]
-      })
-    });
-  },
 
-    passport.authenticate('local', { session: true }),
 
-    (req, res) => {
-      const user = req.user;
-      if (user.isVerified === false) {
-        return res.status(403).send({
+
+      User.findOne({ email: req.body.email }).then(user => {
+
+        console.log("req ", req);
+
+        console.log("user ", user);
+        passport.authenticate('local');
+
+        if (user) {
+          if (user.isVerified === false) {
+            return res.status(403).send({
+              msg: [
+                'Your username has not been verified!',
+                'Check your email for a confirmation link.'
+              ]
+            });
+          }
+          return res.status(200).send({
+            userId: user,
+            msg: [`Your have successfully logged in;`, `Welcome to Hillfinder!`]
+          });
+
+
+        }
+        else if (!user) {
+          return res.status(404).json({
+            msg: [
+              'We were unable to find this user.',
+              'This email may be incorrect. Please confirm with the "Forgot password?" link or the "Create new account" link below.'
+            ]
+          });
+        }
+        return res.status(401).json({
           msg: [
-            'Your username has not been verified!',
-            'Check your email for a confirmation link.'
+            'This email and/or password combo may be incorrect',
+            'Please confirm with the "Forgot password" link above or the "Create new account" link below!'
           ]
-        });
-      }
-      return res.status(200).send({
-        userId: user,
-        msg: [`Your have successfully logged in;`, `Welcome to Hillfinder!`]
+        })
+
+
       });
-    })
+
+
+    } catch (err) {
+      errorHandler(err, res)
+    }
+  })
 
 
 export default handler;
