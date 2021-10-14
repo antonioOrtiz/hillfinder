621887ccc55c3c6402db0526a75a914b0b27b0d4
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-14 14:06:49 -0400

    formatting and added errorHandler function

--- pages/api/logout.js
+++ pages/api/logout.js
@@ -3,17 +3,22 @@ import auth from '../../middleware/auth'
 
 import connectDB from '../../middleware/mongodb';
 
+import errorHandler from './error-handler';
 
 const handler = nextConnect()
 
 connectDB()
 
 handler.use(auth).get((req, res) => {
+  try {
 
-  req.logout();
-  return res.status(201).send({
-    msg: ['Your have successfully logged out!']
-  });
+    req.logout();
+    return res.status(201).send({
+      msg: ['Your have successfully logged out!']
+    });
+  } catch (err) {
+    errorHandler(err, res)
+  }
 })
 
 export default handler
