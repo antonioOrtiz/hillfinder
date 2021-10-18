f19c8497f655ce063cea36fa8dafd7cbb2048b15
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-18 16:33:59 -0400

    fixed login syntax

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,97 +1,62 @@
 import nextConnect from 'next-connect'
-import { check, body, validationResult } from 'express-validator'
-
 import auth from '../../middleware/auth'
-import initMiddleware from '../../middleware/init-middleware'
-import validateMiddleware from '../../middleware/validate-middleware'
-
-import User from '../../models/User'
-
 import passport from '../../lib/passport'
 
-import connectDB from '../../middleware/mongodb';
+import connectDB from '../../lib/mongodb';
 
 import errorHandler from './error-handler'
 
-require('dotenv').config();
-
-const validateBody = initMiddleware(
-  validateMiddleware([
-    body('email').isEmail(),
-    check('password').isLength({ min: 7, max: 11 }),
-  ], validationResult)
-)
-
-
 const handler = nextConnect()
 
-connectDB()
+handler.use(auth).post(async (req, res, next) => {
+  await connectDB()
+  passport.authenticate('local', (err, user, info) => {
+    console.log("user ", user);
+    console.log("info ", info);
 
-handler
-  .use(auth)
-  .post(async (req, res) => {
-    try {
-      await validateBody(req, res)
+    if (err) { return errorHandler(err, res) }
 
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty()) {
-        return res.status(422).json({
-          msg: [
-            'Please follow the validations above',
-            're-enter a proper email and/or password.'
-          ]
-        })
+    if (user) {
+      if (user.isVerified) {
+        return res.status(200).send({
+          userId: user._id,
+          msg: [`Your have successfully logged in;`, `Welcome to Hillfinder!`]
+        });
       }
-
-
-
-      User.findOne({ email: req.body.email }).then(user => {
-
-        console.log("req ", req);
-
-        console.log("user ", user);
-        passport.authenticate('local');
-
-        if (user) {
-          if (user.isVerified === false) {
-            return res.status(403).send({
-              msg: [
-                'Your username has not been verified!',
-                'Check your email for a confirmation link.'
-              ]
-            });
-          }
-          return res.status(200).send({
-            userId: user,
-            msg: [`Your have successfully logged in;`, `Welcome to Hillfinder!`]
-          });
-
-
-        }
-        else if (!user) {
-          return res.status(404).json({
-            msg: [
-              'We were unable to find this user.',
-              'This email may be incorrect. Please confirm with the "Forgot password?" link or the "Create new account" link below.'
-            ]
-          });
-        }
-        return res.status(401).json({
-          msg: [
-            'This email and/or password combo may be incorrect',
-            'Please confirm with the "Forgot password" link above or the "Create new account" link below!'
-          ]
-        })
-
-
+      return res.status(403).send({
+        msg: [
+          'Your username has not been verified!',
+          'Check your email for a confirmation link.'
+        ]
       });
 
 
-    } catch (err) {
-      errorHandler(err, res)
     }
-  })
+
+    else if (user === false) {
+      return res.status(404).send({
+        msg: [
+          `We were unable to find this user.`,
+          `Please confirm with the "Forgot password" link or the "Register" link below!`
+        ]
+      })
+    }
 
 
-export default handler;
+
+
+
+
+
+
+
+
+
+
+
+
+
+  })(req, res, next);
+})
+
+export default handler
