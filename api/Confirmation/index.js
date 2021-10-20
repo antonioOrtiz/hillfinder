4867198ec6525407080dad4386bf9775585c0e40
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-20 09:14:46 -0400

    changed import path of Message component

--- api/Confirmation/index.js
+++ api/Confirmation/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 import axios from 'axios';
 import Layout from '../../components/Layout/index'
-import Message from '../../utils/Message';
+import Message from '../../components/Message/index';
 
 export default function isConfirmation(error, setError, setResponseMessage, responseMessage, dispatch,
   uidispatch) {
