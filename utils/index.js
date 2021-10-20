ab22c1c84b12de601f39fabcd1a0ac95c4bf4ea2
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-20 09:02:48 -0400

    changed install/setup of mailgun

--- utils/index.js
+++ utils/index.js
@@ -5,11 +5,17 @@ import crypto from 'crypto';
 
 import Token from '../models/Token'
 
-const mailgun = require("mailgun-js");
+const formData = require('form-data');
 
-const mg = mailgun({
-  apiKey: process.env.MAILGUN_API_KEY,
-  domain: process.env.MAILGUN_DOMAIN
+const Mailgun = require('mailgun.js');
+
+const mailgun = new Mailgun(formData);
+
+
+const mg = mailgun.client({
+  username: 'api',
+  key: process.env.MAILGUN_API_KEY,
+  public_key: process.env.MAILGUN_PUBLIC_KEY
 });
 
 export function nodeMailerFunc(user, subjectField, textField, emailType, res) {
