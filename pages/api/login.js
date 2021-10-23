3e6bf064c1dbdb21a8b576f8c7e11644bff8d7a7
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-23 10:04:53 -0400

    added emailValidator for server-side rendering and changed responses to strings

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,5 @@
 import nextConnect from 'next-connect'
+
 import auth from '../../middleware/auth'
 import passport from '../../lib/passport'
 
@@ -6,57 +7,42 @@ import connectDB from '../../lib/mongodb';
 
 import errorHandler from './error-handler'
 
+import emailValidator from '../../lib/emailVaildator'
+
+
 const handler = nextConnect()
 
-handler.use(auth).post(async (req, res, next) => {
-  await connectDB()
-  passport.authenticate('local', (err, user, info) => {
-    console.log("user ", user);
-    console.log("info ", info);
+handler.use(auth).post(
+  async (req, res, next) => {
+    await connectDB();
 
-    if (err) { return errorHandler(err, res) }
+    if (emailValidator(req, res, 'email', 'password')) {
+      return
+    }
 
-    if (user) {
-      if (user.isVerified) {
-        return res.status(200).send({
-          userId: user._id,
-          msg: [`Your have successfully logged in;`, `Welcome to Hillfinder!`]
+    passport.authenticate('local', (err, user, info) => {
+
+      if (err) { return errorHandler(err, res) }
+
+      if (user) {
+        if (user.isVerified) {
+          return res.status(200).send({
+            userId: user._id,
+            msg: `Your have successfully logged in; Welcome to Hillfinder!`
+          });
+        }
+        return res.status(403).send({
+          msg: 'Your username has not been verified! Check your email for a confirmation link.'
         });
       }
-      return res.status(403).send({
-        msg: [
-          'Your username has not been verified!',
-          'Check your email for a confirmation link.'
-        ]
-      });
 
+      else if (user === false) {
+        return res.status(404).send({
+          msg: `We were unable to find this user. Please confirm with the "Forgot password" link or the "Register" link below!`
+        })
+      }
 
-    }
-
-    else if (user === false) {
-      return res.status(404).send({
-        msg: [
-          `We were unable to find this user.`,
-          `Please confirm with the "Forgot password" link or the "Register" link below!`
-        ]
-      })
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  })(req, res, next);
-})
+    })(req, res, next);
+  })
 
 export default handler
