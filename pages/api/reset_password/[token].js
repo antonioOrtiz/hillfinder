06ffc42e22eb195459e6e32f7e52f1f093236f37
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-23 10:00:10 -0400

    changed responses to return strings

--- pages/api/reset_password/[token].js
+++ pages/api/reset_password/[token].js
@@ -1,19 +1,14 @@
 import nextConnect from 'next-connect'
 
 import auth from '../../../middleware/auth'
-import { check, body, validationResult } from 'express-validator'
 
 import errorHandler from '../error-handler'
 import User from '../../../models/User'
 import Token from '../../../models/Token'
 
-
 import connectDB from '../../../lib/mongodb';
 
 import { nodeMailerFunc } from '../../../utils/index'
-
-
-
 
 require('dotenv').config();
 
@@ -25,33 +20,22 @@ handler
   .use(auth)
   .post(async (req, res) => {
 
-
-    console.log("hasErrors ", hasErrors);
-    if (hasErrors) {
-      console.log('has errors!')
-      return res.status(422).json({
-        msg: [
-          'Please follow the validations above',
-          're-enter a proper email and/or password.'
-        ]
-      })
-    }
-
     const {
       query: { token },
-    } = req
+    } = req;
+
     try {
       Token.findOne({ token }, (err, innerToken) => {
         if (innerToken === null) {
           return res.status(401).send({
-            msg: ['We were unable to find a valid token. Your token my have expired.']
+            msg: 'We were unable to find a valid token. Your token my have expired.'
           });
         }
         if (innerToken) {
           User.findOne({ _id: innerToken._userId }, (err, user) => {
             if (!user) {
               return res.status(404).send({
-                msg: ['We were unable to find a user for this token.']
+                msg: 'We were unable to find a user for this token.'
               });
             } else if (user) {
               user.password = req.body.password;
@@ -68,7 +52,7 @@ handler
                   res
                 );
                 return res.status(201).send({
-                  msg: ['Your password has been changed!']
+                  msg: 'Your password has been changed!'
                 });
               });
             }
