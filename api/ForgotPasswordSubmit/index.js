c9bea849a96175597d402ef562ec6b08d7303df4
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-23 09:42:37 -0400

    added logic for 422 status code and changed how responseMessages is set

--- api/ForgotPasswordSubmit/index.js
+++ api/ForgotPasswordSubmit/index.js
@@ -9,7 +9,7 @@ export default function forgotPasswordSubmit(email, setEmail, setResponseMessage
     .then(response => {
       if (response.status === 200) {
         setEmail('');
-        setResponseMessage(response.data.msg);
+        setResponseMessage(message => message.concat(response.data.msg))
         setFormError(false);
         setFormSuccess(true);
         setIsLoading(false);
@@ -18,7 +18,27 @@ export default function forgotPasswordSubmit(email, setEmail, setResponseMessage
     .catch((error) => {
       if (error.response) {
         if (error.response.status === 404) {
-          setResponseMessage(error.response.data.msg);
+          setResponseMessage(message => message.concat(error.response.data.msg));
+          setFormError(true);
+          setFormSuccess(false);
+          setIsLoading(false);
+        }
+
+        if (error.response.status === 422) {
+          let str = '';
+          const errors = error.response.data.errors
+
+          const IsEmptyOrWhiteSpace = (str) => (str.match(/^\s*$/) || []).length > 0;
+
+          for (let i = 0; i < errors.length; i++) {
+            const { value, msg, param } = errors[i]
+
+            str += `${IsEmptyOrWhiteSpace(value) ? 'Spaces or an empty value' : `"${value}"`} is an ${msg.toLowerCase()} for the ${param} input. `
+          }
+
+          str += 'Check details above.'
+
+          setResponseMessage(str)
           setFormError(true);
           setFormSuccess(false);
           setIsLoading(false);
