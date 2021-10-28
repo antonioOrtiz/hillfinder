836b4f8a35542ae50a047d188090c7bf6cb6c909
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-28 09:34:20 -0400

    refactored emaiValidator middleware and login logic

--- pages/api/login.js
+++ pages/api/login.js
@@ -12,37 +12,39 @@ import emailValidator from '../../lib/emailVaildator'
 
 const handler = nextConnect()
 
-handler.use(auth).post(
-  async (req, res, next) => {
-    await connectDB();
+handler
+  .use(auth)
+  .post((req, res, next) => {
+    emailValidator(req, res, next, 'email', 'password');
+  },
+    async (req, res, next) => {
+      await connectDB();
 
-    emailValidator(req, res, next, 'email', 'password')
+      passport.authenticate('local', (err, user, info) => {
+        console.log("user ", user);
 
-    passport.authenticate('local', (err, user, info) => {
+        if (err) { return errorHandler(err, res) }
+        if (user === false) {
+          return res.status(404).send({
+            msg: `We were unable to find this user. Please confirm with the "Forgot password" link or the "Register" link below!`
+          })
 
-      if (err) { return errorHandler(err, res) }
-
-      if (user) {
-        if (user.isVerified) {
-          return res.status(200).send({
-            userId: user._id,
-            msg: `Your have successfully logged in; Welcome to Hillfinder!`
-          });
         }
-        return res.status(403).send({
-          msg: 'Your username has not been verified! Check your email for a confirmation link.'
-        });
-      }
+        if (user) {
+          if (user.isVerified) {
+            return res.status(200).send({
+              userId: user._id,
+              msg: `Your have successfully logged in; Welcome to Hillfinder!`
+            });
+          }
+          return res.status(403).send({
+            msg: 'Your username has not been verified! Check your email for a confirmation link.'
+          });
 
-      else if (user === false) {
+        }
+      })(req, res, next);
 
-        res.status(404).send({
-          msg: `We were unable to find this user. Please confirm with the "Forgot password" link or the "Register" link below!`
-        })
 
-      }
-
-    })(req, res, next);
-  })
+    })
 
 export default handler
