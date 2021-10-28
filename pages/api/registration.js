6b9777cfa1a955ee1b96838d98811e7fcd562900
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-28 09:28:12 -0400

    fixed logic for emailValidator

--- pages/api/registration.js
+++ pages/api/registration.js
@@ -17,40 +17,42 @@ const handler = nextConnect()
 
 handler
   .use(auth)
-  .post(async (req, res, next) => {
-    await connectDB();
+  .post((req, res, next) => {
+    emailValidator(req, res, next, 'email', 'password');
+  },
+    async (req, res, next) => {
+      await connectDB();
 
-    emailValidator(req, res, next, 'email', 'password')
 
-    let user = await User.findOne({ email: req.body.email });
-    try {
-      if (user) {
-        return res.status(409).send({
-          msg:
-            `The email address you have entered is already associated with another account.
+      let user = await User.findOne({ email: req.body.email });
+      try {
+        if (user) {
+          return res.status(409).send({
+            msg:
+              `The email address you have entered is already associated with another account.
             Please re-enter another email address.`
+          });
+        }
+        user = new User({
+          email: req.body.email,
+          password: req.body.password
         });
+        await user.save();
+        nodeMailerFunc(
+          user,
+          `Account Verification`,
+          `Hello, Welcome to Hillfinders! An app on the decline—er about declines!\nPlease verify your account by clicking the following link:\nhttp://${req.headers.host
+          }/confirmation`,
+          'verification email',
+          res
+        );
+        return res.status(201).send({
+          msg:
+            `Your user registration was successful. Please check your email to complete your registration!`
+        });
+      } catch (err) {
+        errorHandler(err, res)
       }
-      user = new User({
-        email: req.body.email,
-        password: req.body.password
-      });
-      await user.save();
-      nodeMailerFunc(
-        user,
-        `Account Verification`,
-        `Hello, Welcome to Hillfinders! An app on the decline—er about declines!\nPlease verify your account by clicking the following link:\nhttp://${req.headers.host
-        }/confirmation`,
-        'verification email',
-        res
-      );
-      return res.status(201).send({
-        msg:
-          `Your user registration was successful. Please check your email to complete your registration!`
-      });
-    } catch (err) {
-      errorHandler(err, res)
-    }
-  })
+    })
 
 export default handler;
