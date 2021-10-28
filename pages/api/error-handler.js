74559fa48edb8f1a735cc12a32d3dadb718553d3
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-10-28 09:32:27 -0400

    refactored logic removed cases which are handled elsewhere

--- pages/api/error-handler.js
+++ pages/api/error-handler.js
@@ -1,15 +1,3 @@
-
 export default function errorHandler(err, res) {
-  if (typeof (err) === 'string') {
-    const is404 = err.toLowerCase().endsWith('not found');
-    const statusCode = is404 ? 404 : 400;
-
-    return res.status(statusCode).json({ message: err });
-  }
-
-  if (err.name === 'UnauthorizedError') {
-    // jwt authentication error
-    return res.status(401).json({ message: 'Invalid Token' });
-  }
-  return res.status(500).json({ msg: [err.message] });
+  res.status(500).json({ msg: [err.message] });
 }
