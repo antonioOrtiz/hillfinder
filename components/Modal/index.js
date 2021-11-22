0c7220b5f81dfddb96863295dce4733a728a2b1c
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-11-22 11:06:09 -0500

    removed unused header

--- components/Modal/index.js
+++ components/Modal/index.js
@@ -24,12 +24,7 @@ export default function Modal({
               {/* content */}
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/* header */}
-                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-                  <h3 className="text-3xl font-semibold">
-                    Modal Title
-                  </h3>
-
-                </div>
+                <div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t" />
                 {/* body */}
                 <div className="relative p-6 flex-auto">
                   <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
