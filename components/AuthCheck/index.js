ae948b7ee6cc61ac7d20419a459623b063a302a0
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-11-22 10:52:57 -0500

    added logic to prevent a refresh of an authenticated user go to homepage; The desired behavior is to stay put

--- components/AuthCheck/index.js
+++ components/AuthCheck/index.js
@@ -1,26 +1,32 @@
-import Router from 'next/router'
+import Router, { useRouter } from 'next/router'
 
 import { useUser } from '../../lib/hooks'
 import dynamic from "next/dynamic";
 
 const Layout = dynamic(() => import('../Layout'));
 
+
 export const AuthCheck = ({ children }) => {
+  const { route, router } = useRouter();
+
   function Loading() {
     return (
       <Layout>
-        <span>
-          <i className="fas fa-circle-notch fa-spin fa-5x" />
-        </span >
-      </Layout>)
+        <div className="w-full h-full fixed block top-0 left-0 z-50">
+          <span
+            className="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0"
+          >
+            <i className="fas fa-circle-notch fa-spin fa-5x" />
+          </span>
+        </div>
+      </Layout >)
   }
   const { user } = useUser();
 
-  console.log("Router ", Router);
-
-  if (typeof window !== 'undefined' && user === undefined) {
-    Router.push('/')
+  if (user === undefined) {
+    router && Router.push('/')
   }
+
 
   if (!user) return <Loading />
 
