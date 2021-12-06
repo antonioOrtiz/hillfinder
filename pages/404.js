d0f477835f7ece4468ab4072633353d993c01b3b
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-12-06 12:39:44 -0500

    updated 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,23 +1,41 @@
 import dynamic from "next/dynamic";
-
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link';
 
 const Layout = dynamic(() => import('../components/Layout'), { ssr: false });
 
 export default function FourOhFour() {
-  const router = useRouter()
+  const router = useRouter();
+
+  useEffect(() => {
+
+    console.log("router.pathname ", router);
+
+  }, [])
+
   return (
     <Layout>
-      <h1>
-        404 - Page Not Found;
-      </h1>
-      <h2 className="primary-content" style={{ fontWeight: 'strong' }}>Sorry but the page{' '}
-        {router.pathname.substring(1)} could not be found</h2>{' '}
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-    </Layout>
+      <>
+        <div className="hero min-h-screen bg-base-200">
+          <div className="flex-col hero-content lg:flex-row-reverse">
+            <div>
+              <h1 className="mb-5 text-5xl text-white font-bold">
+                404 - Page Not Found;
+              </h1>
+              <p className="mb-5 text-white">
+                <span className="text-2xl">"/{router.asPath.substring(1)}"</span> could not be found.{' '}
+              </p>
+              <button className="btn btn-primary ">
+                <Link href="/">
+                  <a className="block text-center">Home</a>
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    </Layout >
   )
 
 }
