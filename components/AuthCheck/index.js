e05e2c0c96cda84a9c1cf26b00ea18bc863d7f37
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-12-09 10:31:17 -0500

    updated logic for file

--- components/AuthCheck/index.js
+++ components/AuthCheck/index.js
@@ -1,23 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useUser } from '../../lib/hooks'
 import { PageLoader } from '../../components/Loader'
 
 
 export const AuthCheck = ({ children }) => {
-  const router = useRouter()
-  const { user, isLoading, isError } = useUser()
+  const [Component, setComponent] = useState(null);
+  const router = useRouter();
+  const { isLoading, isError } = useUser()
 
   useEffect(() => {
     if (typeof window !== 'undefined' && isError === 'Unauthorized') router.push('/');
+    else setComponent(children)
   }, [isError])
 
   if (isLoading) return <PageLoader />
 
-  if (user) {
-    return children
-  }
-  return null
+  return Component
 
 }
 
