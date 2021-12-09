a4dbd241c7cc8ab409a970067ef8700699cadc42
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-12-09 10:59:24 -0500

    AuthCheck refactored

--- components/AuthCheck/index.js
+++ components/AuthCheck/index.js
@@ -1,22 +1,21 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { useUser } from '../../lib/hooks'
 import { PageLoader } from '../../components/Loader'
 
 
 export const AuthCheck = ({ children }) => {
-  const [Component, setComponent] = useState(null);
-  const router = useRouter();
-  const { isLoading, isError } = useUser()
+  const router = useRouter()
+  const { user, isLoading, isError } = useUser()
 
   useEffect(() => {
     if (typeof window !== 'undefined' && isError === 'Unauthorized') router.push('/');
-    else setComponent(children)
   }, [isError])
 
-  if (isLoading) return <PageLoader />
+  if (isLoading) return <PageLoader />// a loading component that prevents the page from rendering
 
-  return Component
-
+  if (user) {
+    return children
+  } return null
 }
 
