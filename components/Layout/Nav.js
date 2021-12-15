f4cb5bd9268a7fcea4d80844f41ed240762bcd15
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2021-12-15 16:54:37 -0500

    added id for key prop in LogOutAnchor

--- components/Layout/Nav.js
+++ components/Layout/Nav.js
@@ -3,21 +3,22 @@ import { useEffect } from 'react';
 
 export default function Nav({ mobile = true, uidispatch, user, handleClickOnInput }) {
   const nav = [
-    { id: 1, route: '/', name: 'Home' },
-    { id: 2, route: '/profile', name: 'Profile' },
-    { id: 3, route: '/dashboard', name: 'Dashboard' },
-    { id: 4, route: '/logout', name: 'Logout' },
-    { id: 5, route: '/login', name: 'Login' },
-    { id: 6, route: '/registration', name: 'Registration' }
+    { id: '1', route: '/', name: 'Home' },
+    { id: '2', route: '/profile', name: 'Profile' },
+    { id: '3', route: '/dashboard', name: 'Dashboard' },
+    { id: '4', route: '/logout', name: 'Logout' },
+    { id: '5', route: '/login', name: 'Login' },
+    { id: '6', route: '/registration', name: 'Registration' }
   ];
 
-  function LogOutAnchor({ route, name, isMobile }) {
+  function LogOutAnchor({ route, name, isMobile, id }) {
+
+    console.log("id ", id);
     if (route === '/logout' && isMobile) {
       console.log('foo')
-      return (<li>
+      return (<li key={id}>
         <Link
           href='#'
-          key={route.id}
         ><a
           role="button"
           onClick={() => { uidispatch({ type: 'showModal' }); handleClickOnInput() }}
@@ -25,36 +26,33 @@ export default function Nav({ mobile = true, uidispatch, user, handleClickOnInpu
         </Link>
       </li>)
     } else if (route !== '/logout' && isMobile) {
-      return (
-        <li>
-          <Link
-            href={route}
-            key={route.id}
-          >
-            <a
-              role="button"
-              onClick={() => { handleClickOnInput() }}
-            >{name}</a>
-          </Link>
-        </li>)
-    }
-    else if (route == '/logout' && !isMobile) {
-      return (
-        <li>
-          <Link
-            href='#'
-            key={route.id}
-          >
-            <a
-              role="button"
-              onClick={() => uidispatch({ type: 'showModal' })}
-            >{name}</a></Link></li>)
-    }
-    else if (route != '/logout' && !isMobile) {
-      return (<li>
+      return (<li key={id}>
+
         <Link
           href={route}
-          key={route.id}
+        >
+          <a
+            role="button"
+            onClick={() => { handleClickOnInput() }}
+          >{name}</a>
+        </Link>
+      </li>)
+    }
+    else if (route == '/logout' && !isMobile) {
+      return (<li key={id}>
+
+        <Link
+          href='#'
+        >
+          <a
+            role="button"
+            onClick={() => uidispatch({ type: 'showModal' })}
+          >{name}</a></Link></li>)
+    }
+    else if (route != '/logout' && !isMobile) {
+      return (<li key={id}>
+        <Link
+          href={route}
         ><a >{name}</a>
         </Link>
       </li>)
@@ -64,19 +62,17 @@ export default function Nav({ mobile = true, uidispatch, user, handleClickOnInpu
   return (
     <ul className={mobile ? "p-4 overflow-y-auto menu w-80 bg-base-100" : "menu horizontal"}>
       {
-        nav.map(({ route, name }) => (
-          <>
-            {
-              user && user.isVerified ?
-                ((route === '/login') || route === '/registration') ? null :
-                  <LogOutAnchor key={route.id} route={route} name={name} isMobile={mobile} />
-                :
-                ((route === '/profile') || route === '/dashboard' || route === '/logout') ? null :
-                  <LogOutAnchor key={route.id} route={route} name={name} isMobile={mobile} />
-            }
-          </>
-        ))
+        nav.map(({ route, name, id }) => <>
+          {
+            user && user.isVerified ?
+              ((route === '/login') || route === '/registration') ? null :
+                <LogOutAnchor id={id} route={route} name={name} isMobile={mobile} />
+              :
+              ((route === '/profile') || route === '/dashboard' || route === '/logout') ? null :
+                <LogOutAnchor id={id} route={route} name={name} isMobile={mobile} />
+          }
+        </>)
       }
-    </ul >
+    </ul>
   )
 }
