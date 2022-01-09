0ab2665978e9fc66cbb95c3ca73560baaee18946
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-01-09 13:48:49 -0500

    formatting update

--- pages/confirmation/[token].js
+++ pages/confirmation/[token].js
@@ -2,11 +2,11 @@ import dynamic from "next/dynamic";
 
 const Form = dynamic(() => import('../../components/Form'));
 
+
 export default function Confirmation() {
   return (
     <>
       <Form formType="Confirmation" />
-
     </>
   )
 }
