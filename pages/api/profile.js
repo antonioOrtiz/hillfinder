deda516fd170669e0a8be90d6564767b8e4087cc
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-01-29 11:14:52 -0500

    put route now part of profile api

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -3,6 +3,8 @@ import auth from '../../middleware/auth'
 
 import Profile from '../../models/Profile'
 import errorHandler from './error-handler'
+import emailValidator from '../../lib/emailVaildator'
+
 
 const handler = nextConnect()
 
@@ -18,5 +20,19 @@ handler
         return res.status(201).send(profile)
       });
   })
+  .put(async (req, res, next) => {
+    emailValidator(req, res, next, 'email');
+    Profile.findOneAndUpdate({ id: req.user.id })
+      .populate('_user', 'email')
+
+      .exec((err, profile) => {
+        if (err) return errorHandler(err);
+
+        console.log("req ", req.body);
+        console.log('res', profile);
+
+      });
+  },
+  )
 
 export default handler
