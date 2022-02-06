dc0532b46306093a474b135adbe78252bc2f52eb
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-02-06 16:11:36 -0500

    Broke Frorm Response into component

--- components/Form/FormElements.js
+++ components/Form/FormElements.js
@@ -6,7 +6,6 @@ import dynamic from 'next/dynamic'
 import React, { useState, useEffect, useRef } from 'react';
 
 import { Loader } from '../Loader/index'
-
 import { useToggle } from '../../lib/hooks'
 
 const Message = dynamic(
@@ -16,94 +15,113 @@ const Message = dynamic(
   }
 )
 
+
+function FormResponse({
+  accountNotVerified,
+  emailDup,
+  errorType,
+  formType,
+  formError,
+  formSuccess,
+  responseMessage,
+  tokenExpired
+}) {
+  if (formType === 'Profile') {
+    if (formSuccess) {
+      <Message state="SuccessAlert"
+        content={"You profile information has been saved!"}
+      />
+    } else {
+      <Message state="Error"
+        content={responseMessage}
+      />
+    }
+  }
+
+  if (formError && responseMessage) {
+    return (
+      <Message state="Error"
+        content={responseMessage}
+      />
+    )
+  } else if (emailDup) {
+    return (<
+      Message state="Warning"
+      content={responseMessage}
+    />
+    )
+  } else if (accountNotVerified && !formError && responseMessage) {
+    return (
+      <Message state="Warning"
+        content={responseMessage}
+      />
+    )
+  } else if (tokenExpired) {
+    return (
+      <Message state="Warning"
+        header={'Warning'}
+        content={'This token is expired.'}
+      />
+    )
+  } else if (formSuccess && responseMessage) {
+    return (
+      <Message state="Success"
+        content={responseMessage}
+      />
+    )
+  } else return errorType
+    ? <Message
+      state="Error"
+      header="Error"
+      content={responseMessage}
+    /> : null;
+}
+
 function UserNameComponent({ label, name, value, placeholder, errorType, messageContent, changeHandler }) {
   return (
-    <div className="relative w-full mb-3">
-      <label
-        className="block uppercase text-black-700 text-xs font-bold mb-2"
-        htmlFor="grid-password"
-      >
-        {label}
+    <div className="relative w-full mb-3" >
+      <label className="block uppercase text-black-700 text-xs font-bold mb-2" htmlFor="grid-password" > {label}
       </label>
-      <input
-        type="email"
+      <input type="email"
         name={name}
         className="border-0 px-3 py-3 placeholder-gray-400 text-black-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
         placeholder={placeholder} /* "E-mail address, e.g. joe@schmoe.com" */
-        style={{ transition: "all .15s ease" }}
+        style={
+          { transition: "all .15s ease" }
+        }
         value={value}
         onChange={changeHandler}
       />
-      {errorType ? <Message
-        state="Error"
-        header="Error"
-        content={messageContent}
-      /> : null}
-    </div>
-  )
-}
 
-function PasswordComponent({ label, name, value, placeholder, errorType, messageContent, changeHandler }) {
-  const [inputType, setInputType] = useState('password');
-  const [passwordLabel, setpasswordLabel] = useState('show');
-
-  const handleChangePasswordToggle = () => {
-    if (inputType === 'password') {
-      setpasswordLabel('hide');
-      setInputType('text');
-    } else {
-      setpasswordLabel('show');
-      setInputType('password');
-    }
-  }
-  return (
-    <div className="relative w-full mb-3">
-      <div className="absolute top-8 right-0 flex items-center px-2">
-        <input className="hidden js-password-toggle" type="checkbox" />
-        <label className="bg-gray-300 hover:bg-gray-400 rounded px-2 py-1 text-sm text-black-600 font-mono cursor-pointer js-password-label" onClick={handleChangePasswordToggle}>{passwordLabel}</label>
-      </div>
-      <label
-        className="block uppercase text-black-700 text-xs font-bold mb-2"
-        htmlFor="grid-password"
-      >
-        {label}
-      </label>
-      <input
-        name={name}
-        type={inputType}
-        value={value}
-        className="border-0 px-3 py-3 placeholder-gray-400 text-black-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-        placeholder={placeholder}
-        style={{ transition: "all .15s ease" }}
-        onChange={changeHandler}
-        autoComplete="on"
+      <FormResponse errorType={errorType}
+        messageContent={messageContent}
       />
-      {errorType ? <Message
-        state="Error"
-        header="Error"
-        content={messageContent}
-      /> : null}
-    </div>
+    </div >
   )
 }
+
 
 export default function GenericInputForm({
+  buttonName,
+
+  disableButton,
+
+  handleSubmit,
+
   formType,
   formError,
   formSuccess,
   accountNotVerified,
-  disableButton,
   email,
   emailDup,
-  handleChange = () => { },
-  handleSubmit,
+  handleChange,
   emailError,
   emailFeedback,
-  notInterestedActivitiesRef,
   isLoading,
+  interestedActivities,
   interestedActivitiesInput,
-  interestedActivities = [],
   memberSince,
+  notInterestedActivitiesRef,
   password,
   passwordConfirmation,
   passwordConfirmationError,
@@ -112,14 +130,11 @@ export default function GenericInputForm({
   passwordFeedback,
   profileDataFromApi,
   setDisableButton,
-  buttonName,
-  responseMessage,
-  setIsLoading = () => { },
-  setInterestedActivities,
   setInterestedActivitiesInput,
   setProfileDataFromApi,
+  responseMessage,
+  setIsLoading = () => { },
   tokenExpired,
-  updateProfileSubmit
 }) {
 
   const fileSelect = useRef(null);
@@ -143,7 +158,7 @@ export default function GenericInputForm({
 
   function handleEditOrSaveButtonForProfile(e) {
     if (profileEditSaveButtonRef.current.innerText === 'SAVE') {
-      updateProfileSubmit(e, formType)
+      handleSubmit(e, formType)
     }
     setIsProfileInEditMode()
   }
@@ -153,7 +168,6 @@ export default function GenericInputForm({
       console.log(files[i]);
     }
   }
-
 
   function handleClick(e, id) {
     e.target.previousElementSibling?.focus()
@@ -165,7 +179,7 @@ export default function GenericInputForm({
   function handleKeyUp(e) {
     if (e.keyCode === 188) {
       const newArr = [...interestedActivities];
-      newArr[0].value.push(e.target.value.slice(0, - 1));
+      newArr[0].value.push(e.target.value.slice(0, -1));
       setInterestedActivities(newArr)
       setInterestedActivitiesInput('')
       console.log("interestedActivities 169", interestedActivities);
@@ -179,8 +193,53 @@ export default function GenericInputForm({
     e.target.value
   }
 
-
   useEffect(() => console.log("interestedActivities ", interestedActivities), [])
+
+  function PasswordComponent({ label, name, value, placeholder, errorType, messageContent, changeHandler }) {
+    const [inputType, setInputType] = useState('password');
+
+    const [passwordLabel, setpasswordLabel] = useState('show');
+
+    const handleChangePasswordToggle = () => {
+      if (inputType === 'password') {
+        setpasswordLabel('hide');
+        setInputType('text');
+      } else {
+        setpasswordLabel('show');
+        setInputType('password');
+      }
+    }
+    return (
+      <div className="relative w-full mb-3">
+        <div className="absolute top-8 right-0 flex items-center px-2">
+          <input className="hidden js-password-toggle" type="checkbox" />
+          <label className="bg-gray-300 hover:bg-gray-400 rounded px-2 py-1 text-sm text-black-600 font-mono cursor-pointer js-password-label" onClick={handleChangePasswordToggle}>{passwordLabel}</label>
+        </div>
+        <label
+          className="block uppercase text-black-700 text-xs font-bold mb-2"
+          htmlFor="grid-password"
+        >
+          {label}
+        </label>
+        <input
+          name={name}
+          type={inputType}
+          value={value}
+          className="border-0 px-3 py-3 placeholder-gray-400 text-black-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+          placeholder={placeholder}
+          style={{ transition: "all .15s ease" }}
+          onChange={changeHandler}
+          autoComplete="on"
+        />
+
+        {errorType ? <Message
+          state="Error"
+          header="Error"
+          content={messageContent}
+        /> : null}
+      </div>
+    )
+  }
 
   return (
     <>
@@ -202,15 +261,28 @@ export default function GenericInputForm({
               noValidate
               onSubmit={e => handleSubmit(e, formType)}
             >
-              {UserNameComponent({
-                label: "Email",
-                name: "email",
-                value: email,
-                placeholder: "E-mail address, e.g.joe@schmoe.com",
-                errorType: emailError,
-                messageContent: emailFeedback,
-                changeHandler: handleChange
-              })}
+              <div className="relative w-full mb-3">
+                <label
+                  className="block uppercase text-black-700 text-xs font-bold mb-2"
+                  htmlFor="grid-password"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  className="border-0 px-3 py-3 placeholder-gray-400 text-black-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                  placeholder="E-mail address, e.g. joe@schmoe.com"
+                  style={{ transition: "all .15s ease" }}
+                  value={email}
+                  onChange={handleChange}
+                />
+                {emailError ? <Message
+                  state="Error"
+                  header="Error"
+                  content={emailFeedback}
+                /> : null}
+              </div>
 
               <div className="text-center mt-6">
                 <button
@@ -241,6 +313,7 @@ export default function GenericInputForm({
                 />
                 : null}
             </form>
+
           </div>
 
           {{
@@ -259,73 +332,46 @@ export default function GenericInputForm({
 
         Profile:
           <div className="px-4 gap-3 grid">
-            {console.log(288)}
-            {profileEditSaveButtonRef.current?.innerText === 'SAVE' && <Message state="SuccessAlert" content={"You profile information has been saved!"} />}
-            <div className="card-body card glass ">
-              <div className="avatar online flex justify-center items-center">
+            <FormResponse
+              errorType={emailError}
+              messageContent={responseMessage}
+            />
+
+            {profileEditSaveButtonRef.current?.innerText === 'SAVE' && <Message state="SuccessAlert"
+              content={"You profile information has been saved!"}
+            />}
+            <div className="card-body card glass p-0">
+              <div className="py-5 avatar online flex justify-center">
                 <div className="rounded-full w-24 h-24">
                   <label className="cursor-pointer mt-6">
-
-                    <input
+                    <
+                      input
                       ref={fileSelect}
                       type="file"
                       accept="image/*"
                       className="hidden"
-                      onChange={(e) => handleFiles(e.target.files)}
-                    />
-                    <img onClick={handleImageUpload} src="http://daisyui.com/tailwind-css-component-profile-1@94w.png" />
+                      onChange={
+                        (e) => handleFiles(e.target.files)
+                      }
+                    /> <img
+                      onClick={handleImageUpload}
+                      src="http://daisyui.com/tailwind-css-component-profile-1@94w.png" />
                   </label>
 
                 </div>
-                <p className="ml-2  text-sm  text-textColor">Member Since: <br />{memberSince}</p>
               </div>
+              <hr className="divider glass h-px"></hr>
+              <button ref={profileEditSaveButtonRef}
+                className="mt-5 w-40 self-center inline-block text-textColor btn btn-outline btn-primary"
+                onClick={handleEditOrSaveButtonForProfile}> {isProfileInEditMode ? 'Edit Profile' : 'Save'} </button>
 
-              <button ref={profileEditSaveButtonRef} className="mt-5 w-40 self-center inline-block text-textColor btn btn-outline btn-primary" onClick={handleEditOrSaveButtonForProfile}>{isProfileInEditMode ? 'Edit Profile' : 'Save'}</button>
 
-              <div className="divider glass h-px " />
               <div className="px-4 mt-5">
-                <p className="text-sm text-textColor block mb-5">Click on text e.g. "name, email" etc. to upate values.</p>
-              </div>
+                <p className="text-sm text-textColor block mb-5"> Click on text e.g.
+                  "name, email" etc.to upate values. </p> </div>
 
-              <div className="px-4 ">
-                <ul className="list-none">
-                  {profileDataFromApi && profileDataFromApi.map((data, index) => {
-                    const { id, name, value } = data
-                    return (
-                      <li key={id} className="text-textColor mt-1 mb-5 w-full">
-                        <span className="block">{`${name}:`}{' '}</span>
-                        <input
-                          id={id}
-                          disabled={isProfileInEditMode}
-                          ref={notInterestedActivitiesRef}
-                          type="text"
-                          value={value}
 
-                          onChange={(e) => handleProfileDataFromApiHandleChange(index, e)}
-                          className={`${isProfileInEditMode ? "bg-isDisabled opacity-75 text-isDisabledTextColor" : "bg-input text-textColor"} mt-2 mb-1 inline-block  border-0 px-3 py-3  rounded focus:outline-none focus:ring w-full`}
-                        />
-                      </li>
-                    );
-                  }, this)}
-                </ul>
-                <ul className="mt-3">
-                  <li className="text-textColor" onClick={handleClick}>Interested activities: <span className='text-sm'><br />Accept comma separated input:</span></li>
-                  <input
-                    name="interested_activities"
-                    type="text"
-                    disabled={isProfileInEditMode}
-                    value={interestedActivitiesInput}
-                    onChange={(e) => handleChange(e)}
-                    onKeyUp={handleKeyUp}
-                    className={`${isProfileInEditMode ? "bg-isDisabled opacity-75 text-isDisabledTextColor" : "bg-input text-textColor"} mt-2 mb-1 inline-block  border-0 px-3 py-3  rounded focus:outline-none focus:ring w-full`}
-                  />
-                </ul>
-                <div className="mt-3">
-                  <p className="text-textColor">{interestedActivities[0] != undefined && interestedActivities[0]?.value && interestedActivities[0].value.join(', ')}</p>
-                </div>
-              </div>
-            </div>
-          </div>,
+            </div></div>,
 
         UpdatePassword: <div className="container px-4 max-w-md ">
           <div className="relative flex flex-col min-w-0 break-words shadow-lg rounded-lg bg-gray-300  bg-opacity-50 flex-auto px-4 py-5 pt-0">
@@ -370,31 +416,17 @@ export default function GenericInputForm({
                 </button>
               </div>
 
+
               {isLoading ? <div className="mt-4 flex justify-center items-center">
                 <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32" />
               </div> : null}
 
-              {formError && responseMessage
-                ? <Message
-                  state="Error"
-                  content={responseMessage}
-                />
-                : null}
 
-              {tokenExpired
-                ? <Message
-                  state="Waring"
-                  header={'Warning'}
-                  content={'This token is expired.'}
-                />
-                : null}
-
-              {formSuccess && !formError && responseMessage
-                ? <Message
-                  state="Success"
-                  content={responseMessage}
-                />
-                : null}
+              <FormResponse
+                tokenExpired={tokenExpired}
+                formSuccess={formSuccess}
+                formError={formError}
+                content={responseMessage} />
             </form>
 
           </div>
@@ -412,7 +444,9 @@ export default function GenericInputForm({
 
               : null
           }[formType]}
-        </div>
+
+
+        </div>,
       }[formType] || <div className="container px-4 max-w-md ">
 
           <div className="relative flex flex-col min-w-0 break-words shadow-lg rounded-lg bg-gray-300  bg-opacity-50 flex-auto px-4 py-5 pt-0">
@@ -467,34 +501,13 @@ export default function GenericInputForm({
                 <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32" />
               </div> : null}
 
-              {accountNotVerified && !formError && responseMessage
-                ? <Message
-                  state="Waring"
-                  content={responseMessage}
-                />
-                : null}
-
-
-              {/* {formError && responseMessage
-                ? <Message
-                  state="Error"
-                  content={responseMessage}
-                />
-                : null} */}
-
-              {emailDup || formError && responseMessage
-                ? <Message
-                  state="Error"
-                  content={responseMessage}
-                />
-                : null}
-
-              {formSuccess && responseMessage
-                ? <Message
-                  state="Success"
-                  content={responseMessage}
-                />
-                : null}
+              <FormResponse
+                accountNotVerified={accountNotVerified}
+                emailDup={emailDup}
+                tokenExpired={tokenExpired}
+                formSuccess={formSuccess}
+                formError={formError}
+                responseMessage={responseMessage} />
 
             </form>
 
@@ -520,6 +533,8 @@ export default function GenericInputForm({
               </div>
               : null,
           }[formType]}
+
+
         </div>}
     </>
   )
