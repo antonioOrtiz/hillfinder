8139e15931f4dc6cec9dc52c6eda6dc097d93dab
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-03-05 10:22:38 -0500

    change to line break

--- pages/login.jsx
+++ pages/login.jsx
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 const Form = dynamic(() => import('../components/Form'));
 
 export default function Login() {
+
   return (
     <>
       <Form formType="Login" />
