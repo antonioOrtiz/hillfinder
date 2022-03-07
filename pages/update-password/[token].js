81a999cd43cfb1d4370bf7946fefdcca6a4e3cb3
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-03-07 18:19:18 -0500

    Change to folder naming convention

--- pages/update-password/[token].js
+++ pages/update-password/[token].js
@@ -1,7 +1,7 @@
 import dynamic from "next/dynamic";
 
 const Layout = dynamic(() => import('../../components/Layout'));
-const Form = dynamic(() => import('../../components/Form'));
+const Form = dynamic(() => import('../../components/Forms'));
 
 export default function UpdatePassword() {
   return (
