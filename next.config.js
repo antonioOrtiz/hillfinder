091b3ed61f12cc4ceca5f26e6b95434ca2fb4f99
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-06 21:19:57 -0400

    fix to module import issue

--- next.config.js
+++ next.config.js
@@ -32,8 +32,10 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  webpack5: true,
-
+  future: {
+    webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
+    // Looks like backward compatibility approach.
+  },
   webpack: (config, { isServer }) => {
 
     if (!isServer) {
