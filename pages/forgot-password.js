16a207032311712fb3ffaf8eea3ed7fa042eba38
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-06 22:12:08 -0400

    fix to import issued

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 
-const Form = dynamic(() => import('../components/Forms'));
+const Form = dynamic(() => import('../components/Forms/ForgotPassword/index'));
 export default function ForgotPassword() {
   return (
     <>
