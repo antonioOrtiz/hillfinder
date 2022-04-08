54c259ef862c9ad24578ec83f8e77bc5674c620f
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-08 12:00:52 -0400

    remove unused div

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,13 @@ import Home from './home'
 
 export default function Index() {
   return (
-    <div>
+    <>
       <Head>
         <title>Hillfinder!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Home />
-    </div>
+    </>
   )
 }
 
