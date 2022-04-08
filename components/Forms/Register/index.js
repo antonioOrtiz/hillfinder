fb89cdf531aa9f36bcfb6aa92d25e618b6db1235
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-08 12:08:36 -0400

    fix to layout centering issue

--- components/Forms/Register/index.js
+++ components/Forms/Register/index.js
@@ -38,69 +38,67 @@ export default function RegisterForm({
   }, [formSuccess])
 
   return (
-    mounted && <div className="container px-4 max-w-md ">
+    mounted && <>
 
-      <div className="relative flex flex-col min-w-0 break-words shadow-lg rounded-lg bg-gray-300  bg-opacity-50 flex-auto px-4 py-5 pt-0">
-        <div className="my-3">
-          <h6 className="text-green-900 text-sm text-center font-bold">
-            {
-              { Login: 'Login', Register: 'Register' }[formType]
-            }
-          </h6>
-        </div>
-        <hr className="mb-4 border-b-1 border-gray-500" />
-
-        <form
-          noValidate
-          onSubmit={e => {
-            handleSubmit(e, formType)
-          }}
-        >
-          {console.log("emailError ", emailError)}
-          <UserNameComponent
-            changeHandler={handleChange}
-            classNames={`border-0 px-3 py-3 placeholder-gray-400 text-black-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full`}
-            errorType={emailError}
-            label="Email"
-            name="email"
-            messageContent={emailFeedback}
-            placeholder="E-mail address, e.g.joe@schmoe.com"
-            value={email}
-          />
-
-          <PasswordComponent
-            label="Password"
-            name="password"
-            value={password}
-            placeholder={'Password'}
-            errorType={passwordError}
-            messageContent={passwordFeedback}
-            changeHandler={handleChange}
-          />
-
-          <div className="text-center mt-6">
-            <button
-              className="btn btn-primary w-full"
-              type="submit"
-              style={{ transition: "all .15s ease" }}
-              disabled={disableButton}
-              onClick={() => setIsLoading(true)}
-            >
-              Register
-            </button>
-          </div>
-
-          {isLoading ? <div className="mt-4 flex justify-center items-center">
-            <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32" />
-          </div> : null}
-          <FormResponse
-            accountNotVerified={accountNotVerified}
-            emailDup={emailDup}
-            formSuccess={formSuccess}
-            formError={formError}
-            responseMessage={responseMessage} />
-        </form>
+      <div className="my-3">
+        <h6 className="text-green-900 text-sm text-center font-bold">
+          {
+            { Login: 'Login', Register: 'Register' }[formType]
+          }
+        </h6>
       </div>
+      <hr className="mb-4 border-b-1 border-gray-500" />
+
+      <form
+        noValidate
+        onSubmit={e => {
+          handleSubmit(e, formType)
+        }}
+      >
+        {console.log("emailError ", emailError)}
+        <UserNameComponent
+          changeHandler={handleChange}
+          classNames={`border-0 px-3 py-3 placeholder-gray-400 text-black-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full`}
+          errorType={emailError}
+          label="Email"
+          name="email"
+          messageContent={emailFeedback}
+          placeholder="E-mail address, e.g.joe@schmoe.com"
+          value={email}
+        />
+
+        <PasswordComponent
+          label="Password"
+          name="password"
+          value={password}
+          placeholder={'Password'}
+          errorType={passwordError}
+          messageContent={passwordFeedback}
+          changeHandler={handleChange}
+        />
+
+        <div className="text-center mt-6">
+          <button
+            className="btn btn-primary w-full"
+            type="submit"
+            style={{ transition: "all .15s ease" }}
+            disabled={disableButton}
+            onClick={() => setIsLoading(true)}
+          >
+            Register
+          </button>
+        </div>
+
+        {isLoading ? <div className="mt-4 flex justify-center items-center">
+          <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32" />
+        </div> : null}
+        <FormResponse
+          accountNotVerified={accountNotVerified}
+          emailDup={emailDup}
+          formSuccess={formSuccess}
+          formError={formError}
+          responseMessage={responseMessage} />
+      </form>
 
       {formError
         ? <div className="flex flex-wrap mt-6">
@@ -120,6 +118,6 @@ export default function RegisterForm({
           </div>
         </div>
         : null}
-    </div>
+    </>
   );
 }
