8671227fcf7ea20104b5bbe91a6f6dd58548bb7e
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-14 17:51:03 -0400

    remove the unused props

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,7 @@ import dynamic from "next/dynamic";
 
 const Form = dynamic(() => import('../components/Forms'));
 
-export default function Login({ }) {
+export default function Login() {
   return (
     <>
       <Form formType="Login" />
