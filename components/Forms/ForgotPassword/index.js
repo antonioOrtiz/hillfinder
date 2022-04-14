3e583ed66dd4661d456ff1b81d943500e9260580
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-14 17:57:53 -0400

    update to props passed, styles and logic for children components

--- components/Forms/ForgotPassword/index.js
+++ components/Forms/ForgotPassword/index.js
@@ -8,37 +8,34 @@ import {
 
 export default function ForgotPasswordForm({
   disableButton,
-  formType,
-  formError,
-  formSuccess,
   email,
   emailError,
   emailFeedback,
+  formError,
+  formType,
+  formSuccess,
   handleChange,
   handleSubmit,
   isLoading,
   responseMessage,
-  tokenExpired,
-  setIsLoading = () => { },
 }) {
+
   return (
     <>
-      <div className="my-3">
-        <h6 className="text-green-900 text-sm text-center font-bold">
-          Forgot Password
-        </h6>
-        <p> Not a problem. Just enter your email address below. If it's registered
+      <div className="my-3 h-max"><h6 className="text-green-900 text-sm text-center font-bold">Forgot Password</h6>
+        <p className="my-3 text-white text-sm"> Not a problem. Just enter your email address below. If it's registered
           with Hillfinder, we'll send you a link to reset your password.</p>
       </div>
-      <hr className="mb-4 border-b-1 border-gray-500" />
 
+
+      <hr className="mb-4 border-b-1 border-gray-500" />
       <form
         noValidate
         onSubmit={e => handleSubmit(e, formType)}
       >
         <UserNameComponent
-          changeHandler={handleChange}
-          classNames={`border-0 px-3 py-3 placeholder-gray-400 text-black-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full`}
+          handleChange={handleChange}
+          classNames={`border-0 px-3 mt- py-3 placeholder-gray-400 text-black-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full`}
           errorType={emailError}
           label="Email"
           name="email"
@@ -53,7 +50,6 @@ export default function ForgotPasswordForm({
             type="submit"
             style={{ transition: "all .15s ease" }}
             disabled={disableButton}
-            onClick={() => setIsLoading(true)}
           >
             Forgot password
           </button>
@@ -62,17 +58,16 @@ export default function ForgotPasswordForm({
 
         {isLoading ? <div className="mt-4 flex justify-center items-center">
           <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32" />
-        </div> : null}
-
-        <FormResponse
-          tokenExpired={tokenExpired}
+        </div> : <FormResponse
           formSuccess={formSuccess}
           formError={formError}
-          content={responseMessage} />
+          responseMessage={responseMessage} />}
+
+
       </form>
 
 
-      <div className="w-1/2 text-left">
+      <div className="w-1/2 text-left mt-6">
         <Link href="/registration">
           <a className="text-white">
             <small>Create new account</small>
