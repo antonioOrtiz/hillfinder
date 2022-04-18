4a294a4e6751e1bd8434c0aed07652dd72f7bc1a
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-18 12:21:26 -0400

    FormWrapper component now being used

--- components/Forms/ForgotPassword/index.js
+++ components/Forms/ForgotPassword/index.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import {
   FormResponse,
+  FormWrapper,
   UserNameComponent,
 } from '../FormElements';
 
@@ -17,11 +18,12 @@ export default function ForgotPasswordForm({
   handleChange,
   handleSubmit,
   isLoading,
+  mounted,
   responseMessage,
 }) {
 
   return (
-    <>
+    mounted && <FormWrapper>
       <div className="my-3 h-max"><h6 className="text-green-900 text-sm text-center font-bold">Forgot Password</h6>
         <p className="my-3 text-white text-sm"> Not a problem. Just enter your email address below. If it's registered
           with Hillfinder, we'll send you a link to reset your password.</p>
@@ -62,10 +64,7 @@ export default function ForgotPasswordForm({
           formSuccess={formSuccess}
           formError={formError}
           responseMessage={responseMessage} />}
-
-
       </form>
-
 
       <div className="w-1/2 text-left mt-6">
         <Link href="/registration">
@@ -74,6 +73,6 @@ export default function ForgotPasswordForm({
           </a>
         </Link>
       </div>
-    </>
+    </FormWrapper>
   );
 }
