b72d3550437eb3d2d2eb64a855a3cd2e9c5c81a6
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-18 12:26:28 -0400

    update path to reflect new architecture

--- pages/update-password/[token].js
+++ pages/update-password/[token].js
@@ -1,6 +1,5 @@
 import dynamic from "next/dynamic";
 
-const Layout = dynamic(() => import('../../components/Layout'));
 const Form = dynamic(() => import('../../components/Forms'));
 
 export default function UpdatePassword() {
