6b5c2301836b544feffae96cd1eac59d7c88549c
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-18 12:24:27 -0400

    consolidate Loader component

--- components/Loader/index.js
+++ components/Loader/index.js
@@ -1,10 +1,5 @@
 export const Loader = () => (
-  <div className=" flex justify-center items-center">
+  <div className="grid place-items-center h-screen">
     <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32" />
   </div>
-)
-
-export const PageLoader = () => (
-  <div className=" loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32" />
-
 )
