62de7ddda7b2a46754e0edf02e8f47e4a54fd690
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-23 15:25:32 -0400

    move some of the logic to be closer to component

--- components/Forms/Profile/index.js
+++ components/Forms/Profile/index.js
@@ -1,7 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import dynamic from 'next/dynamic'
+import moment from 'moment';
+import getProfile from '../../../clientApi/GetProfile'
 
 import { Loader } from '../../Loader'
+
+import { useUser } from '../../../lib/hooks'
+
+import { userState } from '../../Context/UserContext'
+
+import {
+  FormWrapper
+} from '../FormElements';
 
 const Message = dynamic(
   () => import('../../Message'),
@@ -20,9 +30,8 @@ export default function ProfileForm({
   formSuccess,
   handleChange,
   handleSubmit,
-  handleCancelSaveProfileForUseCallback,
+  interestedActivities,
   isProfileInEditMode,
-  memberSince,
   mounted,
   profileEmail,
   profileDisplayName,
@@ -30,121 +39,157 @@ export default function ProfileForm({
   profileDisplayNameFeedback,
   profileUserAvatar,
   responseMessage,
+  setEmailError,
+  setFormError,
   setInterestedActivities,
+  setProfileDisplayName,
+  setProfileDisplayNameError,
+  setProfileEmail,
+  setProfileUserAvatar,
   toggle
 }) {
-  const SaveButton = useRef(null)
+  const [memberSince, setMemberSince] = useState('');
+  const [profileDataFromApi, setProfileDataFromApi] = useState([]);
 
-  const ProfileButton = `mb-5 mr-2.5 w-40 self-center inline-block text-textColor btn btn-primary`
-
+  const { userstate } = userState();
+  const { isLoggedIn } = userstate;
+  const { user } = useUser(!isLoggedIn ? false : true);
 
   useEffect(() => {
-    if (formError) {
-      SaveButton.current.classList.add('cursor-not-allowed', 'opacity-50')
-    }
-    return () => SaveButton.current.classList.remove('cursor-not-allowed', 'opacity-50')
-  }, [formError])
+    if (user !== undefined) {
+      const fetchProfile = async () => {
 
+        const { data } = await getProfile();
+
+        const email = data[0]._user.email
+        const obj = data[0];
+        const { __v, _user, _id, memberSince, ...profile } = obj;
+        const fomatted_date = moment(memberSince).format('MM/DD/YYYY');
+        const { displayName, userAvatar } = profile
+
+        setProfileDataFromApi({ ...profile, email })
+        setMemberSince(fomatted_date)
+        setProfileDisplayName(displayName)
+        setProfileEmail(email)
+        setInterestedActivities(profile.interestedActivities)
+        setProfileUserAvatar(userAvatar)
+      }
+      fetchProfile()
+    }
+  }, [])
+
+
+  useEffect(() => console.log("profileDataFromApi ", profileDataFromApi), [profileDataFromApi])
+
+
+  const ProfileButton = `mb-5 mr-2.5 self-center inline-block text-textColor btn btn-primary`
+  const SaveButton = `${formError || formSuccess ? "'cursor-not-allowed', 'opacity-50'" : null} mb-5 mr-2.5 self-center inline-block text-textColor btn btn-primary`
+
+  useEffect(() => {
+    if (formSuccess && formError === false) {
+      toggle()
+    }
+  }, [formError, formSuccess])
+
+  function handleCancelSaveProfileForUseCallback() {
+    setProfileDisplayName(profileDataFromApi.displayName)
+    setProfileEmail(profileDataFromApi.email)
+    setFormError(false)
+    setEmailError(false)
+    setProfileDisplayNameError(false)
+    toggle()
+  }
 
   return (
-    mounted && <div className="grid h-screen place-items-center">
-      <div className="p-5 card-body glass card">
-        <div className="p-8 rounded-md shadow-inner bg-slate-50 drop-shadow-lg">
-          {formSuccess
-            ? <Message
-              state="Success"
-              header="Success"
-              content={responseMessage}
-            /> : null}
-          {formError
-            ? <Message
-              state="Error"
-              header="Error"
-              content={'Your update ran into a problem, see below for details.'}
-            /> : null}
-          <UserAvatarComponent
+    mounted &&
+    <FormWrapper>
+      <div className="p-4 rounded-md shadow-inner bg-slate-50 drop-shadow-lg">
+        {formSuccess
+          ? <Message
+            state="Success"
+            header="Success"
+            content={responseMessage}
+          /> : null}
+        {formError
+          ? <Message
+            state="Error"
+            header="Error"
+            content={'Your update ran into a problem, see below for details.'}
+          /> : null}
+        <UserAvatarComponent
+          isProfileInEditMode={isProfileInEditMode}
+          profileUserAvatar={profileUserAvatar}
+        />
+        <p className="mb-5 text-center text-profileColor"> Member since: <br /> {memberSince} </p>
+
+        <form
+          noValidate
+          onSubmit={e => {
+            handleSubmit(e, formType)
+          }}
+        >
+          <div className="text-center">
+            {isProfileInEditMode ?
+              <button
+                className={SaveButton + ' w-2/5'}
+                type="submit"
+              >
+                Save
+              </button> : null}
+
+            {isProfileInEditMode
+              ?
+              <button
+                className={ProfileButton + ' w-2/5'}
+                type="button"
+                onClick={handleCancelSaveProfileForUseCallback}
+              >
+                Cancel
+              </button>
+              :
+              <button
+                className={ProfileButton + ' w-40'}
+                type='button'
+                onClick={toggle}
+              >
+                Edit Profile
+              </button>}
+          </div>
+
+          <ProfileDisplayNameComponent
+            classNames={`${isProfileInEditMode ? "bg-white" : "opacity-50 cursor-not-allowed"} border-2 px-3 mt-1 py-3 rounded text-sm text-black shadow shadow-input focus:outline-none focus:ring w-full
+           `}
+            errorType={profileDisplayNameError}
+            handleChange={handleChange}
             isProfileInEditMode={isProfileInEditMode}
-            profileUserAvatar={profileUserAvatar}
+            label="Display name:"
+            name="profileDisplayName"
+            messageContent={profileDisplayNameFeedback}
+            value={profileDisplayName}
           />
-          <p className="mb-5 text-center text-profileColor"> Member since: <br /> {memberSince} </p>
 
-          <form
-            noValidate
-            onSubmit={e => {
-              handleSubmit(e, formType)
-            }}
-          >
-            <div className="text-center">
-              {isProfileInEditMode ?
-                <button
-                  ref={SaveButton}
-                  className={ProfileButton}
-                  type="submit"
-                >
-                  Save
-                </button> : null}
+          <ProfileEmailComponent
+            classNames={`${isProfileInEditMode ? "bg-white" : "opacity-50 cursor-not-allowed"} border-2 px-3 mt-1 py-3  rounded text-sm text-black shadow shadow-input focus:outline-none focus:ring w-full
+           `}
+            errorType={emailError}
+            handleChange={handleChange}
+            isProfileInEditMode={isProfileInEditMode}
+            label="Email"
+            name="profileEmail"
+            messageContent={emailFeedback}
+            placeholder="E-mail address, e.g.joe@schmoe.com"
+            value={profileEmail}
+            wrapperClassForProfileComponent={true}
+          />
 
-              {isProfileInEditMode
-                ?
-                <button
-                  className={ProfileButton}
-                  type="button"
-                  onClick={handleCancelSaveProfileForUseCallback}
-                >
-                  Cancel
-                </button>
-                :
-                <button
-                  className={ProfileButton}
-                  type='button'
-                  onClick={toggle}
-
-                >
-                  Edit Profile
-                </button>}
-            </div>
-
-            <ProfileDisplayNameComponent
-              classNames={`${isProfileInEditMode ? "text-profileColor" : null} border-0 px-3 mt-1 py-3 placeholder-gray-600  rounded text-sm shadow shadow-input focus:outline-none focus:ring w-full
-               `}
-              errorType={profileDisplayNameError}
-              handleChange={handleChange}
-              isProfileInEditMode={!isProfileInEditMode}
-              label="Display name:"
-              name="profileDisplayName"
-              messageContent={profileDisplayNameFeedback}
-              value={profileDisplayName}
-            />
-
-            <ProfileEmailComponent
-              classNames={`${isProfileInEditMode ? "text-profileColor" : null} border-0 px-3 mt-1 py-3 placeholder-gray-600  rounded text-sm shadow shadow-input focus:outline-none focus:ring w-full
-               `}
-              errorType={emailError}
-              handleChange={handleChange}
-              isProfileInEditMode={!isProfileInEditMode}
-              label="Email"
-              name="profileEmail"
-              messageContent={emailFeedback}
-              placeholder="E-mail address, e.g.joe@schmoe.com"
-              value={profileEmail}
-              wrapperClassForProfileComponent={true}
-            />
-
-            <InterestedActivitiesComponent
-              classNames={`${isProfileInEditMode
-                ? `focus:border-borderColorInEditMode`
-                : ` text-green-900 border-green-900  `}
-               w-full inline-block align-bottom bg-transparent
-               border-b-2 placeholder-gray-700 placeholder-opacity-50 outline-none text-white
-            `}
-              setInterestedActivities={setInterestedActivities}
-
-              isProfileInEditMode={!isProfileInEditMode}
-              label="Interested activities:"
-
-            />
-          </form>
-        </div>
-      </div></div>
+          <InterestedActivitiesComponent
+            interestedActivities={interestedActivities}
+            setInterestedActivities={setInterestedActivities}
+            isProfileInEditMode={isProfileInEditMode}
+            label="Interested activities:"
+          />
+        </form>
+      </div>
+    </FormWrapper>
   );
 }
