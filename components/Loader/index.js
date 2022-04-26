ca6331a71ff717b248b10e6933ab7d21dbe58c21
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-26 16:25:05 -0400

    change to Loader height

--- components/Loader/index.js
+++ components/Loader/index.js
@@ -1,5 +1,12 @@
 export const Loader = () => (
-  <div className="grid place-items-center h-screen">
+  <div className="grid place-items-center">
     <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32" />
   </div>
 )
+
+
+// export const Loader = () => (
+//   <div className="grid place-items-center h-screen">
+//     <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-32 w-32" />
+//   </div>
+// )
