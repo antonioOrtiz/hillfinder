736dd8e2bab052b49bbdae35a1742b5b3ad85871
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-04-27 12:52:45 -0400

    adde logic if user is auth

--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,27 @@
 import dynamic from "next/dynamic";
+import { useEffect } from "react";
+import { useRouter } from 'next/router'
 
-const Form = dynamic(() => import('../components/Forms'));
+const Form = dynamic(() => import('components/Forms'));
+
+import { isLoggedIn } from 'utils/index'
+
+import { useUser } from 'lib/hooks'
 
 export default function Login() {
+  const isLoggedInTrue = isLoggedIn();
+
+  const { user } = useUser(isLoggedInTrue);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // redirect to home if already logged in
+    if (user?.isVerified) {
+      router.replace('/', undefined, { shallow: true })
+    }
+  }, []);
+
   return (
     <>
       <Form formType="Login" />
--- pages/registration.js
+++ pages/registration.js
@@ -1,7 +1,28 @@
 import dynamic from "next/dynamic";
 
-const Form = dynamic(() => import('../components/Forms'));
-export default function FormComponent() {
+import { useEffect } from "react";
+import { useRouter } from 'next/router'
+
+const Form = dynamic(() => import('components/Forms'));
+
+import { isLoggedIn } from 'utils/index'
+
+import { useUser } from 'lib/hooks'
+
+export default function Registration() {
+  const isLoggedInTrue = isLoggedIn();
+
+  const { user } = useUser(isLoggedInTrue);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    // redirect to home if already logged in
+    if (user?.isVerified) {
+      router.replace('/', undefined, { shallow: true })
+    }
+  }, []);
+
   return (
     <>
       <Form formType="Register" />
