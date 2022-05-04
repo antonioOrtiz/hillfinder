d16f62985d15bd11cb5c15feb712be5cf80903cc
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-05-04 11:08:44 -0400

    made page for map dashboard

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,12 +1,9 @@
 import dynamic from "next/dynamic";
 
-import { useEffect } from "react";
-import { useRouter } from 'next/router'
-
 import { PageLoader } from 'components/Loader/index'
 
 
-const Map = dynamic(() => import('components/Map/index'), { loading: () => <PageLoader />, ssr: false });
+const Map = dynamic(() => import('components/Map'), { loading: () => <PageLoader />, ssr: false });
 
 export default function Dashboard() {
   return (
