a4f00cb6177b33ffd5424a060defa0b62e4c9969
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-05-04 11:20:43 -0400

    remove the old location component for imported one

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,52 +1,33 @@
-import { useEffect, useState } from 'react'
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
-import L from "leaflet";
+import { MapContainer, TileLayer } from 'react-leaflet'
 
-import icon from "./constants";
+import LocationMarker from './LocationMarker'
 
-function LocationMarker() {
-  const [position, setPosition] = useState(null);
-  const [bbox, setBbox] = useState([]);
+import { PageLoader } from 'components/Loader/index'
 
-  const map = useMap();
+import dynamic from "next/dynamic";
 
-  useEffect(() => {
-    map.locate().on("locationfound", function (e) {
-      setPosition(e.latlng);
-      map.flyTo(e.latlng, map.getZoom());
-      const radius = e.accuracy;
-      const circle = L.circle(e.latlng, radius);
-      circle.addTo(map);
-      setBbox(e.bounds.toBBoxString().split(","));
-    });
-  }, [map]);
-
-  return position === null ? null : (
-    <Marker position={position} icon={icon}>
-      <Popup>
-        You are here. <br />
-        Map bbox: <br />
-        <b>Southwest lng</b>: {bbox[0]} <br />
-        <b>Southwest lat</b>: {bbox[1]} <br />
-        <b>Northeast lng</b>: {bbox[2]} <br />
-        <b>Northeast lat</b>: {bbox[3]}
-      </Popup>
-    </Marker>
-  );
-}
-
+const SearchControl = dynamic(() => import('./SearchControl'), { loading: () => <PageLoader />, ssr: false });
 
 export default function Map() {
 
   return (
-    <MapContainer center={[50.5, 30.5]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={[37.0902, 95.7129]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         url={`https://api.mapbox.com/styles/v1/${process.env.MAPBOX_USERNAME}/${process.env.MAPBOX_STYLE_ID
           }/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`}
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
       />
-      <LocationMarker />
-
+      <LocationMarker startDirectly />
+      <SearchControl
+        showMarker={false}
+        showPopup={false}
+        popupFormat={({ query, result }) => result.label}
+        retainZoomLevel={false}
+        animateZoom={true}
+        autoClose={false}
+        searchLabel={"Enter address, please"}
+        keepResult={true}
+      />
     </MapContainer>
   )
 }
