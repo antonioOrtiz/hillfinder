f4a53ff11181e2f8cf9035f63b13ef01e43de812
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-05-11 10:46:43 -0400

    add geocoder css

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,8 @@ import { CloudinaryContext } from 'cloudinary-react'
 import 'tailwindcss/tailwind.css'
 import 'leaflet/dist/leaflet.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
+
 import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
-
-
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css'
 
 import 'styles/styles.scss'
