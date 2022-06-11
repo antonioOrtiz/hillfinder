8e1f413aba0053d884b8fcfdb15514eadab2faec
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2022-06-11 11:13:49 -0400

    update to logic to set starting/end points

--- components/Map/RoutingMachine/index.js
+++ components/Map/RoutingMachine/index.js
@@ -5,29 +5,21 @@ import { createControlComponent } from "@react-leaflet/core";
 import "leaflet-routing-machine";
 
 const createRoutineMachineLayer = ({ startingPoints }) => {
-  const [pointsOnLoad, setPointsOnLoad] = useState({})
+  const [pointsOnLoad, setPointsOnLoad] = useState([])
 
   function addDistanceToMaker(lat, lng) {
-    return GeometryUtil.destination(L.latLng(lat, lng), 180, 10);
+    return GeometryUtil.destination(L.latLng(lat, lng), 180, 20);
   }
 
   useEffect(() => {
-    var updatedPointsOnLoad = startingPoints.map(sp => {
-      return {
-        starting: L.latLng(...sp),
-        end: addDistanceToMaker(...sp)
-      }
-    })
-    setPointsOnLoad(updatedPointsOnLoad)
-  }, [startingPoints])
+    console.log("startingPoints ", startingPoints);
+  }, [])
 
 
-
-  console.log("pointsOnLoad ", pointsOnLoad);
   const instance = L.Routing.control({
     waypoints: [
-      L.latLng(40.74128501730291, -73.76121707494242),
-      L.latLng(40.74216368204567, -73.7596824213515)
+      L.latLng(...startingPoints),
+      L.latLng(addDistanceToMaker(...startingPoints))
     ],
     lineOptions: {
       styles: [{ color: "#6FA1EC", weight: 4 }]
@@ -39,9 +31,6 @@ const createRoutineMachineLayer = ({ startingPoints }) => {
     fitSelectedRoutes: true,
     showAlternatives: false
   });
-
-
-  console.log("instance ", instance);
 
   return instance;
 };
