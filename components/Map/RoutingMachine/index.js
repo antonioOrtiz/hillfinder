2f9411200c095d2ee33cf82c3d0363c5d9ccab39
Antonio PavicevacOrtiz <antonioPavicevacOrtiz@WKMZT887FC3A.global.publicisgroupe.net>
2022-07-18 12:45:16 -0400

    update: custom route work; augment the lat/lang between starting and end points in routing machine

--- components/Map/RoutingMachine/index.js
+++ components/Map/RoutingMachine/index.js
@@ -1,84 +1,99 @@
 import L from "leaflet";
-import { useEffect } from 'react'
-import { createControlComponent } from "@react-leaflet/core";
+import { useEffect } from "react";
+import { useMap } from "react-leaflet";
 import "leaflet-routing-machine";
-import { startIcon, finishIcon } from '../Icon/'
+import { startIcon, finishIcon } from "../Icon/";
 
-const createRoutineMachineLayer = ({ startingPoints, options }) => {
+export function RoutingMachine({ startingPoints }) {
+  const map = useMap();
 
-  console.log("L.latLng(startingPoints[0]?.highestEl?.latlng?.lat, startingPoints[0]?.highestEl?.latlng?.lng)", L.latLng(startingPoints[0]?.highestEl?.latlng?.lat, startingPoints[0]?.highestEl?.latlng?.lng));
+  const customCallback = (callback) => (context, error, routes) => {
+    for (const route of routes) {
+      route.coordinates = [...route.coordinates, L.latLng(12.33, 49.6753)];
+    }
 
+    callback(context, error, routes);
+  };
 
-  console.log("L.latLng(startingPoints[0]?.lowestEl?.latlng?.lat, startingPoints[0]?.lowestEl?.latlng?.lng) ", L.latLng(
-    startingPoints[0]?.lowestEl?.latlng?.lat,
-    startingPoints[0]?.lowestEl?.latlng?.lng
-  ));
-
-  const instance = L.Routing.control({
-    addWaypoints: false,
-
-    collapsible: true,
-    createMarker: function (i, wp, nWps) {
-      if (i === 0) {
-        return L.marker(wp.latLng, {
-          icon: startIcon,
-          draggable: true,
-          keyboard: true,
-          alt: 'current location'
-        })
-      }
-      if (i === nWps - 1) {
-        return L.marker(wp.latLng, {
-          icon: finishIcon,
-          draggable: true,
-          alt: 'current destination'
-        })
-      }
+  const router = L.Routing.OSRMv1.extend({
+    initialize: function (options) {
+      L.Routing.OSRMv1.prototype.initialize.call(this, options);
     },
-    draggableWaypoints: true,
 
-    fitSelectedRoutes: true,
+    route: function (waypoints, callback, context, options) {
+      console.log("waypoints ", waypoints);
+      console.log("callback ", callback);
+      console.log("context", context);
+      console.log("options ", options);
+      const originalCallback = options.callback;
 
-    geocoder: L.Control.Geocoder.nominatim(),
-
-    lineOptions: {
-      styles: [{ color: 'chartreuse', opacity: 1, weight: 5 }]
+      console.log("originalCallback ", originalCallback);
+      L.Routing.OSRMv1.prototype.route.call(
+        waypoints,
+        customCallback(originalCallback),
+        this,
+        options
+      );
     },
-    position: 'bottomright',
-    plan: new L.Routing.plan({
-      createMarker: function (i, wp, nWps) {
-        if (i === 0) {
-          console.log('wp', wp)
-        }
-        if (i === nWps - 1) {
-          console.log('wp', wp)
-        }
-      },
-    }),
-    routeWhileDragging: true,
+  });
 
+  useEffect(() => {
+    if (!map) return;
 
-    show: true,
-    showAlternatives: false,
-
-    waypoints: [
+    const waypoints = [
       L.latLng(
         startingPoints[0]?.highestEl?.latlng?.lat,
-        startingPoints[0]?.highestEl?.latlng?.lng),
+        startingPoints[0]?.highestEl?.latlng?.lng
+      ),
       L.latLng(
         startingPoints[0]?.lowestEl?.latlng?.lat,
         startingPoints[0]?.lowestEl?.latlng?.lng
       ),
-    ],
-  });
+    ];
 
+    const routingControl = L.Routing.control({
+      addWaypoints: false,
 
+      collapsible: true,
+      createMarker: function (i, wp, nWps) {
+        if (i === 0) {
+          return L.marker(wp.latLng, {
+            icon: startIcon,
+            draggable: true,
+            keyboard: true,
+            alt: "current location",
+          });
+        }
+        if (i === nWps - 1) {
+          return L.marker(wp.latLng, {
+            icon: finishIcon,
+            draggable: true,
+            alt: "current destination",
+          });
+        }
+      },
+      draggableWaypoints: true,
 
+      fitSelectedRoutes: true,
 
-  return instance;
-};
+      geocoder: L.Control.Geocoder.nominatim(),
 
+      lineOptions: {
+        styles: [{ color: "chartreuse", opacity: 1, weight: 5 }],
+      },
+      position: "bottomright",
 
-const RoutingMachine = createControlComponent(createRoutineMachineLayer);
+      routeWhileDragging: true,
+      router: new router(),
 
-export default RoutingMachine;
+      show: true,
+      showAlternatives: false,
+
+      waypoints,
+    }).addTo(map);
+
+    return () => map.removeControl(routingControl);
+  }, [map]);
+
+  return null;
+}
--- components/Map/index.js
+++ components/Map/index.js
@@ -1,49 +1,51 @@
 import L from "leaflet";
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { MapContainer, TileLayer, Circle } from "react-leaflet";
-import '@geoman-io/leaflet-geoman-free';
-import 'leaflet.fullscreen/Control.FullScreen.js'
+import "@geoman-io/leaflet-geoman-free";
+import "leaflet.fullscreen/Control.FullScreen.js";
 import "leaflet-geometryutil";
-import { getTopography } from 'leaflet-topography';
-import 'regenerator-runtime/runtime';
+import { getTopography } from "leaflet-topography";
+import "regenerator-runtime/runtime";
 
 import { Draw } from "./Draw";
 import LeafletControlGeocoder from "./GeoSearch";
-import { LocateComponent as Locate } from './LocateControl'
-import RoutingMachine from './RoutingMachine';
-import { Spinner } from "./Spinner"
+import { LocateComponent as Locate } from "./LocateControl";
+import { RoutingMachine } from "./RoutingMachine";
+import { Spinner } from "./Spinner";
 
 const options = {
-  token: process.env.MAPBOX_ACCESS_TOKEN
-}
+  token: process.env.MAPBOX_ACCESS_TOKEN,
+};
 
-function IntialMarkers({
-  amountOfMarkersOnLoad,
-  initRadiusForCircle
-}) {
-
-  const [markers, setMarkers] = useState(() => Array.apply(null, Array(amountOfMarkersOnLoad)).map(function () { return 0 }));
-  const [topographyData, setTopographyData] = useState([])
-  const [startingPointsRoutingMachine, startingPointsForRoutingMachine] = useState([])
-  const [init, setInit] = useState(true)
+function IntialMarkers({ amountOfMarkersOnLoad, initRadiusForCircle }) {
+  const [markers, setMarkers] = useState(() =>
+    Array.apply(null, Array(amountOfMarkersOnLoad)).map(function () {
+      return 0;
+    })
+  );
+  const [topographyData, setTopographyData] = useState([]);
+  const [startingPointsRoutingMachine, startingPointsForRoutingMachine] =
+    useState([]);
+  const [init, setInit] = useState(true);
 
   const handleInitPointsInRoutingMachine = useCallback(
     (initStartingPoints) => {
-      startingPointsForRoutingMachine(initStartingPoints)
+      startingPointsForRoutingMachine(initStartingPoints);
     },
-    [startingPointsRoutingMachine],
-  )
+    [startingPointsRoutingMachine]
+  );
 
   function getCurrentValueForDegree(iter, times) {
-    let i = 0
+    let i = 0;
+
     function value() {
       while (i < iter.length * times) {
         const cur = Math.floor(i / times);
-        i += 1
-        return iter[cur]
+        i += 1;
+        return iter[cur];
       }
     }
-    return value
+    return value;
   }
 
   function getCurrentValue(values) {
@@ -53,11 +55,11 @@ function IntialMarkers({
     function increment() {
       ++index;
       if (index < l) {
-        return values[index]
+        return values[index];
       } else {
         index = -1;
         ++index;
-        return values[index]
+        return values[index];
       }
     }
 
@@ -65,93 +67,82 @@ function IntialMarkers({
   }
 
   async function getTopographyData(latLang) {
-    var newObj = {}
+    var newObj = {};
     newObj.topography = await getTopography(latLang, options);
-    newObj.latlng = latLang
-    setTopographyData(prev => [...prev, newObj])
+    newObj.latlng = latLang;
+    setTopographyData((prev) => [...prev, newObj]);
   }
 
-  let degree = getCurrentValueForDegree([
-    0,
-    345,
-    330,
-    315,
-    300,
-    285,
-    270,
-    255,
-    240,
-    225,
-    210,
-    195,
-    180,
-    165,
-    150,
-    135,
-    120,
-    105,
-    90,
-    75,
-    60,
-    45,
-    30,
-    15], 3)
+  let degree = getCurrentValueForDegree(
+    [
+      0, 345, 330, 315, 300, 285, 270, 255, 240, 225, 210, 195, 180, 165, 150,
+      135, 120, 105, 90, 75, 60, 45, 30, 15,
+    ],
+    3
+  );
 
-  let distance = getCurrentValue([141, 308, 475])
-  let max = (a, f) => a.reduce((m, x) => m['topography'][f] > x['topography'][f] ? m : x);
-  let min = (a, f) => a.reduce((m, x) => m['topography'][f] < x['topography'][f] ? m : x);
+  let distance = getCurrentValue([141, 308, 475]);
+  let max = (a, f) =>
+    a.reduce((m, x) => (m["topography"][f] > x["topography"][f] ? m : x));
+  let min = (a, f) =>
+    a.reduce((m, x) => (m["topography"][f] < x["topography"][f] ? m : x));
 
   function setMarkerToSegment(degree, distance) {
-    const { lat, lng } = initRadiusForCircle
-    const marker = L.GeometryUtil.destination({ lat, lng }, degree(), distance());
-    return marker
+    const { lat, lng } = initRadiusForCircle;
+    const marker = L.GeometryUtil.destination(
+      { lat, lng },
+      degree(),
+      distance()
+    );
+    return marker;
   }
 
   useEffect(() => {
     if (init) {
       markers
         .map(() => setMarkerToSegment(degree, distance))
-        .map(marker => getTopographyData(marker))
-      setInit(false)
+        .map((marker) => getTopographyData(marker));
+      setInit(false);
     }
-    return () => setInit(true)
-  }, [])
+    return () => setInit(true);
+  }, []);
 
   useEffect(() => {
-
     // console.log("topographyData ", topographyData);
     if (topographyData.length === amountOfMarkersOnLoad) {
-      const routingInfo = []
-      const highestEl = max(topographyData, 'elevation');
-      const lowestEl = min(topographyData, 'elevation');
+      const routingInfo = [];
+      const highestEl = max(topographyData, "elevation");
+      const lowestEl = min(topographyData, "elevation");
 
-      routingInfo.push({ highestEl, lowestEl })
+      routingInfo.push({ highestEl, lowestEl });
 
-      handleInitPointsInRoutingMachine(routingInfo)
+      handleInitPointsInRoutingMachine(routingInfo);
     }
-  }, [topographyData.length])
-
+  }, [topographyData.length]);
 
   return (
     <>
-      <Spinner showOrHide={startingPointsRoutingMachine.length != true} />
-
-
-      {startingPointsRoutingMachine.length != true ?
+      <Spinner showOrHide={startingPointsRoutingMachine.length != true} />{" "}
+      {startingPointsRoutingMachine.length != true ? (
         <Spinner showOrHide={startingPointsRoutingMachine.length} />
-        :
-        <RoutingMachine startingPoints={startingPointsRoutingMachine} options={options} topographyData={topographyData} />}
+      ) : (
+        <RoutingMachine
+          startingPoints={startingPointsRoutingMachine}
+          options={options}
+          topographyData={topographyData}
+        />
+      )}{" "}
     </>
   );
 }
 
-const MemoizedInitialMarkers = React.memo(IntialMarkers)
+const MemoizedInitialMarkers = React.memo(IntialMarkers);
 
 export default function MyMap() {
-  const [cirlcleRadius, setCircleRadius] = useState(500)
-  const circleRef = useRef()
-  const amountOfMarkersOnLoad = 72
-  const [initRadiusForCircle, setInitialRadiusForInitCircle] = useState([])
+  const [cirlcleRadius, setCircleRadius] = useState(500);
+  const circleRef = useRef();
+  const amountOfMarkersOnLoad = 72;
+  const [initRadiusForCircle, setInitialRadiusForInitCircle] = useState([]);
 
   return (
     <MapContainer
@@ -159,14 +150,14 @@ export default function MyMap() {
       scrollWheelZoom={false}
       zoom={15}
       whenReady={(e) => {
-        const { lat, lng } = e.target._lastCenter
-        setInitialRadiusForInitCircle({ lat, lng })
+        const { lat, lng } = e.target._lastCenter;
+        setInitialRadiusForInitCircle({ lat, lng });
         // Topography.getTopography(e.target._lastCenter, options)
         //   .then((results) => console.log(results));
-      }}>
+      }}
+    >
       <TileLayer
-        url={`https://api.mapbox.com/styles/v1/${process.env.MAPBOX_USERNAME}/${process.env.MAPBOX_STYLE_ID
-          }/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`}
+        url={`https://api.mapbox.com/styles/v1/${process.env.MAPBOX_USERNAME}/${process.env.MAPBOX_STYLE_ID}/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.MAPBOX_ACCESS_TOKEN}`}
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
       />
       <Locate startDirectly />
@@ -177,12 +168,11 @@ export default function MyMap() {
         center={L.latLng(initRadiusForCircle)}
         key="1"
         radius={cirlcleRadius}
-      />
+      />{" "}
       <MemoizedInitialMarkers
         initRadiusForCircle={initRadiusForCircle}
         amountOfMarkersOnLoad={amountOfMarkersOnLoad}
-      />
-
+      />{" "}
     </MapContainer>
-  )
+  );
 }
